219cdfbf5a1b71c6bc66c9822a88d16fec7eed9c
Danny Tuppeny <danny@tuppeny.com>
2016-07-30 18:05:43 +0100

    Rough handling of server connected events.

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -1,4 +1,5 @@
 "use strict";
+import * as vscode from "vscode";
 import * as child_process from "child_process";
 
 export class Analyzer {
@@ -11,11 +12,27 @@ export class Analyzer {
 		this.analyzerProcess.stdout.on('data', (data: Buffer) => {
 			let message = data.toString();
 			console.log(`RCV: ${message}`);
-			this.handleMessage(message);
+			if (message != null && message.trim() != "")
+				this.handleMessage(message);
 		});
 	}
 
 	private handleMessage(message: string) {
+		let evt = <UnknownEvent>JSON.parse(message);
+		switch (evt.event) {
+			case "server.connected":
+				this.serverConnected(<ServerConnectedEvent>evt);
+				break;
+		}
+	}
+
+	private serverConnected(evt: ServerConnectedEvent) {
+		let message = `Connected to Dart analysis server version ${evt.params.version}`;
+
+		console.log(message);
+		let disposable = vscode.window.setStatusBarMessage(message);
+
+		setTimeout(() => disposable.dispose(), 3000);		
 	}
 
 	stop() {
@@ -23,4 +40,18 @@ export class Analyzer {
 		// TODO: end gracefully!
 		this.analyzerProcess.kill();
 	}
+}
+
+class Event<T> {
+	event: string;
+	params: T;
+}
+
+class UnknownEvent extends Event<any> { }
+
+class ServerConnectedEvent extends Event<SeverConnectedParams> {}
+
+class SeverConnectedParams {
+	version: string;
+	pid: number;
 }
