6129fb2308c156310c705175939f6799b0af562b
Danny Tuppeny <danny@tuppeny.com>
2016-08-07 12:17:28 +0100

    Cache config and only re-fetch when it updates.

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,22 @@
 "use strict";
 
-import { workspace } from "vscode";
+import { workspace, WorkspaceConfiguration } from "vscode";
 
 class Config {
+	config: WorkspaceConfiguration;
+
+	constructor() {
+		workspace.onDidChangeConfiguration(e => this.loadConfig());
+		this.loadConfig();
+	}
+
+	private loadConfig() {
+		this.config = workspace.getConfiguration("dart");
+		console.log(`Loaded config! verbose = ${this.getConfig("verbose")}`);
+	}
+
 	private getConfig<T>(key: string): T {
-		return workspace.getConfiguration("dart").get<T>(key);
+		return this.config.get<T>(key);
 	}
 
 	get userDefinedSdkPath() { return this.getConfig<string>("sdkPath"); }
