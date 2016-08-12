e4a019166a935904525d8199c7c8f593411cec1d
Danny Tuppeny <danny@tuppeny.com>
2016-08-12 12:14:32 +0100

    Fix test.

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -18,6 +18,5 @@ suite("Extension Tests", () => {
     test("Something 1", () => {
         assert.equal(-1, [1, 2, 3].indexOf(5));
         assert.equal(-1, [1, 2, 3].indexOf(0));
-        assert.equal(-1, [1, 2, 3].indexOf(1));
     });
 });
