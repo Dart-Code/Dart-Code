f20a6a6e32611b7ed4e0842a1a75494bbb1f3a7c
Danny Tuppeny <danny@tuppeny.com>
2016-08-12 11:49:03 +0100

    Break compilation to see if both Travis and AppVeyor notice.

--- src/project.ts
+++ src/project.ts
@@ -12,7 +12,7 @@ export function locateBestProjectRoot(): string {
 
 	let editor = vs.window.activeTextEditor;
 	if (!editor)
-		return root;
+		return rootBroken;
 
 	if (!util.isWithinRootPath(editor.document))
 		return root;
