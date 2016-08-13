31e5ab88907742525b7c9806d77942f7315f2321
Danny Tuppeny <danny@tuppeny.com>
2016-08-13 21:25:03 +0100

    Improve display of search results.
    See #62

--- src/providers/dart_workspace_symbol_provider.ts
+++ src/providers/dart_workspace_symbol_provider.ts
@@ -1,6 +1,8 @@
 "use strict";
 
-import { WorkspaceSymbolProvider, SymbolInformation, CancellationToken, SymbolKind, Location, Uri, Range, Position } from "vscode";
+import { WorkspaceSymbolProvider, SymbolInformation, CancellationToken, SymbolKind, Location, Uri, Range, Position, workspace } from "vscode";
+import * as path from "path";
+import * as fs from "fs";
 import { Analyzer, getSymbolKindForElementKind } from "../analysis/analyzer";
 import { toRange, isWithinRootPath } from "../utils";
 import * as as from "../analysis/analysis_server_types";
@@ -84,14 +86,54 @@ export class DartWorkspaceSymbolProvider implements WorkspaceSymbolProvider {
 	}
 
 	private convertResult(result: as.SearchResult): SymbolInformation {
+		// Rewrite the filename for best display.
+		result.location.file = this.rewriteFilename(result.location.file);
+
+		// If the parent name is just the filename, send null, else Code will display it badly (strip up to last dot).
+		let containerName = result.path[1].name;
+		if (path.basename(result.location.file) == result.path[1].name)
+			containerName = null;
+
 		return {
-			name: result.path[0].name,
+			name: result.path[0].name + (result.path[0].parameters || ""),
 			kind: getSymbolKindForElementKind(result.path[0].kind),
 			location: {
 				uri: Uri.file(result.location.file),
 				range: toRange(result.location)
 			},
-			containerName: result.path.length > 1 ? result.path[1].name : null
+			containerName: containerName
 		};
+	}
+
+	private rewriteFilename(inputPath: string): string {
+		// HACK: The AS returns paths to the PUB_CACHE folder, which Code can't
+		// convert to relative paths (so they look terrible). If the file exists in
+		// workspace.rootPath we rewrite the path to there which gives us a nice
+		// relative path.
+
+		// Currently I only do this for "hosted\pub.dartlang.org" as I'm not sure of the
+		// rules for these paths!
+
+		const pubCachePath = "hosted" + path.sep + "pub.dartlang.org";
+		let pubCachePathIndex = inputPath.indexOf(pubCachePath);
+		if (pubCachePathIndex > -1) {
+
+			let relativePath = inputPath.substring(pubCachePathIndex + pubCachePath.length + 1);
+
+			// Packages in pubcache are versioned so trim the "-x.x.x" off the end of the foldername.
+			let pathComponents = relativePath.split(path.sep);
+			pathComponents[0] = pathComponents[0].split("-")[0];
+
+			// Symlink goes into the lib folder, so strip that out of the path.
+			if (pathComponents[1] == "lib")
+				pathComponents.splice(1, 1);
+
+			// Build the path and check it exists.
+			let possiblePath = path.join(workspace.rootPath, "packages", pathComponents.join(path.sep));
+			if (fs.existsSync(possiblePath))
+				return possiblePath;
+		}
+
+		return inputPath;
 	}
 }
