f3a8f27a18a717198038800f8107a2a0a782a475
Ikhwan Hayat <ikhwanhayat@gmail.com>
2016-08-15 22:21:35 +0800

    Do not show "Info" popup when there's error.

--- src/providers/dart_rename_provider.ts
+++ src/providers/dart_rename_provider.ts
@@ -4,19 +4,19 @@ import { window, workspace, RenameProvider, WorkspaceEdit, TextDocument, Positio
 import { Analyzer } from "../analysis/analyzer";
 import * as as from "../analysis/analysis_server_types";
 
-export class DartRenameProvider implements RenameProvider {	
+export class DartRenameProvider implements RenameProvider {
 	private analyzer: Analyzer;
 	constructor(analyzer: Analyzer) {
 		this.analyzer = analyzer;
 	}
-	
-	provideRenameEdits(document: TextDocument, position: Position, newName: string, token: CancellationToken): Thenable<WorkspaceEdit> {		
+
+	provideRenameEdits(document: TextDocument, position: Position, newName: string, token: CancellationToken): Thenable<WorkspaceEdit> {
 		return this.doRename(document, position, newName, token);
 	}
 
 	private doRename(document: TextDocument, position: Position, newName: string, token: CancellationToken): Thenable<WorkspaceEdit> {
 		return new Promise<WorkspaceEdit>((resolve, reject) => {
-			var wordRange = document.getWordRangeAtPosition(position);				
+			var wordRange = document.getWordRangeAtPosition(position);
 			this.analyzer.editGetRefactoring({
 				kind: "RENAME",
 				file: document.fileName,
@@ -25,8 +25,8 @@ export class DartRenameProvider implements RenameProvider {
 				validateOnly: false,
 				options: {
 					newName: newName
-				}			
-			}).then(resp => {				
+				}
+			}).then(resp => {
 				let workspaceEdit = new WorkspaceEdit();
 
 				let hasError = this.handleProblem(
@@ -36,34 +36,34 @@ export class DartRenameProvider implements RenameProvider {
 				);
 
 				if (hasError) {
-					reject("Rename cancelled.");
+					reject("");
 					return;
 				}
 
-				let promises = [];				
+				let promises = [];
 				resp.change.edits.forEach(changeEdit => {
 					changeEdit.edits.forEach(fileEdit => {
 						let uri = Uri.file(changeEdit.file);
 						let promise = workspace.openTextDocument(uri)
 						promises.push(promise);
-						promise.then(document => {											
+						promise.then(document => {
 							workspaceEdit.replace(
 								uri,
 								new Range(
-									document.positionAt(fileEdit.offset), 
+									document.positionAt(fileEdit.offset),
 									document.positionAt(fileEdit.offset + fileEdit.length)),
 								fileEdit.replacement
 							);
 						});
-					});					
+					});
 				});
 
 				// Wait all openTextDocument to finish
 				Promise.all(promises).then(() => {
-					window.showInformationMessage(resp.change.message); 
+					window.showInformationMessage(resp.change.message);
 					resolve(workspaceEdit)
 				});
-				
+
 			}, e => console.warn(e.message));
 		});
 	}
