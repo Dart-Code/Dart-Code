457c62fe438514087a03a35663a97047eaecad10
Ikhwan Hayat <ikhwanhayat@gmail.com>
2016-08-15 08:53:21 +0800

    Implement rename.

---
+++ src/providers/dart_rename_provider.ts
@@ -0,0 +1,87 @@
+"use strict";
+
+import { window, workspace, RenameProvider, WorkspaceEdit, TextDocument, Position, CancellationToken, Uri, TextEdit, Range } from "vscode";
+import { Analyzer } from "../analysis/analyzer";
+import * as as from "../analysis/analysis_server_types";
+
+export class DartRenameProvider implements RenameProvider {	
+	private analyzer: Analyzer;
+	constructor(analyzer: Analyzer) {
+		this.analyzer = analyzer;
+	}
+	
+	provideRenameEdits(document: TextDocument, position: Position, newName: string, token: CancellationToken): Thenable<WorkspaceEdit> {		
+		return workspace.saveAll(false).then(() => {
+			return this.doRename(document, position, newName, token);
+		});
+	}
+
+	private doRename(document: TextDocument, position: Position, newName: string, token: CancellationToken): Thenable<WorkspaceEdit> {
+        return new Promise<WorkspaceEdit>((resolve, reject) => {
+			this.analyzer.editGetRefactoring({
+				kind: "RENAME",
+				file: document.fileName,
+				offset: document.offsetAt(position),
+				length: position.character,
+				validateOnly: false,
+				options: {
+					newName: newName
+				}			
+			}).then(resp => {				
+				let workspaceEdit = new WorkspaceEdit();
+
+				let hasError = this.handleProblem(
+					resp.initialProblems
+						.concat(resp.optionsProblems)
+						.concat(resp.finalProblems)
+				);
+
+				if (hasError)
+					reject("Rename cancelled.");
+
+				let promises = [];				
+				resp.change.edits.forEach(changeEdit => {
+					changeEdit.edits.forEach(fileEdit => {
+						let uri = Uri.file(changeEdit.file);
+						let promise = workspace.openTextDocument(uri)
+						promises.push(promise);
+						promise.then(document => {											
+							workspaceEdit.replace(
+								uri,
+								new Range(
+									document.positionAt(fileEdit.offset), 
+									document.positionAt(fileEdit.offset + fileEdit.length)),
+								fileEdit.replacement
+							);
+						});
+					});					
+				});
+
+				// Wait all openTextDocument to finish
+				Promise.all(promises).then(() => resolve(workspaceEdit));
+				
+			}, e => console.warn(e.message));
+		});
+	}
+
+	private handleProblem(problems: as.RefactoringProblem[]): boolean {
+		let hasError = false;
+		problems.forEach(problem => {
+			switch (problem.severity) {
+
+				case "INFO":
+					window.showInformationMessage(problem.message);
+					break;
+
+				case "WARNING":
+					window.showWarningMessage(problem.message);
+					break;
+
+				default: // This can be ERROR or FATAL problems
+					hasError = true;
+					window.showErrorMessage(problem.message);
+			}
+		});
+		return hasError;
+	}
+}
