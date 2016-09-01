06b21714521f6bad5137ef702b38585a5cfa1091
Devon Carew <devoncarew@google.com>
2016-08-31 23:47:52 -0700

    set detect indentation to false

--- src/editors.ts
+++ src/editors.ts
@@ -3,8 +3,8 @@
 import * as vs from 'vscode';
 
 export function hasActiveDartEditor(): boolean {
-  if (!vs.window.activeTextEditor)
-    return false;
+	if (!vs.window.activeTextEditor)
+		return false;
 
-  return vs.window.activeTextEditor.document.languageId === "dart";
+	return vs.window.activeTextEditor.document.languageId === "dart";
 }
