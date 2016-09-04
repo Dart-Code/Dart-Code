0d1957adf9aadcca1326889adf712817850976f9
Danny Tuppeny <danny@tuppeny.com>
2016-09-04 13:48:14 +0100

    Switch to bdd testing.

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,19 +1,19 @@
 import * as assert from 'assert';
 import * as util from '../src/utils';
 
-suite("util.isOutOfDate", () => {
-    test("should not consider build numbers when compariing versions", () => {
+describe("util.isOutOfDate", () => {
+    it("should not consider build numbers when compariing versions", () => {
         assert.equal(util.isOutOfDate("1.2.3", "1.2.3"), false);
         assert.equal(util.isOutOfDate("1.2.3+012345", "1.2.3"), false);
     });
-    test("should consider pre-release versions older than release versions", () => {
+    it("should consider pre-release versions older than release versions", () => {
         assert.equal(util.isOutOfDate("1.2.3-alpha", "1.2.3"), true);
         assert.equal(util.isOutOfDate("1.2.3-alpha+012345", "1.2.3"), true);
     });
-    test("should compare segments as individual numbers, not decimals", () => {
+    it("should compare segments as individual numbers, not decimals", () => {
         assert.equal(util.isOutOfDate("1.9.0", "1.10.0"), true);
     });
-    test("should return the correct result for some real world tests", () => {
+    it("should return the correct result for some real world tests", () => {
         assert.equal(util.isOutOfDate("1.2.0", "1.18.1"), true);
         assert.equal(util.isOutOfDate("1.18.0", "1.18.1"), true);
         assert.equal(util.isOutOfDate("1.18.1", "1.18.1"), false);
