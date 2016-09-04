e03d907c4b40dcdcc119f9d34bf94e947f81c545
Danny Tuppeny <danny@tuppeny.com>
2016-09-04 14:46:07 +0100

    Add basic tests that ensure the extension activates successfully.

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,2 +1,22 @@
 import * as assert from 'assert';
 import * as vs from "vscode";
+import * as extension from "../src/extension";
+
+const isWin = /^win/.test(process.platform);
+const ext = vs.extensions.getExtension/*<extension.ExtensionApi>*/("DanTup.dart-code");
+const sampleFilePath = (isWin ? "X:\\" : "/tmp/") + "sample.dart";
+const sampleFileUri = vs.Uri.parse(`untitled:${sampleFilePath}`);
+
+describe("Extension", () => {
+    it("is not activated initially", () => {
+		assert.equal(ext.isActive, false);
+    });
+	it("is activated successfully upon opening a Dart file", done => {
+		vs.workspace.openTextDocument(sampleFileUri)
+			.then(() => assert.equal(ext.isActive, true))
+			.then(() => ext.activate())
+			.then(api => assert.notEqual(extension.dartSdkRoot, null))
+			.then(api => assert.notEqual(extension.analyzer, null))
+			.then(done, e => done(new Error(e)));
+    });
+});
