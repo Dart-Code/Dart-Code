781f8b727dda5e817b859e9cea017f4f0e3abcae
Danny Tuppeny <danny@tuppeny.com>
2016-11-18 12:20:03 +0000

    Fix indenting.

--- src/analyzer_status_reporter.ts
+++ src/analyzer_status_reporter.ts
@@ -107,7 +107,7 @@ ${error.stackTrace}
 }
 
 function getRandomInt(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
 }
