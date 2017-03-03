96b1d5edb346051f8c0bed00d1dc9a1863f73601
Danny Tuppeny <danny@tuppeny.com>
2017-03-03 12:45:40 +0000

    Automatically update launch.json if is old style.
    Fixes #263.

--- src/project_upgrade.ts
+++ src/project_upgrade.ts
@@ -2,6 +2,7 @@ import * as vs from "vscode";
 
 export function upgradeProject() {
 	update_launch_json_change_sdk_to_settings();
+	update_launch_json_change_command_to_colon();
 }
 
 function update_launch_json_change_sdk_to_settings() {
@@ -26,7 +27,31 @@ function update_launch_json_change_sdk_to_settings() {
 		// Add the new debugSettings.
 		if (!d.debugSettings) {
 			console.log("Didn't find debugSettings, adding...");
-			d.debugSettings = "${command.debugSettings}";
+			d.debugSettings = "${command:debugSettings}";
+			hasChanged = true;
+		}
+	});
+
+	if (hasChanged)
+		launchFile.update("configurations", configs);
+}
+
+function update_launch_json_change_command_to_colon() {
+	// Read launch.json config.
+	let launchFile = vs.workspace.getConfiguration("launch");
+
+	let configs = launchFile.get<any[]>("configurations");
+	if (!configs)
+		return;
+
+	let hasChanged = false;
+
+	// Find Dart CLI items that might need upgrading.
+	configs.filter(c => c.type == "dart-cli").map(d => {
+		// Add the new debugSettings.
+		if (d.debugSettings == "${command.debugSettings}") {
+			console.log("Found old debugSettings, updating...");
+			d.debugSettings = "${command:debugSettings}";
 			hasChanged = true;
 		}
 	});
