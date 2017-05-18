7b5ba1b1308a461668e8f8b14aad7916fd413e60
Danny Tuppeny <danny@tuppeny.com>
2017-05-18 21:51:28 +0100

    Set cusor position after source edits.
    Fixes #303

--- src/commands/edit.ts
+++ src/commands/edit.ts
@@ -69,6 +69,14 @@ export class EditCommands implements vs.Disposable {
 			})
 		});
 
+		// Apply the edits.		
 		vs.workspace.applyEdit(changes);
+
+		// Set the cursor position.
+		if (change.selection) {
+			let pos = document.positionAt(change.selection.offset);
+			let selection = new vs.Selection(pos, pos);
+			vs.window.showTextDocument(document).then(ed => ed.selection = selection);
+		}
 	}
 }
