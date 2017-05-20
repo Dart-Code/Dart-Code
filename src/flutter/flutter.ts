ef05d770c2258033dae354d272c1d1f7db35f353
Danny Tuppeny <danny@tuppeny.com>
2017-05-20 13:23:26 +0100

    Track connected devices in status bar.
    See #198

--- src/flutter/flutter.ts
+++ src/flutter/flutter.ts
@@ -6,12 +6,27 @@ import * as fs from "fs";
 import { config } from "../config";
 import { log, logError, extensionVersion } from "../utils";
 import { StdIOService, Request, UnknownResponse, UnknownNotification } from "../services/stdio_service";
+import * as f from "./flutter_types";
 
 export class Flutter extends StdIOService {
 	constructor(flutterBinPath: string, projectFolder: string) {
-		super("Flutter daemon", config.flutterDaemonLogFile);
+		super(config.flutterDaemonLogFile, true);
 
 		this.createProcess(projectFolder, flutterBinPath, ["daemon"]);
+	}
+
+	protected sendMessage<T>(json: string) {
+		try {
+			super.sendMessage(json);
+		}
+		catch (e) {
+			const reloadAction: string = "Reload Project";
+			vs.window.showErrorMessage(`The Flutter Daemon has terminated. Save your changes then reload the project to resume.`, reloadAction).then(res => {
+				if (res == reloadAction)
+					vs.commands.executeCommand("workbench.action.reloadWindow");
+			});
+			throw e;
+		}
 	}
 
 	protected shouldHandleMessage(message: string): boolean {
@@ -19,11 +34,39 @@ export class Flutter extends StdIOService {
 		return message.startsWith('[') && message.endsWith(']');
 	}
 
+	// TODO: Can we code-gen all this like the analysis server?
+
 	protected handleNotification(evt: UnknownNotification) {
 		switch (evt.event) {
-			// case "server.connected":
-			// 	this.notify(this.serverConnectedSubscriptions, <as.ServerConnectedNotification>evt.params);
-			// 	break;
+			case "device.added":
+				this.notify(this.deviceAddedSubscriptions, <f.Device>evt.params);
+				break;
+			case "device.removed":
+				this.notify(this.deviceRemovedSubscriptions, <f.Device>evt.params);
+				break;
 		}
+	}
+
+	// Subscription lists.	
+
+	private deviceAddedSubscriptions: ((notification: f.Device) => void)[] = [];
+	private deviceRemovedSubscriptions: ((notification: f.Device) => void)[] = [];
+
+
+	// Request methods.
+
+	deviceEnable(): Thenable<UnknownResponse> {
+		return this.sendRequest("device.enable");
+	}
+
+
+	// Subscription methods.
+
+	registerForDeviceAdded(subscriber: (notification: f.Device) => void): vs.Disposable {
+		return this.subscribe(this.deviceAddedSubscriptions, subscriber);
+	}
+
+	registerForDeviceRemoved(subscriber: (notification: f.Device) => void): vs.Disposable {
+		return this.subscribe(this.deviceRemovedSubscriptions, subscriber);
 	}
 }
