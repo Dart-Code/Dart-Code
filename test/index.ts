a31c55af45a4251c7d60124419d2382d88403729
Danny Tuppeny <danny@tuppeny.com>
2017-06-16 14:33:26 +0100

    Increase test timeout.

--- test/index.ts
+++ test/index.ts
@@ -15,8 +15,9 @@ var testRunner = require('vscode/lib/testrunner');
 // You can directly control Mocha options by uncommenting the following lines
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
 testRunner.configure({
-    ui: 'bdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true // colored output from test results
+	ui: 'bdd',        // the TDD UI is being used in extension.test.ts (suite, test, etc.)
+	useColors: true,  // colored output from test results
+	timeout: 10000    // increased timeout because starting up Code, Analyzer, etc. is slooow
 });
 
 module.exports = testRunner;
