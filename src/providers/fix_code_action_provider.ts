3c14a9d23115277d7ea9365a173985f7b497f787
Danny Tuppeny <danny@tuppeny.com>
2018-02-12 12:20:29 +0000

    Split fixes from assists to simplify code

--- src/providers/fix_code_action_provider.ts
+++ src/providers/fix_code_action_provider.ts
@@ -9,7 +9,7 @@ import { logError, isAnalyzableAndInWorkspace } from "../utils";
 import * as as from "../analysis/analysis_server_types";
 import { DartDiagnosticProvider } from "./dart_diagnostic_provider";
 
-export class DartCodeActionProvider implements CodeActionProvider {
+export class FixCodeActionProvider implements CodeActionProvider {
 	private analyzer: Analyzer;
 	constructor(analyzer: Analyzer) {
 		this.analyzer = analyzer;
@@ -19,25 +19,30 @@ export class DartCodeActionProvider implements CodeActionProvider {
 		if (!isAnalyzableAndInWorkspace(document))
 			return null;
 		return new Promise<CodeAction[]>((resolve, reject) => {
-			this.analyzer.editGetAssists({
+			this.analyzer.editGetFixes({
 				file: document.fileName,
-				length: range.end.character - range.start.character,
 				offset: document.offsetAt(range.start),
-			}).then((assists) => {
-				const actions = assists.assists.map((assist) => this.convertResult(document, assist, CodeActionKind.Refactor));
-				resolve(actions);
+			}).then((result) => {
+				const allActions = new Array<CodeAction>();
+
+				for (const errorFix of result.fixes)
+					allActions.push(...errorFix.fixes.map((fix) => this.convertResult(document, fix, CodeActionKind.QuickFix, errorFix.error)));
+
+				resolve(allActions);
 			}, (e) => { logError(e); reject(); });
 		});
 	}
 
-	private convertResult(document: TextDocument, change: as.SourceChange, kind: CodeActionKind): CodeAction {
+	private convertResult(document: TextDocument, change: as.SourceChange, kind: CodeActionKind, error: as.AnalysisError): CodeAction {
 		const title = change.message;
+		const diagnostics = error ? [DartDiagnosticProvider.createDiagnostic(error)] : undefined;
 		return {
 			command: {
 				arguments: [document, change],
 				command: "_dart.applySourceChange",
 				title,
 			},
+			diagnostics,
 			kind,
 			title,
 		};
