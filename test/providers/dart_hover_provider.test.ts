061da4530959f8d66905401f0d4f9b09e69fe622
Danny Tuppeny <danny@tuppeny.com>
2018-02-12 18:07:06 +0000

    Call showTextDocument rather than assuming we're the active editor

--- test/providers/dart_hover_provider.test.ts
+++ test/providers/dart_hover_provider.test.ts
@@ -7,27 +7,30 @@ const sampleFilePath = (isWin ? "X:\\" : "/tmp/") + "sample.dart";
 const sampleFileUri = vs.Uri.parse(`untitled:${sampleFilePath}`);
 
 describe("dart_hover_provider", () => {
+	let doc: vs.TextDocument;
+	let editor: vs.TextEditor;
 	before(async () => {
 		await ext.activate();
-		await vs.workspace.openTextDocument(sampleFileUri);
-		await vs.window.activeTextEditor.edit((b) => b.insert(new vs.Position(0, 0), "\r\nprint('Hello, world');\r\n"));
+		doc = await vs.workspace.openTextDocument(sampleFileUri);
+		editor = await vs.window.showTextDocument(doc);
+		await editor.edit((b) => b.insert(new vs.Position(0, 0), "\r\nprint('Hello, world');\r\n"));
 	});
 
-	function getHovers(searchText: string): Thenable<vs.Hover[]> {
-		const index = vs.window.activeTextEditor.document.getText().indexOf(searchText);
+	function getHoversAt(searchText: string): Thenable<vs.Hover[]> {
+		const index = doc.getText().indexOf(searchText);
 		assert.notEqual(index, -1, `Couldn't find string ${searchText} in the document to send hovers`);
-		const position = vs.window.activeTextEditor.document.positionAt(index);
+		const position = doc.positionAt(index);
 		return vs.commands.executeCommand("vscode.executeHoverProvider", sampleFileUri, position) as Thenable<vs.Hover[]>;
 	}
 
 	it("does not return hovers for blank areas of the document", async () => {
-		const hovers = await getHovers("\n");
+		const hovers = await getHoversAt("\n");
 		assert.equal(hovers.length, 0);
 	});
 
 	// TODO: Figure out why this isn't working...
 	// it("does returns a hover for a function call", async () => {
-	// 	const hovers = await getHovers("print");
+	// 	const hovers = await getHoversAt("print");
 	// 	assert.equal(hovers.length, 1);
 	// });
 });
