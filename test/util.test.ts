2c5ffaef7fe2cf3b5c36841cbd6e1aae99020b4e
Danny Tuppeny <danny@tuppeny.com>
2018-02-12 13:32:51 +0000

    Remove own version of version comparisons

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,25 +1,26 @@
 import * as assert from "assert";
 import * as util from "../src/utils";
 
-describe("util.isOutOfDate", () => {
+describe("util.versionIsAtLeast", () => {
 	it("should not consider build numbers when comparing versions", () => {
-		assert.equal(util.isOutOfDate("1.2.3", "1.2.3"), false);
-		assert.equal(util.isOutOfDate("1.2.3+012345", "1.2.3"), false);
+		assert.equal(util.versionIsAtLeast("1.2.3", "1.2.3"), true);
+		assert.equal(util.versionIsAtLeast("1.2.3+012345", "1.2.3"), true);
 	});
 	it("should consider pre-release versions older than release versions", () => {
-		assert.equal(util.isOutOfDate("1.2.3-alpha", "1.2.3"), true);
-		assert.equal(util.isOutOfDate("1.2.3-alpha+012345", "1.2.3"), true);
+		assert.equal(util.versionIsAtLeast("1.2.3-alpha", "1.2.3"), false);
+		assert.equal(util.versionIsAtLeast("1.2.3-alpha+012345", "1.2.3"), false);
 	});
 	it("should compare segments as individual numbers, not decimals", () => {
-		assert.equal(util.isOutOfDate("1.9.0", "1.10.0"), true);
+		assert.equal(util.versionIsAtLeast("1.9.0", "1.10.0"), false);
 	});
 	it("should return the correct result for some real world tests", () => {
-		assert.equal(util.isOutOfDate("1.2.0", "1.18.1"), true);
-		assert.equal(util.isOutOfDate("1.18.0", "1.18.1"), true);
-		assert.equal(util.isOutOfDate("1.18.1", "1.18.1"), false);
-		assert.equal(util.isOutOfDate("1.19.0", "1.18.1"), false);
-		assert.equal(util.isOutOfDate("1.19.0-dev.0.0", "1.18.1"), false);
-		assert.equal(util.isOutOfDate("1.19.0-dev.5.0", "1.18.1"), false);
-		assert.equal(util.isOutOfDate("1.19.0-dev.7.0", "1.18.1"), false);
+		assert.equal(util.versionIsAtLeast("1.2.0", "1.18.1"), false);
+		assert.equal(util.versionIsAtLeast("1.18.0", "1.18.1"), false);
+		assert.equal(util.versionIsAtLeast("1.18.1", "1.18.1"), true);
+		assert.equal(util.versionIsAtLeast("1.19.0", "1.18.1"), true);
+		assert.equal(util.versionIsAtLeast("1.19.0-dev.0.0", "1.18.1"), true);
+		assert.equal(util.versionIsAtLeast("1.19.0-dev.5.0", "1.18.1"), true);
+		assert.equal(util.versionIsAtLeast("1.19.0-dev.7.0", "1.18.1"), true);
+		assert.equal(util.versionIsAtLeast("1.19.1-dev.0.0", "1.19.0"), true);
 	});
 });
