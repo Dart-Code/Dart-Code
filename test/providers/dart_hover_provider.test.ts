4e77ece4e9dfa17c547600a38f83007f77030225
Danny Tuppeny <danny@tuppeny.com>
2018-02-13 11:38:16 +0000

    Tweaks to test to use hello_world

--- test/providers/dart_hover_provider.test.ts
+++ test/providers/dart_hover_provider.test.ts
@@ -1,26 +1,33 @@
 import * as assert from "assert";
+import * as path from "path";
+import * as fs from "fs";
 import * as vs from "vscode";
 
-const isWin = /^win/.test(process.platform);
 const ext = vs.extensions.getExtension("Dart-Code.dart-code");
-const sampleFilePath = (isWin ? "X:\\" : "/tmp/") + "sample.dart";
-const sampleFileUri = vs.Uri.parse(`untitled:${sampleFilePath}`);
+const helloWorldFolder = vs.Uri.file(path.join(ext.extensionPath, "test/data/hello_world"));
+const helloWorldFile = vs.Uri.file(path.join(helloWorldFolder.fsPath, "bin/main.dart"));
 
 describe("dart_hover_provider", () => {
 	let doc: vs.TextDocument;
 	let editor: vs.TextEditor;
+
 	before(async () => {
 		await ext.activate();
-		doc = await vs.workspace.openTextDocument(sampleFileUri);
+		await vs.commands.executeCommand("vscode.openFolder", helloWorldFolder);
+		console.log("OPENING " + helloWorldFile.fsPath);
+		console.log("EXISTS? " + fs.existsSync(helloWorldFile.fsPath));
+		doc = await vs.workspace.openTextDocument(helloWorldFile);
+		console.log("OPENED");
 		editor = await vs.window.showTextDocument(doc);
-		await editor.edit((b) => b.insert(new vs.Position(0, 0), "\r\nprint('Hello, world');\r\n"));
+		console.log("SHOWED");
 	});
 
 	function getHoversAt(searchText: string): Thenable<vs.Hover[]> {
 		const index = doc.getText().indexOf(searchText);
 		assert.notEqual(index, -1, `Couldn't find string ${searchText} in the document to send hovers`);
 		const position = doc.positionAt(index);
-		return vs.commands.executeCommand("vscode.executeHoverProvider", sampleFileUri, position) as Thenable<vs.Hover[]>;
+		console.warn("EXECUTING vscode.executeHoverProvider");
+		return vs.commands.executeCommand("vscode.executeHoverProvider", doc.uri, position) as Thenable<vs.Hover[]>;
 	}
 
 	it("does not return hovers for blank areas of the document", async () => {
@@ -28,9 +35,8 @@ describe("dart_hover_provider", () => {
 		assert.equal(hovers.length, 0);
 	});
 
-	// TODO: Figure out why this isn't working...
-	// it("does returns a hover for a function call", async () => {
-	// 	const hovers = await getHoversAt("print");
-	// 	assert.equal(hovers.length, 1);
-	// });
+	it("does returns a hover for a function call", async () => {
+		const hovers = await getHoversAt("print");
+		assert.equal(hovers.length, 1);
+	});
 });
