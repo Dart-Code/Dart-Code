6bfe5759add89fce039d6b2ad60d54956e9162b1
Danny Tuppeny <danny@tuppeny.com>
2018-02-26 20:14:06 +0000

    Extract some testing helpers to a file for sharing

--- test/providers/dart_hover_provider.test.ts
+++ test/providers/dart_hover_provider.test.ts
@@ -2,35 +2,11 @@ import * as assert from "assert";
 import * as path from "path";
 import * as fs from "fs";
 import * as vs from "vscode";
-
-const ext = vs.extensions.getExtension("Dart-Code.dart-code");
-const helloWorldFolder = vs.Uri.file(path.join(ext.extensionPath, "test/test_projects/hello_world"));
-const emptyFile = vs.Uri.file(path.join(helloWorldFolder.fsPath, "lib/empty.dart"));
+import { activate, doc, getPositionOf, setTestContent, rangeOf } from "../helpers";
 
 describe("dart_hover_provider", () => {
-	let doc: vs.TextDocument;
-	let editor: vs.TextEditor;
 
-	before(async () => ext.activate());
-
-	async function setTestContent(content: string): Promise<boolean> {
-		doc = await vs.workspace.openTextDocument(emptyFile);
-		editor = await vs.window.showTextDocument(doc);
-		const all = new vs.Range(
-			doc.positionAt(0),
-			doc.positionAt(doc.getText().length),
-		);
-		return editor.edit((eb) => eb.replace(all, content));
-	}
-
-	function getPositionOf(searchText: string): vs.Position {
-		const caretOffset = searchText.indexOf("^");
-		assert.notEqual(caretOffset, -1, `Couldn't find a ^ in search text (${searchText})`);
-		const matchedTextIndex = doc.getText().indexOf(searchText.replace("^", ""));
-		assert.notEqual(matchedTextIndex, -1, `Couldn't find string ${searchText.replace("^", "")} in the document to get position of`);
-
-		return doc.positionAt(matchedTextIndex + caretOffset);
-	}
+	before(async () => activate());
 
 	async function getHoversAt(searchText: string): Promise<Array<{ displayText: string, documentation?: string, range: vs.Range }>> {
 		const position = getPositionOf(searchText);
@@ -59,21 +35,6 @@ describe("dart_hover_provider", () => {
 		const hovers = await getHoversAt(searchText);
 		assert.equal(hovers.length, 1);
 		return hovers[0];
-	}
-
-	function rangeOf(searchText: string): vs.Range {
-		const startOffset = searchText.indexOf("|");
-		assert.notEqual(startOffset, -1, `Couldn't find a | in search text (${searchText})`);
-		const endOffset = searchText.lastIndexOf("|");
-		assert.notEqual(endOffset, -1, `Couldn't find a second | in search text (${searchText})`);
-
-		const matchedTextIndex = doc.getText().indexOf(searchText.replace(/\|/g, ""));
-		assert.notEqual(matchedTextIndex, -1, `Couldn't find string ${searchText.replace(/\|/g, "")} in the document to get range of`);
-
-		return new vs.Range(
-			doc.positionAt(matchedTextIndex + startOffset),
-			doc.positionAt(matchedTextIndex + endOffset - 1),
-		);
 	}
 
 	it("does not return hovers for blank areas of the document", async () => {
