111a8e1dc173665981eee33866e4df5a814734fc
Danny Tuppeny <danny@tuppeny.com>
2018-03-02 21:13:12 +0000

    Update launch configs for general/flutter tests

--- test/test_all.js
+++ test/test_all.js
@@ -2,12 +2,16 @@ var path = require('path');
 var childProcess = require('child_process');
 
 const env = Object.create(process.env);
-const args = ['node_modules/vscode/bin/test'].concat(process.argv);
+const args = ['node_modules/vscode/bin/test'];
 
 function runTests(testFolder, workspaceFolder) {
 	env.CODE_TESTS_WORKSPACE = path.join(process.cwd(), 'test', 'test_projects', workspaceFolder);
 	env.CODE_TESTS_PATH = path.join(process.cwd(), 'out', 'test', testFolder);
-	childProcess.execFileSync('node', args, { env: env, stdio: 'inherit' });
+	const res = childProcess.spawnSync('node', args, { env: env, stdio: 'pipe', cwd: process.cwd() });
+	if (res.error)
+		throw error;
+	if (res.output)
+		res.output.forEach(l => console.log((l || "").toString()));
 }
 
 runTests('general', 'hello_world');
