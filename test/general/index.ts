0c0b8be7d4de84490e0d195aab15cc4327ad30fa
Danny Tuppeny <danny@tuppeny.com>
2018-03-07 19:10:33 +0000

    Handle running tests without coverage

--- test/general/index.ts
+++ test/general/index.ts
@@ -5,7 +5,7 @@ const onExit = require("signal-exit"); // tslint:disable-line:no-var-requires
 // Ensure we write coverage on exit.
 declare const __coverage__: any;
 onExit(() => {
-	if (__coverage__) {
+	if (typeof __coverage__ !== "undefined") {
 		if (!fs.existsSync("./.nyc_output"))
 			fs.mkdirSync("./.nyc_output");
 		fs.writeFileSync("./.nyc_output/" + new Date().getTime() + ".json", JSON.stringify(__coverage__));
