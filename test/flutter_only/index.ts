a844d2d67f1a4998bb0badb704b9c35602757100
Danny Tuppeny <danny@tuppeny.com>
2018-03-10 20:20:34 +0000

    Share test runner code

--- test/flutter_only/index.ts
+++ test/flutter_only/index.ts
@@ -1,26 +1,3 @@
-import * as fs from "fs";
-import testRunner = require("vscode/lib/testrunner");
-const onExit = require("signal-exit"); // tslint:disable-line:no-var-requires
+import * as test_runner from "../test_runner";
 
-// Ensure we write coverage on exit.
-declare const __coverage__: any;
-onExit(() => {
-	// Unhandled exceptions here seem to hang, but console.error+process.exit do not! ¯\_(ツ)_/¯
-	try {
-		if (typeof __coverage__ !== "undefined" && typeof process.env.COVERAGE_OUTPUT !== "undefined" && process.env.COVERAGE_OUTPUT) {
-			fs.writeFileSync(process.env.COVERAGE_OUTPUT, JSON.stringify(__coverage__));
-		}
-	} catch (e) {
-		console.error(e);
-		process.exit(1);
-	}
-});
-
-testRunner.configure({
-	slow: 1500,       // increased threshold before marking a test as slow
-	timeout: 30000,   // increased timeout because starting up Code, Analyzer, etc. is slooow
-	ui: "bdd",        // the TDD UI is being used in extension.test.ts (suite, test, etc.)
-	useColors: true,  // colored output from test results
-});
-
-module.exports = testRunner;
+module.exports = test_runner;
