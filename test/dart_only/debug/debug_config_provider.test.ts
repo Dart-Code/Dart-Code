eb237a7a1f4368ff03eba5e418f4e8b665302adf
Danny Tuppeny <danny@tuppeny.com>
2018-03-11 22:10:30 +0000

    Test we get correct debugger executable for workspaces
    This will need changing as part of #541.

---
+++ test/dart_only/debug/debug_config_provider.test.ts
@@ -0,0 +1,18 @@
+import * as assert from "assert";
+import * as path from "path";
+import * as fs from "fs";
+import * as vs from "vscode";
+import { activate, doc, setTestContent, editor, eol } from "../../helpers";
+
+describe("debug_config_provider", () => {
+
+	before(async () => activate());
+
+	it("returns the dart debug entry script", async () => {
+		const entry = await vs.commands.executeCommand("dart.getDebuggerExecutable", vs.workspace.workspaceFolders[0].uri.toString()) as { args: string[], command: string };
+		assert.ok(entry);
+		assert.ok(entry.args);
+		assert.equal(entry.args.length, 1);
+		assert.notEqual(entry.args[0].indexOf("dart_debug_entry"), -1);
+	});
+});
