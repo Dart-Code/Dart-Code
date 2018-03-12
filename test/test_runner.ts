c02aba3c52dd54e86be3859cb8938ffde12c0c24
Danny Tuppeny <danny@tuppeny.com>
2018-03-12 19:08:44 +0000

    Remove list reported which is formatted worse but still has extra newlines

--- test/test_runner.ts
+++ test/test_runner.ts
@@ -17,7 +17,6 @@ onExit(() => {
 });
 
 testRunner.configure({
-	reporter: "list",
 	slow: 1500,       // increased threshold before marking a test as slow
 	timeout: 10000,   // increased timeout because starting up Code, Analyzer, etc. is slooow
 	ui: "bdd",        // the TDD UI is being used in extension.test.ts (suite, test, etc.)
