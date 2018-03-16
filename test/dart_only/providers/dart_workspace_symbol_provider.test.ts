018661c18767d4a19fc31d80244f1c53fffab2b2
Danny Tuppeny <danny@tuppeny.com>
2018-03-16 22:20:09 +0000

    Fix expected path separators for Windows

--- test/dart_only/providers/dart_workspace_symbol_provider.test.ts
+++ test/dart_only/providers/dart_workspace_symbol_provider.test.ts
@@ -17,16 +17,16 @@ describe("workspace_symbol_provider", () => {
 	it("includes items from 'everything.dart'", async () => {
 		const symbols = await getWorkspaceSymbols("my");
 
-		ensureSymbol(symbols, "MyClass", vs.SymbolKind.Class, "lib/everything.dart", everythingFile);
+		ensureSymbol(symbols, "MyClass", vs.SymbolKind.Class, `lib${path.sep}everything.dart`, everythingFile);
 
 		// Results in legacy version are kinda junk (these are apparently missing!) so only check in v2.
 		if (ext.exports.analyzerCapabilities.isDart2) {
-			ensureSymbol(symbols, "MyClass.myNumField", vs.SymbolKind.Field, "lib/everything.dart", everythingFile);
-			ensureSymbol(symbols, "MyClass.myNumGetter", vs.SymbolKind.Property, "lib/everything.dart", everythingFile);
-			ensureSymbol(symbols, "MyClass.myNumSetter", vs.SymbolKind.Property, "lib/everything.dart", everythingFile);
-			ensureSymbol(symbols, "MyClass.myNamed()", vs.SymbolKind.Constructor, "lib/everything.dart", everythingFile);
-			ensureSymbol(symbols, "MyClass.myVoidReturningMethod()", vs.SymbolKind.Method, "lib/everything.dart", everythingFile);
-			ensureSymbol(symbols, "MyClass.myStringReturningMethod()", vs.SymbolKind.Method, "lib/everything.dart", everythingFile);
+			ensureSymbol(symbols, "MyClass.myNumField", vs.SymbolKind.Field, "lib${path.sep}everything.dart", everythingFile);
+			ensureSymbol(symbols, "MyClass.myNumGetter", vs.SymbolKind.Property, "lib${path.sep}everything.dart", everythingFile);
+			ensureSymbol(symbols, "MyClass.myNumSetter", vs.SymbolKind.Property, "lib${path.sep}everything.dart", everythingFile);
+			ensureSymbol(symbols, "MyClass.myNamed()", vs.SymbolKind.Constructor, "lib${path.sep}everything.dart", everythingFile);
+			ensureSymbol(symbols, "MyClass.myVoidReturningMethod()", vs.SymbolKind.Method, "lib${path.sep}everything.dart", everythingFile);
+			ensureSymbol(symbols, "MyClass.myStringReturningMethod()", vs.SymbolKind.Method, "lib${path.sep}everything.dart", everythingFile);
 			assert.equal(symbols.length, 7);
 		}
 	});
