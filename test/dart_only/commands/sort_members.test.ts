3aa1b7c51c27879cfc724eab207894e422cebb28
Danny Tuppeny <danny@tuppeny.com>
2018-03-27 19:12:26 +0100

    Simplify waiting for editor changes
    As well as increase timeout for Travis.

--- test/dart_only/commands/sort_members.test.ts
+++ test/dart_only/commands/sort_members.test.ts
@@ -2,7 +2,7 @@ import * as assert from "assert";
 import * as path from "path";
 import * as fs from "fs";
 import * as vs from "vscode";
-import { activate, doc, getPositionOf, setTestContent, editor, ensureTestContent, delay, waitFor } from "../../helpers";
+import { activate, doc, getPositionOf, setTestContent, editor, ensureTestContent, delay, waitFor, waitForEditorChange } from "../../helpers";
 
 describe("sort members", () => {
 
@@ -22,11 +22,7 @@ dd() {}
 cc() {}
 		`);
 
-		const oldVersion = doc.version;
-		await vs.commands.executeCommand("dart.sortMembers");
-
-		// Wait a while since text editor commands are fire-and-forget
-		await waitFor(() => doc.version !== oldVersion, 500);
+		await waitForEditorChange(() => vs.commands.executeCommand("dart.sortMembers"));
 
 		// b&a inside the class seem sorted the wrong way?
 		// https://github.com/dart-lang/sdk/issues/32683
