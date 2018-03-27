139989be8049426f37f419f53ca90bbb34d9e430
Danny Tuppeny <danny@tuppeny.com>
2018-03-27 18:07:34 +0100

    Fix some races in tests

--- test/dart_only/commands/sort_members.test.ts
+++ test/dart_only/commands/sort_members.test.ts
@@ -2,15 +2,14 @@ import * as assert from "assert";
 import * as path from "path";
 import * as fs from "fs";
 import * as vs from "vscode";
-import { activate, doc, getPositionOf, setTestContent, editor, ensureTestContent, delay } from "../../helpers";
+import { activate, doc, getPositionOf, setTestContent, editor, ensureTestContent, delay, waitFor } from "../../helpers";
 
 describe("sort members", () => {
 
 	before(() => activate());
 
 	it("sorts members", async () => {
-		await vs.window.activeTextEditor.edit(async (edit) => {
-			await setTestContent(`
+		await setTestContent(`
 int b;
 int a;
 class Person {
@@ -21,9 +20,13 @@ class Person {
 }
 dd() {}
 cc() {}
-			`);
-			await vs.commands.executeCommand("dart.sortMembers", vs.window.activeTextEditor, edit);
-		});
+		`);
+
+		const oldVersion = doc.version;
+		await vs.commands.executeCommand("dart.sortMembers");
+
+		// Wait a while since text editor commands are fire-and-forget
+		await waitFor(() => doc.version !== oldVersion, 500);
 
 		// b&a inside the class seem sorted the wrong way?
 		// https://github.com/dart-lang/sdk/issues/32683
