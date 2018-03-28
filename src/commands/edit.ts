0db42a0158774c812a406969e1a871fa19857626
Danny Tuppeny <danny@tuppeny.com>
2018-03-28 15:01:14 +0100

    Flatten edit code to use async/await

--- src/commands/edit.ts
+++ src/commands/edit.ts
@@ -65,7 +65,7 @@ export class EditCommands implements vs.Disposable {
 			command.dispose();
 	}
 
-	private applyEdits(document: vs.TextDocument, change: as.SourceChange) {
+	private async applyEdits(document: vs.TextDocument, change: as.SourceChange): Promise<void> {
 		// We can only apply with snippets if there's a single change.
 		if (change.edits.length === 1 && change.linkedEditGroups != null && change.linkedEditGroups.length !== 0)
 			return this.applyEditsWithSnippets(document, change);
@@ -73,8 +73,8 @@ export class EditCommands implements vs.Disposable {
 		// Otherwise, just make all the edits without the snippets.
 		const changes = new vs.WorkspaceEdit();
 
-		change.edits.forEach((edit) => {
-			edit.edits.forEach((e) => {
+		for (const edit of change.edits) {
+			for (const e of edit.edits) {
 				changes.replace(
 					vs.Uri.file(edit.file),
 					new vs.Range(
@@ -84,72 +84,71 @@ export class EditCommands implements vs.Disposable {
 					),
 					e.replacement,
 				);
-			});
-		});
+			}
+		}
 
 		// Apply the edits.
-		vs.workspace.applyEdit(changes).then((success) => {
-			// Set the cursor position.
-			if (change.selection) {
-				const pos = document.positionAt(change.selection.offset);
-				const selection = new vs.Selection(pos, pos);
-				vs.window.showTextDocument(document).then((ed) => ed.selection = selection);
-			}
-		});
+		await vs.workspace.applyEdit(changes);
+
+		// Set the cursor position.
+		if (change.selection) {
+			const pos = document.positionAt(change.selection.offset);
+			const selection = new vs.Selection(pos, pos);
+			const ed = await vs.window.showTextDocument(document);
+			ed.selection = selection;
+		}
 	}
 
-	private applyEditsWithSnippets(document: vs.TextDocument, change: as.SourceChange) {
+	private async applyEditsWithSnippets(document: vs.TextDocument, change: as.SourceChange): Promise<void> {
 		const edit = change.edits[0];
-		vs.window.showTextDocument(document).then((editor) => {
-			// Apply of all of the edits.
-			editor.edit((eb) => {
-				edit.edits.forEach((e) => {
-					eb.replace(
-						new vs.Range(document.positionAt(e.offset), document.positionAt(e.offset + e.length)),
-						e.replacement,
-					);
-				});
-			}).then((_) => {
-				const documentText = editor.document.getText();
-
-				// Create a list of all the placeholders.
-				const placeholders: Array<{ offset: number, length: number, defaultValue: string, choices?: string[], placeholderNumber: number }> = [];
-				let placeholderNumber = 1;
-				change.linkedEditGroups.forEach((leg) => {
-					leg.positions.forEach((pos) => {
-						const defaultValue = documentText.substr(pos.offset, leg.length);
-						const choices = leg.suggestions ? leg.suggestions.map((s) => s.value) : null;
-						placeholders.push({ offset: pos.offset, length: leg.length, defaultValue, choices, placeholderNumber });
-					});
-					placeholderNumber++;
-				});
-
-				// Ensure they're in offset order so the next maths works!
-				placeholders.sort((p1, p2) => p1.offset - p2.offset);
-
-				const snippet = new vs.SnippetString();
-				const firstPlaceholder = placeholders[0];
-				const lastPlaceholder = placeholders[placeholders.length - 1];
-				const startPos = firstPlaceholder.offset;
-				const endPos = lastPlaceholder.offset + lastPlaceholder.length;
-				let currentPos = startPos;
-				placeholders.forEach((p) => {
-					// Add the text from where we last were up to current placeholder.
-					if (currentPos !== p.offset)
-						snippet.appendText(documentText.substring(currentPos, p.offset));
-					// Add the choices / placeholder.
-					// Uncomment for https://github.com/Dart-Code/Dart-Code/issues/569 when there's an API we can use
-					if (p.choices && p.choices.length > 1)
-						snippet.appendText("").value += "${" + p.placeholderNumber + "|" + p.choices.map((c) => this.snippetStringEscape(c)).join(",") + "|}";
-					else
-						snippet.appendPlaceholder(p.defaultValue, p.placeholderNumber);
-					currentPos = p.offset + p.length;
-				});
-
-				// Replace the document.
-				editor.insertSnippet(snippet, new vs.Range(document.positionAt(startPos), document.positionAt(endPos)));
+		const editor = await vs.window.showTextDocument(document);
+		// Apply of all of the edits.
+		await editor.edit((eb) => {
+			edit.edits.forEach((e) => {
+				eb.replace(
+					new vs.Range(document.positionAt(e.offset), document.positionAt(e.offset + e.length)),
+					e.replacement,
+				);
 			});
 		});
+		const documentText = editor.document.getText();
+
+		// Create a list of all the placeholders.
+		const placeholders: Array<{ offset: number, length: number, defaultValue: string, choices?: string[], placeholderNumber: number }> = [];
+		let placeholderNumber = 1;
+		change.linkedEditGroups.forEach((leg) => {
+			leg.positions.forEach((pos) => {
+				const defaultValue = documentText.substr(pos.offset, leg.length);
+				const choices = leg.suggestions ? leg.suggestions.map((s) => s.value) : null;
+				placeholders.push({ offset: pos.offset, length: leg.length, defaultValue, choices, placeholderNumber });
+			});
+			placeholderNumber++;
+		});
+
+		// Ensure they're in offset order so the next maths works!
+		placeholders.sort((p1, p2) => p1.offset - p2.offset);
+
+		const snippet = new vs.SnippetString();
+		const firstPlaceholder = placeholders[0];
+		const lastPlaceholder = placeholders[placeholders.length - 1];
+		const startPos = firstPlaceholder.offset;
+		const endPos = lastPlaceholder.offset + lastPlaceholder.length;
+		let currentPos = startPos;
+		placeholders.forEach((p) => {
+			// Add the text from where we last were up to current placeholder.
+			if (currentPos !== p.offset)
+				snippet.appendText(documentText.substring(currentPos, p.offset));
+			// Add the choices / placeholder.
+			// Uncomment for https://github.com/Dart-Code/Dart-Code/issues/569 when there's an API we can use
+			if (p.choices && p.choices.length > 1)
+				snippet.appendText("").value += "${" + p.placeholderNumber + "|" + p.choices.map((c) => this.snippetStringEscape(c)).join(",") + "|}";
+			else
+				snippet.appendPlaceholder(p.defaultValue, p.placeholderNumber);
+			currentPos = p.offset + p.length;
+		});
+
+		// Replace the document.
+		await editor.insertSnippet(snippet, new vs.Range(document.positionAt(startPos), document.positionAt(endPos)));
 	}
 
 	private snippetStringEscape(value: string): string {
