03a97be6f3c83910048ec767dbef6a3a11659463
Danny Tuppeny <danny@tuppeny.com>
2018-03-29 14:58:14 +0100

    Extract cleanDartDoc method

--- src/providers/dart_hover_provider.ts
+++ src/providers/dart_hover_provider.ts
@@ -3,6 +3,7 @@ import { Analyzer } from "../analysis/analyzer";
 import * as as from "../analysis/analysis_server_types";
 import { logError } from "../utils";
 import { vsCodeVersion } from "../config";
+import { cleanDartdoc } from "../dartdocs";
 
 export class DartHoverProvider implements HoverProvider {
 	private analyzer: Analyzer;
@@ -56,33 +57,12 @@ export class DartHoverProvider implements HoverProvider {
 		if (elementDescription) displayString += (hover.isDeprecated ? "(deprecated) " : "") + `${elementDescription}\n`;
 		if (propagatedType) displayString += `propogated type: ${propagatedType.trim()}`;
 
-		let documentation = DartHoverProvider.cleanDartdoc(dartdoc);
+		let documentation = cleanDartdoc(dartdoc);
 		if (containingLibraryName) documentation = `_${containingLibraryName}_\r\n\r\n` + (documentation != null ? documentation : "");
 
 		return {
 			displayString: displayString.trim(),
 			documentation,
 		};
-	}
-
-	private static cleanDartdoc(doc: string): string {
-		if (!doc)
-			return null;
-
-		// Clean up some dart.core dartdoc.
-		const index = doc.indexOf("## Other resources");
-		if (index !== -1)
-			doc = doc.substring(0, index);
-
-		// Remove colons from old-style references like [:foo:].
-		doc = doc.replace(/\[:\S+:\]/g, (match) => `[${match.substring(2, match.length - 2)}]`);
-
-		// Change any links without hyperlinks to just code syntax.
-		// That is, anything in [squares] that isn't a [link](http://blah).
-		// Note: To ensure we get things at the end, we need to match "not a paren or end of string"
-		// and we need to put that character back in since the regex consumed it.
-		doc = doc.replace(/\[(\S+)\]([^(]|$)/g, (match, one, two) => `\`${one}\`${two}`);
-
-		return doc;
 	}
 }
