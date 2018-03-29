d7b1f8887e1d10fcb63835e5fa6ed74237dddaad
Danny Tuppeny <danny@tuppeny.com>
2018-03-29 15:35:48 +0100

    Replace material icon markup in dartdocs with images
    Fixes #638.

--- src/dartdocs.ts
+++ src/dartdocs.ts
@@ -1,3 +1,16 @@
+import { escapeRegExp } from "./utils";
+
+const darkIconUrlFormat = "https://storage.googleapis.com/material-icons/external-assets/v4/icons/svg/ic_$1_white_36px.svg";
+const lightIconUrlFormat = "https://storage.googleapis.com/material-icons/external-assets/v4/icons/svg/ic_$1_black_36px.svg";
+const iconRegex = new RegExp(
+	escapeRegExp('<p><i class="material-icons md-36">')
+	+ "([\\w\\s_]+)"
+	+ escapeRegExp('</i> &#x2014; material icon named "')
+	+ "([\\w\\s_]+)"
+	+ escapeRegExp('".</p>'),
+	"gi",
+);
+
 export function cleanDartdoc(doc: string): string {
 	if (!doc)
 		return null;
@@ -15,6 +28,9 @@ export function cleanDartdoc(doc: string): string {
 	// Note: To ensure we get things at the end, we need to match "not a paren or end of string"
 	// and we need to put that character back in since the regex consumed it.
 	doc = doc.replace(/\[(\S+)\]([^(]|$)/g, (match, one, two) => `\`${one}\`${two}`);
+
+	// TODO: Use light/dark theme as appropriate.
+	doc = doc.replace(iconRegex, `![$1](${darkIconUrlFormat}|width=100,height=100)`);
 
 	return doc;
 }
