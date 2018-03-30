a5e7163f1225cd2b620f59a797e0f75bd2c43b57
Danny Tuppeny <danny@tuppeny.com>
2018-03-30 10:52:25 +0100

    Extract update check and use async/await

---
+++ src/sdk/update_check.ts
@@ -0,0 +1,21 @@
+import { window } from "vscode";
+import { config } from "../config";
+import { Sdks, openInBrowser, logError, getLatestSdkVersion, ProjectType, versionIsAtLeast, DART_DOWNLOAD_URL } from "../utils";
+
+export async function checkForSdkUpdates(sdks: Sdks, dartSdkVersion: string): Promise<void> {
+	if (!config.checkForSdkUpdates || sdks.projectType !== ProjectType.Dart)
+		return;
+
+	try {
+		const version = await getLatestSdkVersion();
+		if (versionIsAtLeast(dartSdkVersion, version))
+			return;
+
+		const message = `Version ${version} of the Dart SDK is available (you have ${dartSdkVersion}). Some features of Dart Code may not work correctly with an old SDK.`;
+		if (await window.showWarningMessage(message, "Go to Dart Downloads"))
+			openInBrowser(DART_DOWNLOAD_URL);
+
+	} catch (e) {
+		logError(e);
+	}
+}
