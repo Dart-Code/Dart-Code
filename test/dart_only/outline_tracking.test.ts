750dd1bebd6cae8251ee1bf722ce75f240598c72
Danny Tuppeny <danny@tuppeny.com>
2018-04-03 17:05:42 +0100

    Enable one file tracking test
    We can't enable the other because we can't tell when a document is (really) closed. We can handle the open one by opening a specific file used only by this test, so it's known to not be "open" (according to VS Code) when the test starts.

--- test/dart_only/outline_tracking.test.ts
+++ test/dart_only/outline_tracking.test.ts
@@ -1,20 +1,26 @@
 import * as assert from "assert";
+import * as path from "path";
 import * as vs from "vscode";
-import { ext, activate, emptyFile, closeAllOpenFiles, waitFor, closeFile, everythingFile } from "../helpers";
+import { ext, activate, closeAllOpenFiles, waitFor, closeFile, everythingFile, helloWorldFolder } from "../helpers";
 import { OpenFileTracker } from "../../src/analysis/open_file_tracker";
 
-describe.skip("file tracker", () => {
+export const outlineTrackingFile = vs.Uri.file(path.join(helloWorldFolder.fsPath, "lib/outline_tracking/empty.dart"));
+
+describe("file tracker", () => {
 	before(() => activate());
 	it("has a tracked outline when a file is opened", async () => {
 		await closeAllOpenFiles();
-		await waitFor(() => !OpenFileTracker.getOutlineFor(emptyFile), "Outline was already present");
-		await vs.workspace.openTextDocument(emptyFile);
-		await waitFor(() => !!OpenFileTracker.getOutlineFor(emptyFile));
+		await waitFor(() => !OpenFileTracker.getOutlineFor(outlineTrackingFile), "Outline was already present");
+		await vs.workspace.openTextDocument(outlineTrackingFile);
+		await waitFor(() => !!OpenFileTracker.getOutlineFor(outlineTrackingFile), "Outline was not removed");
 	});
-	it("has no tracked outline when a file is closed", async () => {
-		const doc = await vs.workspace.openTextDocument(emptyFile);
-		await waitFor(() => !!OpenFileTracker.getOutlineFor(emptyFile), "Outline was never present");
-		await closeFile(emptyFile);
-		await waitFor(() => !OpenFileTracker.getOutlineFor(emptyFile));
+	// Skipped because we can't clean up until Code tells us when the editor is closed, which could be
+	// up to three minutes :(
+	// https://github.com/Microsoft/vscode/issues/15178
+	it.skip("has no tracked outline when a file is closed", async () => {
+		const doc = await vs.workspace.openTextDocument(outlineTrackingFile);
+		await waitFor(() => !!OpenFileTracker.getOutlineFor(outlineTrackingFile), "Outline was never present");
+		await closeFile(outlineTrackingFile);
+		await waitFor(() => !OpenFileTracker.getOutlineFor(outlineTrackingFile), "Outline was not added");
 	});
 });
