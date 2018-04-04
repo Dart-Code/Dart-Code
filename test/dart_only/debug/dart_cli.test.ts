6f5e73d1aa13a0e910da240da1c91b6076f329e8
Danny Tuppeny <danny@tuppeny.com>
2018-04-04 14:36:46 +0100

    Add some additional debugger tests
    See #785.

--- test/dart_only/debug/dart_cli.test.ts
+++ test/dart_only/debug/dart_cli.test.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import * as fs from "fs";
 import * as vs from "vscode";
 import { DebugClient } from "vscode-debugadapter-testsupport";
-import { activate, ext, helloWorldMainFile } from "../../helpers";
+import { activate, ext, helloWorldMainFile, helloWorldBrokenFile } from "../../helpers";
 import { FlutterLaunchRequestArguments } from "../../../src/debug/utils";
 
 describe("dart cli debugger", () => {
@@ -13,21 +13,55 @@ describe("dart cli debugger", () => {
 	beforeEach(() => dc.start());
 	afterEach(() => dc.stop());
 
-	it("runs a Dart script to completion", async () => {
-		const debugConfig = await ext.exports.debugProvider.resolveDebugConfiguration(
+	async function configFor(script: vs.Uri): Promise<vs.DebugConfiguration> {
+		return await ext.exports.debugProvider.resolveDebugConfiguration(
 			vs.workspace.workspaceFolders[0],
 			{
 				name: "Dart & Flutter",
-				program: helloWorldMainFile.fsPath,
+				program: script.fsPath,
 				request: "launch",
 				type: "dart",
 			},
 		);
+	}
 
+	it("runs a Dart script to completion", async () => {
+		const config = await configFor(helloWorldMainFile);
 		await Promise.all([
 			dc.configurationSequence(),
-			dc.launch(debugConfig),
+			dc.launch(config),
 			dc.waitForEvent("terminated"),
+		]);
+	});
+
+	it("receives the expected output from a Dart script", async () => {
+		const config = await configFor(helloWorldMainFile);
+		await Promise.all([
+			dc.configurationSequence(),
+			dc.launch(config),
+			dc.assertOutput("stdout", "Hello, world!"),
+			dc.waitForEvent("terminated"),
+		]);
+	});
+
+	// TODO: Figure out why this doesn't work...
+	it.skip("receives stdout for a broken script", async () => {
+		const config = await configFor(helloWorldBrokenFile);
+		await Promise.all([
+			dc.configurationSequence(),
+			dc.launch(config),
+			dc.assertOutput("stderr", "something..."),
+			dc.waitForEvent("terminated"),
+		]);
+	});
+
+	it("stops at a breakpoint", async () => {
+		const config = await configFor(helloWorldMainFile);
+		await Promise.all([
+			dc.hitBreakpoint(config, {
+				line: 2,
+				path: helloWorldMainFile.fsPath,
+			}),
 		]);
 	});
 });
