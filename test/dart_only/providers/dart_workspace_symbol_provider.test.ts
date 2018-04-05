4b0830610888968e30c0a486c954a5196b875d1c
Danny Tuppeny <danny@tuppeny.com>
2018-04-05 14:30:41 +0100

    Fix a bunch of typos in tests

--- test/dart_only/providers/dart_workspace_symbol_provider.test.ts
+++ test/dart_only/providers/dart_workspace_symbol_provider.test.ts
@@ -21,12 +21,12 @@ describe("workspace_symbol_provider", () => {
 
 		// Results in legacy version are kinda junk (these are apparently missing!) so only check in v2.
 		if (ext.exports.analyzerCapabilities.isDart2) {
-			ensureSymbol(symbols, "MyClass.myNumField", vs.SymbolKind.Field, "lib${path.sep}everything.dart", everythingFile);
-			ensureSymbol(symbols, "MyClass.myNumGetter", vs.SymbolKind.Property, "lib${path.sep}everything.dart", everythingFile);
-			ensureSymbol(symbols, "MyClass.myNumSetter", vs.SymbolKind.Property, "lib${path.sep}everything.dart", everythingFile);
-			ensureSymbol(symbols, "MyClass.myNamed()", vs.SymbolKind.Constructor, "lib${path.sep}everything.dart", everythingFile);
-			ensureSymbol(symbols, "MyClass.myVoidReturningMethod()", vs.SymbolKind.Method, "lib${path.sep}everything.dart", everythingFile);
-			ensureSymbol(symbols, "MyClass.myStringReturningMethod()", vs.SymbolKind.Method, "lib${path.sep}everything.dart", everythingFile);
+			ensureSymbol(symbols, "MyClass.myNumField", vs.SymbolKind.Field, `lib${path.sep}everything.dart`, everythingFile);
+			ensureSymbol(symbols, "MyClass.myNumGetter", vs.SymbolKind.Property, `lib${path.sep}everything.dart`, everythingFile);
+			ensureSymbol(symbols, "MyClass.myNumSetter", vs.SymbolKind.Property, `lib${path.sep}everything.dart`, everythingFile);
+			ensureSymbol(symbols, "MyClass.myNamed()", vs.SymbolKind.Constructor, `lib${path.sep}everything.dart`, everythingFile);
+			ensureSymbol(symbols, "MyClass.myVoidReturningMethod()", vs.SymbolKind.Method, `lib${path.sep}everything.dart`, everythingFile);
+			ensureSymbol(symbols, "MyClass.myStringReturningMethod()", vs.SymbolKind.Method, `lib${path.sep}everything.dart`, everythingFile);
 			assert.equal(symbols.length, 7);
 		}
 	});
