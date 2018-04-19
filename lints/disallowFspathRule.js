49c9b0463c8000ee307e6873a8ad977f18f223fe
Danny Tuppeny <danny@tuppeny.com>
2018-04-19 12:26:50 +0100

    Fix typo and add TODO

--- lints/disallowFspathRule.js
+++ lints/disallowFspathRule.js
@@ -9,10 +9,11 @@ class Rule extends Lint.Rules.AbstractRule {
 		return this.applyWithWalker(new NoFsPathWalker(sourceFile, this.getOptions()));
 	}
 }
-Rule.FAILURE_STRING = "Do not use Uri.fsPath (or fsPath(document.uri)), use fsPath() instead";
+Rule.FAILURE_STRING = "Do not use Uri.fsPath or TextDocument.fileName because they lowercase Windows drive letters causing issues with interop with other tools. Use fsPath(uri) instead.";
 
 class NoFsPathWalker extends Lint.RuleWalker {
 	visitPropertyAccessExpression(node) {
+		// TODO: Figure out how to get parent type to avoid false positives.
 		if (node.name.text === "fsPath" || node.name.text === "fileName") {
 			this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
 		}
