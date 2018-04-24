7b628728d9fd23522d2ad3213378b69adad4b01c
Danny Tuppeny <danny@tuppeny.com>
2018-04-24 10:38:28 +0100

    Remove workaround for multi-cursor edits
    Previously we didn't have the offset and has to use document.offsetAt, but this was incorrect for multi-cursor edits because the offsets were affected by changes earlier in the doc.
    
    https://github.com/Microsoft/vscode/issues/10047 added the original offset to the change object, so now we're able to use this as usual, rather than re-transmitting the entire document when the user does a multi-cursor edit!

--- src/analysis/file_change_handler.ts
+++ src/analysis/file_change_handler.ts
@@ -41,34 +41,14 @@ export class FileChangeHandler implements vs.Disposable {
 		if (e.contentChanges.length === 0) // This event fires for metadata changes (dirty?) so don't need to notify AS then.
 			return;
 
-		// TODO: Fix this...
-		// HACK: e.document.offsetAt appears to return the wrong offset when there are
-		// multiple edits (since it uses the current document state which can include
-		// earlier edits, offsetting the values!)
-		//   See https://github.com/Microsoft/vscode/issues/10047
-		//
-		// As a workaround, we just send the full contents if there was more than one edit.
+		const files: { [key: string]: as.ChangeContentOverlay } = {};
 
-		if (e.contentChanges.length === 1) {
-			const files: { [key: string]: as.ChangeContentOverlay } = {};
+		files[fsPath(e.document.uri)] = {
+			edits: e.contentChanges.map((c) => this.convertChange(e.document, c)),
+			type: "change",
+		};
 
-			files[fsPath(e.document.uri)] = {
-				edits: e.contentChanges.map((c) => this.convertChange(e.document, c)),
-				type: "change",
-			};
-
-			this.analyzer.analysisUpdateContent({ files });
-		} else {
-			// TODO: Remove this block when the bug is fixed (or we figure out it's not a bug).
-			const files: { [key: string]: as.AddContentOverlay } = {};
-
-			files[fsPath(e.document.uri)] = {
-				content: e.document.getText(),
-				type: "add",
-			};
-
-			this.analyzer.analysisUpdateContent({ files });
-		}
+		this.analyzer.analysisUpdateContent({ files });
 	}
 
 	public onDidCloseTextDocument(document: vs.TextDocument) {
@@ -88,7 +68,7 @@ export class FileChangeHandler implements vs.Disposable {
 		return {
 			id: "",
 			length: change.rangeLength,
-			offset: document.offsetAt(change.range.start),
+			offset: change.rangeOffset,
 			replacement: change.text,
 		};
 	}
