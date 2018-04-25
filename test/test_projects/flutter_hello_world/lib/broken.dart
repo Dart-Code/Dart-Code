6d92f1c5c9e95afff3c8a5831e7b68736b1dc4e7
Danny Tuppeny <danny@tuppeny.com>
2018-04-25 15:13:17 +0100

    Improve tests
    Previous scripts didn't run properly, so these are more like the `flutter create` ones.

--- test/test_projects/flutter_hello_world/lib/broken.dart
+++ test/test_projects/flutter_hello_world/lib/broken.dart
@@ -1,10 +1,21 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MyBrokenHome());
+void main() => runApp(new MyBrokenApp());
+
+class MyBrokenApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(primarySwatch: Colors.blue),
+      home: new MyBrokenHomePage(),
+    );
+  }
 }
 
-class MyBrokenHome extends StatelessWidget {
+class MyBrokenHomePage extends StatelessWidget {
+  MyBrokenHomePage({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     throw new Exception("Oops");
