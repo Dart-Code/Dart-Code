a3cdc578ebce152c5fe02a92a8da63fa62c95344
Danny Tuppeny <danny@tuppeny.com>
2018-05-06 21:35:40 +0100

    Missing changes for previous test fixes

--- test/test_projects/hello_world/bin/main.dart
+++ test/test_projects/hello_world/bin/main.dart
@@ -1,6 +1,14 @@
 main() {
   var s = "Hello!";
   var l = [0, 1];
-  var m = {"l": l, "s": s};
+  var m = {
+    "l": l,
+    "s": s,
+    new DateTime.now(): "today",
+    new DateTime.now().add(const Duration(days: 1)): "tomorrow",
+    true: true,
+    1: "one",
+    1.1: "one-point-one",
+  };
   print("Hello, world!"); // BREAKPOINT1
 }
