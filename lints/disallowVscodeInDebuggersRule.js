a95ab9cfe96fddd12999aa7cb59e51659d667108
Danny Tuppeny <danny@tuppeny.com>
2018-05-08 15:18:14 +0100

    Add a lint to forbid vscode import into DAs
    Fixes #819.

---
+++ lints/disallowVscodeInDebuggersRule.js
@@ -0,0 +1,27 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+
+const Lint = require("tslint");
+// These files aren't part of the debug adapters and should probably be separated
+// into another folder at some point.
+const excludedPaths = ["src/debug/flutter_run.ts", "src/debug/flutter_test.ts"]
+
+class Rule extends Lint.Rules.AbstractRule {
+	apply(sourceFile) {
+		if (sourceFile.fileName.indexOf("src/debug/") === 0 && excludedPaths.indexOf(sourceFile.fileName) === -1) {
+			return this.applyWithWalker(new NoVsCodeInDebuggers(sourceFile, this.getOptions()));
+		}
+	}
+}
+Rule.FAILURE_STRING = "Do not import vscode into debug adapters as they may be run in a separate process to VS Code.";
+
+class NoVsCodeInDebuggers extends Lint.RuleWalker {
+	visitImportDeclaration(node) {
+		if (node.moduleSpecifier.text === "vscode") {
+			this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+		}
+	}
+}
+
+exports.Rule = Rule;
