97c5d1d131f099ba9e713971606f1f4ab76864be
Danny Tuppeny <danny@tuppeny.com>
2018-05-10 14:47:39 +0100

    Add a safety check that we never call launch for attach
    This avoids the issue we had previously, where our attach tests were launching processes and using them!

--- test/debug_client.ts
+++ test/debug_client.ts
@@ -1,4 +1,10 @@
+import { DebugProtocol } from "vscode-debugprotocol";
 import { DebugClient } from "./debug_client_ms";
 
 export class DartDebugClient extends DebugClient {
+	public launch(launchArgs: any): Promise<DebugProtocol.LaunchResponse> {
+		if (launchArgs.request === "attach")
+			throw new Error("Cannot call launch with an attach config");
+		return super.launch(launchArgs);
+	}
 }
