c301f6654edda3549549ca4f9716a4f88d8d06fc
Danny Tuppeny <danny@tuppeny.com>
2018-05-14 15:31:30 +0100

    Update test for changes in everything.dart

--- test/dart_only/providers/dart_workspace_symbol_provider.test.ts
+++ test/dart_only/providers/dart_workspace_symbol_provider.test.ts
@@ -13,20 +13,22 @@ describe("workspace_symbol_provider", () => {
 		assert.equal(symbols.length, 0);
 	});
 
-	it("includes items from 'everything.dart'", async () => {
+	it("includes items from 'everything.dart'", async function () {
+		// Results in legacy version are kinda junk.
+		if (!ext.exports.analyzerCapabilities.isDart2)
+			this.skip();
+
 		const symbols = await getWorkspaceSymbols("my");
 
 		ensureSymbol(symbols, "MyClass", vs.SymbolKind.Class, `lib${path.sep}everything.dart`, everythingFile);
-
-		// Results in legacy version are kinda junk (these are apparently missing!) so only check in v2.
-		if (ext.exports.analyzerCapabilities.isDart2) {
-			ensureSymbol(symbols, "MyClass.myNumField", vs.SymbolKind.Field, `lib${path.sep}everything.dart`, everythingFile);
-			ensureSymbol(symbols, "MyClass.myNumGetter", vs.SymbolKind.Property, `lib${path.sep}everything.dart`, everythingFile);
-			ensureSymbol(symbols, "MyClass.myNumSetter", vs.SymbolKind.Property, `lib${path.sep}everything.dart`, everythingFile);
-			ensureSymbol(symbols, "MyClass.myNamed()", vs.SymbolKind.Constructor, `lib${path.sep}everything.dart`, everythingFile);
-			ensureSymbol(symbols, "MyClass.myVoidReturningMethod()", vs.SymbolKind.Method, `lib${path.sep}everything.dart`, everythingFile);
-			ensureSymbol(symbols, "MyClass.myStringReturningMethod()", vs.SymbolKind.Method, `lib${path.sep}everything.dart`, everythingFile);
-			assert.equal(symbols.length, 7);
-		}
+		ensureSymbol(symbols, "MyClass.myNumField", vs.SymbolKind.Field, `lib${path.sep}everything.dart`, everythingFile);
+		ensureSymbol(symbols, "MyClass.myHttpClient", vs.SymbolKind.Field, `lib${path.sep}everything.dart`, everythingFile);
+		ensureSymbol(symbols, "MyClass.myFutureString", vs.SymbolKind.Field, `lib${path.sep}everything.dart`, everythingFile);
+		ensureSymbol(symbols, "MyClass.myNumGetter", vs.SymbolKind.Property, `lib${path.sep}everything.dart`, everythingFile);
+		ensureSymbol(symbols, "MyClass.myNumSetter", vs.SymbolKind.Property, `lib${path.sep}everything.dart`, everythingFile);
+		ensureSymbol(symbols, "MyClass.myNamed()", vs.SymbolKind.Constructor, `lib${path.sep}everything.dart`, everythingFile);
+		ensureSymbol(symbols, "MyClass.myVoidReturningMethod()", vs.SymbolKind.Method, `lib${path.sep}everything.dart`, everythingFile);
+		ensureSymbol(symbols, "MyClass.myStringReturningMethod()", vs.SymbolKind.Method, `lib${path.sep}everything.dart`, everythingFile);
+		assert.equal(symbols.length, 9);
 	});
 });
