9ecc79a6e8ccc12b03290dc9c4b5c44751bf61b5
Danny Tuppeny <danny@tuppeny.com>
2018-05-15 10:42:51 +0100

    Track pending resumes so we don't send too many
    This feels a bit dirty, but the current code allowed sending multiple resumes, eg. if we got an IsolateRunnable event at startup right after we sent (but had not had a response to) a Resume. This code currently just silently drops resumes if one is in progress (or the thread is not paused) which feels bad, but better than sending them and potentially unpausing later (when unexpected).
    
    Probably there's a better way to do it, and we should consider a bigger refactor of some of this once the test coverage is better.
    
    Fixes #902. Probably. Maybe. Hopefully.

--- test/debug_client.ts
+++ test/debug_client.ts
@@ -1,8 +1,9 @@
+import * as assert from "assert";
 import { DebugProtocol } from "vscode-debugprotocol";
 import { DebugClient } from "./debug_client_ms";
 
 export class DartDebugClient extends DebugClient {
-	public async launch(launchArgs: any): Promise<DebugProtocol.LaunchResponse> {
+	public async launch(launchArgs: any): Promise<void> {
 		// We override the base method to swap for attachRequest when required, so that
 		// all the existing methods that provide useful functionality but assume launching
 		// (for ex. hitBreakpoint) can be used in attach tests.
@@ -11,9 +12,19 @@ export class DartDebugClient extends DebugClient {
 			this._supportsConfigurationDoneRequest = true;
 		}
 		if (launchArgs.request === "attach") {
-			return this.attachRequest(launchArgs);
+			// Attach will be paused by default and issue a step when we connect; but our tests
+			// generally assume we will automatically resume.
+			await this.attachRequest(launchArgs);
+			await this.waitForEvent("stopped");
+			await this.resume();
 		} else {
-			return this.launchRequest(launchArgs);
+			await this.launchRequest(launchArgs);
 		}
+	}
+
+	public async resume(): Promise<DebugProtocol.ContinueResponse> {
+		const threads = await this.threadsRequest();
+		assert.equal(threads.body.threads.length, 1);
+		return this.continueRequest({ threadId: threads.body.threads[0].id });
 	}
 }
