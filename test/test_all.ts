40dc15f2962825f4164c1bea18006161c9ac84b0
Danny Tuppeny <danny@tuppeny.com>
2018-05-16 11:01:38 +0100

    Increase test timeouts
    Looks like we have failures caused by termination which are not hung tests. We should investigate performance, but we don't want to abort runs because they're a little slow (some of them are restoring packages, using debuggers, etc.).

--- test/test_all.ts
+++ test/test_all.ts
@@ -16,8 +16,8 @@ function color(col: number, message: string) {
 	return "\u001b[" + col + "m" + message + "\u001b[0m";
 }
 
-// Set timeout (Travis kills us at 10 min without output).
-const timeoutInMilliseconds = 1000 * 60 * 5;
+// Set timeout at 15 mins (Travis kills us at 10 min without output but we write a warning at half time).
+const timeoutInMilliseconds = 1000 * 60 * 15;
 function runNode(cwd: string, args: string[], env: any): Promise<number> {
 	return new Promise<number>((resolve, reject) => {
 		let timerWarn: NodeJS.Timer;
