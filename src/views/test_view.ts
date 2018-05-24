091fb8a5f8fc149fa898f932a167043220d966a1
Danny Tuppeny <danny@tuppeny.com>
2018-05-24 13:05:12 +0100

    Move test protocol classes to their own file

--- src/views/test_view.ts
+++ src/views/test_view.ts
@@ -1,4 +1,5 @@
 import * as vs from "vscode";
+import { AllSuitesNotification, DoneNotification, ErrorNotification, Group, GroupNotification, PrintNotification, StartNotification, Suite, SuiteNotification, Test, TestDoneNotification, TestStartNotification } from "./test_protocol";
 
 const tick = "✓";
 const cross = "✖";
@@ -114,82 +115,4 @@ export class TestResultsProvider implements vs.Disposable, vs.TreeDataProvider<o
 		if (evt.stackTrace)
 			console.error(evt.stackTrace);
 	}
-}
-
-export interface Notification {
-	type: string;
-	time: number;
-}
-
-export interface StartNotification extends Notification {
-	protocolVersion: string;
-	runnerVersion?: string;
-}
-
-export interface AllSuitesNotification extends Notification {
-	count: number;
-}
-
-export interface SuiteNotification extends Notification {
-	suite: Suite;
-}
-
-export interface Suite {
-	id: number;
-	platform: string;
-	path: string;
-}
-
-export interface TestNotification extends Notification {
-	test: Test;
-}
-
-export interface Item {
-	id: number;
-	name?: string;
-	suiteID: number;
-	line?: number;
-	column?: number;
-	url?: string;
-}
-
-export interface Test extends Item {
-	groupIDs: number[];
-}
-
-export interface GroupNotification extends Notification {
-	group: Group;
-}
-
-export interface Group extends Item {
-	parentID?: number;
-	testCount: number;
-}
-
-export interface TestStartNotification extends Notification {
-	test: Test;
-}
-
-export interface TestDoneNotification extends Notification {
-	testID: number;
-	result: "success" | "failure" | "error";
-	skipped: boolean;
-	hidden: boolean;
-}
-
-export interface DoneNotification extends Notification {
-	success: boolean;
-}
-
-export interface PrintNotification extends Notification {
-	testID: number;
-	messageType: string;
-	message: string;
-}
-
-export interface ErrorNotification extends Notification {
-	testID: number;
-	error: string;
-	stackTrace: string;
-	isFailure: boolean;
 }
