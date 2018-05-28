c08e72c48fe5857772fd60f171dccb87dae296fe
Danny Tuppeny <danny@tuppeny.com>
2018-05-28 14:59:10 +0100

    Handle keeping results across multiple files
    flutter run doesn't support multiple suites with --start-paused so we staash results by the suite path so you can run multiple. This code somewhat allows for multiple-suites within a "suite" just in case we somehow end up with many suites back from a single run (but if this becomes normal, we may wish to adjust how it displays).

--- src/views/test_view.ts
+++ src/views/test_view.ts
@@ -1,15 +1,14 @@
+import * as _ from "lodash";
 import * as path from "path";
 import * as vs from "vscode";
 import { extensionPath } from "../extension";
-import { DoneNotification, ErrorNotification, Group, GroupNotification, PrintNotification, StartNotification, Suite, SuiteNotification, Test, TestDoneNotification, TestStartNotification } from "./test_protocol";
+import { DoneNotification, ErrorNotification, Group, GroupNotification, PrintNotification, Suite, SuiteNotification, Test, TestDoneNotification, TestStartNotification } from "./test_protocol";
 
 const DART_TEST_SUITE_NODE = "dart-code:testSuiteNode";
 const DART_TEST_GROUP_NODE = "dart-code:testGroupNode";
 const DART_TEST_TEST_NODE = "dart-code:testTestNode";
 
-const suites: SuiteTreeItem[] = [];
-const groups: GroupTreeItem[] = [];
-const tests: TestTreeItem[] = [];
+const suites: { [key: string]: SuiteData } = {};
 
 export class TestResultsProvider implements vs.Disposable, vs.TreeDataProvider<object> {
 	private disposables: vs.Disposable[] = [];
@@ -19,7 +18,7 @@ export class TestResultsProvider implements vs.Disposable, vs.TreeDataProvider<o
 	constructor() {
 		this.disposables.push(vs.debug.onDidReceiveDebugSessionCustomEvent((e) => {
 			if (e.event === "dart.testRunNotification") {
-				this.handleNotification(e.body);
+				this.handleNotification(e.body.suitePath, e.body.notification);
 			}
 		}));
 	}
@@ -30,14 +29,19 @@ export class TestResultsProvider implements vs.Disposable, vs.TreeDataProvider<o
 
 	public getChildren(element?: vs.TreeItem): vs.ProviderResult<vs.TreeItem[]> {
 		if (!element) {
-			return suites;
+			return _.flatMap(Object.keys(suites).map((k) => suites[k].suites));
 		} else if (element instanceof SuiteTreeItem || element instanceof GroupTreeItem) {
 			// If we got a Suite and it has only a single phantom child group, then just bounce over it.
 			// (but still include its tests so we can see loading...)
 			if (element instanceof SuiteTreeItem && element.groups.length === 1 && !element.groups[0].group.name)
-				return [].concat(element.tests.filter((t) => !t.hidden)).concat(element.groups[0].groups).concat(element.groups[0].tests);
+				return []
+					.concat(element.tests.filter((t) => !t.hidden))
+					.concat(element.groups[0].groups)
+					.concat(element.groups[0].tests);
 			else
-				return [].concat(element.groups).concat(element.tests);
+				return []
+					.concat(element.groups)
+					.concat(element.tests);
 		}
 	}
 
@@ -54,64 +58,69 @@ export class TestResultsProvider implements vs.Disposable, vs.TreeDataProvider<o
 		this.disposables.forEach((d) => d.dispose());
 	}
 
-	private handleNotification(evt: any) {
+	private handleNotification(suitePath: string, evt: any) {
+		const suite = suites[suitePath];
 		switch (evt.type) {
-			case "start":
-				this.handleStartNotification(evt as StartNotification);
-				break;
+			// case "start":
+			// 	this.handleStartNotification(evt as StartNotification);
+			// 	break;
 			// case "allSuites":
 			// 	this.handleAllSuitesNotification(evt as AllSuitesNotification);
 			// 	break;
 			case "suite":
-				this.handleSuiteNotification(evt as SuiteNotification);
+				this.handleSuiteNotification(suitePath, evt as SuiteNotification);
 				break;
 			case "testStart":
-				this.handleTestStartNotifcation(evt as TestStartNotification);
+				this.handleTestStartNotifcation(suite, evt as TestStartNotification);
 				break;
 			case "testDone":
-				this.handleTestDoneNotification(evt as TestDoneNotification);
+				this.handleTestDoneNotification(suite, evt as TestDoneNotification);
 				break;
 			case "group":
-				this.handleGroupNotification(evt as GroupNotification);
+				this.handleGroupNotification(suite, evt as GroupNotification);
 				break;
 			case "done":
-				this.handleDoneNotification(evt as DoneNotification);
+				this.handleDoneNotification(suite, evt as DoneNotification);
 				break;
 			case "print":
-				this.handlePrintNotification(evt as PrintNotification);
+				this.handlePrintNotification(suite, evt as PrintNotification);
 				break;
 			case "error":
-				this.handleErrorNotification(evt as ErrorNotification);
+				this.handleErrorNotification(suite, evt as ErrorNotification);
 				break;
 		}
 	}
 
-	private handleStartNotification(evt: StartNotification) {
-		tests.forEach((t) => t.status = TestStatus.Stale);
-		this.onDidChangeTreeDataEmitter.fire();
-	}
+	// private handleStartNotification(evt: StartNotification) {}
 
 	// private handleAllSuitesNotification(evt: AllSuitesNotification) {}
 
-	private handleSuiteNotification(evt: SuiteNotification) {
-		if (suites[evt.suite.id]) {
-			suites[evt.suite.id].suite = evt.suite;
+	private handleSuiteNotification(suitePath: string, evt: SuiteNotification) {
+		let suite = suites[evt.suite.path];
+		if (!suite) {
+			suite = new SuiteData(suitePath, [new SuiteTreeItem(evt.suite)]);
+			suites[evt.suite.path] = suite;
+		}
+		suite.tests.forEach((t) => t.status = TestStatus.Stale);
+		if (suite.suites[evt.suite.id]) {
+			suite.suites[evt.suite.id].suite = evt.suite;
 		} else {
 			const suiteNode = new SuiteTreeItem(evt.suite);
-			suites[evt.suite.id] = suiteNode;
+			suite.suites[evt.suite.id] = suiteNode;
 		}
 		this.onDidChangeTreeDataEmitter.fire();
+		// suite.suites.forEach((s) => this.onDidChangeTreeDataEmitter.fire(s));
 	}
 
-	private handleTestStartNotifcation(evt: TestStartNotification) {
-		const isExistingTest = !!tests[evt.test.id];
-		const testNode = tests[evt.test.id] || new TestTreeItem(evt.test);
+	private handleTestStartNotifcation(suite: SuiteData, evt: TestStartNotification) {
+		const isExistingTest = !!suite.tests[evt.test.id];
+		const testNode = suite.tests[evt.test.id] || new TestTreeItem(suite, evt.test);
 		let oldParent: SuiteTreeItem | GroupTreeItem;
 
-		if (!isExistingTest) {
-			tests[evt.test.id] = testNode;
+		if (!isExistingTest)
+			suite.tests[evt.test.id] = testNode;
+		else
 			oldParent = testNode.parent;
-		}
 		testNode.hidden = false;
 		testNode.status = TestStatus.Running;
 		testNode.test = evt.test;
@@ -123,16 +132,15 @@ export class TestResultsProvider implements vs.Disposable, vs.TreeDataProvider<o
 		}
 
 		// Push to new parent if required.
-		if (!isExistingTest) {
+		if (!isExistingTest)
 			testNode.parent.tests.push(testNode);
-			this.onDidChangeTreeDataEmitter.fire(testNode.parent);
-		}
-		// Always fire this, whether it was new or not, as the status has changed.
+
+		this.onDidChangeTreeDataEmitter.fire(testNode.parent);
 		this.onDidChangeTreeDataEmitter.fire(testNode);
 	}
 
-	private handleTestDoneNotification(evt: TestDoneNotification) {
-		const testNode = tests[evt.testID];
+	private handleTestDoneNotification(suite: SuiteData, evt: TestDoneNotification) {
+		const testNode = suite.tests[evt.testID];
 
 		testNode.hidden = evt.hidden;
 		if (evt.skipped) {
@@ -151,36 +159,42 @@ export class TestResultsProvider implements vs.Disposable, vs.TreeDataProvider<o
 		this.onDidChangeTreeDataEmitter.fire(testNode);
 	}
 
-	private handleGroupNotification(evt: GroupNotification) {
-		if (groups[evt.group.id]) {
-			const groupNode = groups[evt.group.id];
+	private handleGroupNotification(suite: SuiteData, evt: GroupNotification) {
+		if (suite.groups[evt.group.id]) {
+			const groupNode = suite.groups[evt.group.id];
 			groupNode.group = evt.group;
 			this.onDidChangeTreeDataEmitter.fire(groupNode);
 			// TODO: Change parent if required...
 		} else {
-			const groupNode = new GroupTreeItem(evt.group);
-			groups[evt.group.id] = groupNode;
+			const groupNode = new GroupTreeItem(suite, evt.group);
+			suite.groups[evt.group.id] = groupNode;
 			groupNode.parent.groups.push(groupNode);
 			this.onDidChangeTreeDataEmitter.fire(groupNode.parent);
 		}
 	}
 
-	private handleDoneNotification(evt: DoneNotification) {
+	private handleDoneNotification(suite: SuiteData, evt: DoneNotification) {
 		// TODO: Some notification that things are complete?
 		// TODO: Maybe a progress bar during the run?
 	}
 
-	private handlePrintNotification(evt: PrintNotification) {
+	private handlePrintNotification(suite: SuiteData, evt: PrintNotification) {
 		// TODO: Provide a better way of seeing this?
 		console.log(`${evt.message}\n`);
 	}
 
-	private handleErrorNotification(evt: ErrorNotification) {
+	private handleErrorNotification(suite: SuiteData, evt: ErrorNotification) {
 		// TODO: Provide a better way of seeing this?
 		console.error(evt.error);
 		if (evt.stackTrace)
 			console.error(evt.stackTrace);
 	}
+}
+
+class SuiteData {
+	constructor(public readonly path: string, public readonly suites: SuiteTreeItem[]) { }
+	public readonly groups: GroupTreeItem[] = [];
+	public readonly tests: TestTreeItem[] = [];
 }
 
 class SuiteTreeItem extends vs.TreeItem {
@@ -190,7 +204,7 @@ class SuiteTreeItem extends vs.TreeItem {
 	constructor(public suite: Suite) {
 		super(vs.Uri.file(suite.path), vs.TreeItemCollapsibleState.Expanded);
 		this.contextValue = DART_TEST_SUITE_NODE;
-		this.id = `suite_${this.suite.id}`;
+		this.id = `suite_${this.suite.path}_${this.suite.id}`;
 	}
 }
 
@@ -198,31 +212,31 @@ class GroupTreeItem extends vs.TreeItem {
 	public readonly groups: GroupTreeItem[] = [];
 	public readonly tests: TestTreeItem[] = [];
 
-	constructor(public group: Group) {
+	constructor(public suite: SuiteData, public group: Group) {
 		super(group.name, vs.TreeItemCollapsibleState.Expanded);
 		this.contextValue = DART_TEST_GROUP_NODE;
-		this.id = `group_${this.group.id}`;
+		this.id = `suite_${this.suite.path}_group_${this.group.id}`;
 	}
 
 	get parent(): SuiteTreeItem | GroupTreeItem {
 		return this.group.parentID
-			? groups[this.group.parentID]
-			: suites[this.group.suiteID];
+			? this.suite.groups[this.group.parentID]
+			: this.suite.suites[this.group.suiteID];
 	}
 }
 
 class TestTreeItem extends vs.TreeItem {
-	constructor(public test: Test, public hidden = false) {
+	constructor(public suite: SuiteData, public test: Test, public hidden = false) {
 		super(test.name, vs.TreeItemCollapsibleState.None);
-		this.id = `test_${this.test.id}`;
+		this.id = `suite_${this.suite.path}_test_${this.test.id}`;
 		// TODO: Allow re-running tests/groups/suites
 		this.contextValue = DART_TEST_TEST_NODE;
 	}
 
 	get parent(): SuiteTreeItem | GroupTreeItem {
 		return this.test.groupIDs && this.test.groupIDs.length
-			? groups[this.test.groupIDs[this.test.groupIDs.length - 1]]
-			: suites[this.test.suiteID];
+			? this.suite.groups[this.test.groupIDs[this.test.groupIDs.length - 1]]
+			: this.suite.suites[this.test.suiteID];
 	}
 
 	set status(status: TestStatus) {
