9582c314f6870d49a19d89d470b3bdde1857a9f2
Danny Tuppeny <danny@tuppeny.com>
2018-05-30 14:09:34 +0100

    Copy showCode from Flutter Outline branch

--- src/commands/edit.ts
+++ src/commands/edit.ts
@@ -18,12 +18,28 @@ export class EditCommands implements vs.Disposable {
 			vs.commands.registerCommand("_dart.organizeImports", this.organizeImports, this),
 			vs.commands.registerCommand("dart.sortMembers", this.sortMembers, this),
 			vs.commands.registerCommand("_dart.applySourceChange", this.applyEdits, this),
+			vs.commands.registerCommand("_dart.showCode", this.showCode, this),
 			vs.commands.registerCommand("dart.completeStatement", this.completeStatement, this),
 		);
 	}
 
 	private organizeImports(): Thenable<void> {
 		return this.sendEdit(this.analyzer.editOrganizeDirectives, "Organize Imports");
+	}
+
+	private showCode(editor: vs.TextEditor, displayRange: vs.Range, highlightRange: vs.Range, selectionRange?: vs.Range): void {
+		if (selectionRange)
+			editor.selection = new vs.Selection(selectionRange.start, selectionRange.end);
+
+		// Ensure the code is visible on screen.
+		editor.revealRange(displayRange, vs.TextEditorRevealType.InCenterIfOutsideViewport);
+
+		// Re-reveal the first line, to ensure it was always visible (eg. in case the main range was bigger than the screen).
+		// Using .Default means it'll do as little scrolling as possible.
+		editor.revealRange(new vs.Range(displayRange.start, displayRange.start), vs.TextEditorRevealType.Default);
+
+		// TODO: Implement highlighting
+		// See https://github.com/Microsoft/vscode/issues/45059
 	}
 
 	private sortMembers(): Thenable<void> {
