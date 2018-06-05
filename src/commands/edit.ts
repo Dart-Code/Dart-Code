66ec01d11751b8991243196af59b0355b82b7d3a
Danny Tuppeny <danny@tuppeny.com>
2018-06-05 11:09:20 +0100

    Better handling of jumpToLineColInUri args

--- src/commands/edit.ts
+++ src/commands/edit.ts
@@ -28,12 +28,17 @@ export class EditCommands implements vs.Disposable {
 		return this.sendEdit(this.analyzer.editOrganizeDirectives, "Organize Imports");
 	}
 
-	private async jumpToLineColInUri(uri: vs.Uri, lineNumber: number, columnNumber: number) {
+	private async jumpToLineColInUri(uri: vs.Uri, lineNumber?: number, columnNumber?: number) {
+		if (!uri || uri.scheme !== "file")
+			return;
+
 		const doc = await vs.workspace.openTextDocument(uri);
 		const editor = await vs.window.showTextDocument(doc);
-		const line = doc.lineAt(lineNumber);
-		const firstChar = line.range.start.translate({ characterDelta: line.firstNonWhitespaceCharacterIndex });
-		this.showCode(editor, line.range, line.range, new vs.Range(firstChar, firstChar));
+		if (lineNumber && columnNumber) {
+			const line = doc.lineAt(lineNumber);
+			const firstChar = line.range.start.translate({ characterDelta: line.firstNonWhitespaceCharacterIndex });
+			this.showCode(editor, line.range, line.range, new vs.Range(firstChar, firstChar));
+		}
 	}
 
 	private showCode(editor: vs.TextEditor, displayRange: vs.Range, highlightRange: vs.Range, selectionRange?: vs.Range): void {
