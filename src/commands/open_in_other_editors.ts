9cffdad84f983b98870edea7f32690771a9a430e
Danny Tuppeny <danny@tuppeny.com>
2018-06-18 18:28:18 +0100

    Open in AS WIP

--- src/commands/open_in_other_editors.ts
+++ src/commands/open_in_other_editors.ts
@@ -1,17 +1,49 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as vs from "vscode";
-import { safeSpawn } from "../debug/utils";
-import { fsPath } from "../utils";
+import { isMac, safeSpawn } from "../debug/utils";
+import { flutterPath } from "../sdk/utils";
+import { Sdks, fsPath } from "../utils";
+import { logError } from "../utils/log";
 
 export class OpenInOtherEditorCommands implements vs.Disposable {
 	private disposables: vs.Disposable[] = [];
 
-	constructor() {
+	constructor(private readonly sdks: Sdks) {
 
 		this.disposables.push(
+			vs.commands.registerCommand("flutter.openInAndroidStudio", this.openInAndroidStudio, this),
 			vs.commands.registerCommand("flutter.openInXcode", this.openInXcode, this),
 		);
+	}
+
+	private async openInAndroidStudio(resource: vs.Uri): Promise<void> {
+		const folder = fsPath(resource);
+		const parentFolder = path.dirname(folder);
+		const files = fs
+			.readdirSync(parentFolder)
+			.filter((item) => fs.statSync(path.join(parentFolder, item)).isFile())
+			.filter((item) => item.endsWith("_android.iml"));
+
+		if (!files || !files.length) {
+			vs.window.showErrorMessage(`Unable to find an Android .iml file in your project`);
+			return;
+		}
+
+		let androidStudioDir = await this.getAndroidStudioDir(parentFolder);
+
+		if (!androidStudioDir) {
+			vs.window.showErrorMessage(`Unable to find Android Studio`);
+			return;
+		}
+		if (isMac && androidStudioDir.endsWith("/Contents"))
+			androidStudioDir = androidStudioDir.substr(0, androidStudioDir.length - "/Contents".length);
+
+		const file = path.join(parentFolder, files[0]);
+		if (isMac)
+			safeSpawn(folder, "open", ["-a", androidStudioDir, file]);
+		else
+			safeSpawn(folder, androidStudioDir, [file]);
 	}
 
 	private async openInXcode(resource: vs.Uri): Promise<void> {
@@ -22,12 +54,36 @@ export class OpenInOtherEditorCommands implements vs.Disposable {
 			.filter((item) => item.endsWith(".xcworkspace") || item.endsWith(".xcodeproj"))
 			.sort((f1, f2) => f1.endsWith(".xcworkspace") ? 0 : 1);
 
-		if (files && files.length) {
-			const file = path.join(folder, files[0]);
-			safeSpawn(folder, "open", [file]);
-		} else {
+		if (!files || !files.length) {
 			vs.window.showErrorMessage(`Unable to find an Xcode project in your 'ios' folder`);
+			return;
 		}
+
+		const file = path.join(folder, files[0]);
+		safeSpawn(folder, "open", [file]);
+	}
+
+	private getAndroidStudioDir(folder: string): Promise<string> {
+		return new Promise((resolve, reject) => {
+			const binPath = path.join(this.sdks.flutter, flutterPath);
+			const proc = safeSpawn(folder, binPath, ["config", "--machine"]);
+			const output: string[] = [];
+			proc.stdout.on("data", (data: Buffer) => {
+				output.push(data.toString());
+			});
+			proc.on("exit", () => {
+				try {
+					if (output.length) {
+						const json = JSON.parse(output.join(""));
+						resolve(json["android-studio-dir"] as string);
+						return;
+					}
+				} catch (e) {
+					logError(e);
+				}
+				reject();
+			});
+		});
 	}
 
 	public dispose(): any {
