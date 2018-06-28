04352f01b399187a82dce01acf7fbdaa6b79ae1a
Ronnie Andrew Magatti <ronniemagatti@gmail.com>
2018-06-27 22:07:57 -0300

    Use config settings

--- src/config.ts
+++ src/config.ts
@@ -51,6 +51,7 @@ class Config {
 	public setSdkPath(value: string): Thenable<void> { return this.setConfig("sdkPath", value, ConfigurationTarget.Workspace); }
 	get sdkPaths() { return (this.getConfig<string[]>("sdkPaths") || []).map(resolvePaths); }
 	get flutterSelectDeviceWhenConnected() { return this.getConfig<boolean>("flutterSelectDeviceWhenConnected"); }
+	get flutterScreenshotPath() { return createFolderIfRequired(resolvePaths(this.getConfig<string>("flutterScreenshotPath"))); }
 
 	public setGlobalDartSdkPath(value: string): Thenable<void> { return this.setConfig("sdkPath", value, ConfigurationTarget.Global); }
 	public setGlobalFlutterSdkPath(value: string): Thenable<void> { return this.setConfig("flutterSdkPath", value, ConfigurationTarget.Global); }
