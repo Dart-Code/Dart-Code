e6c5f13b9981050992c4bd6e416cf104615e9d7f
Danny Tuppeny <danny@tuppeny.com>
2018-07-12 08:31:18 +0100

    Don't use the active editor for source actions
    Fixes #1083.

--- src/editors.ts
+++ src/editors.ts
@@ -1,8 +1,13 @@
 import * as vs from "vscode";
 
 export function hasActiveDartEditor(): boolean {
-	if (!vs.window.activeTextEditor)
-		return false;
+	return isDartEditor(vs.window.activeTextEditor);
+}
 
-	return vs.window.activeTextEditor.document.languageId === "dart";
+export function isDartEditor(editor: vs.TextEditor): boolean {
+	return editor && isDartDocument(editor.document);
+}
+
+export function isDartDocument(document: vs.TextDocument): boolean {
+	return document && document.languageId === "dart";
 }
