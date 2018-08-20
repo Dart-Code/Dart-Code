00adbc6d35dc223bd28ab3e292ccb576726b8b0b
Danny Tuppeny <danny@tuppeny.com>
2018-08-20 10:01:38 +0100

    Remove unused files

--- src/debug/flutter_test.ts
+++
@@ -1,8 +0,0 @@
-import { TestRunner } from "./test_runner";
-import { globalFlutterArgs } from "./utils";
-
-export class FlutterTest extends TestRunner {
-	constructor(flutterBinPath: string, projectFolder: string, args: string[], logFile: string, logger: (message: string) => void, maxLogLineLength: number) {
-		super(flutterBinPath, projectFolder, globalFlutterArgs.concat(["test", "--machine"]).concat(args), logFile, logger, maxLogLineLength);
-	}
-}
