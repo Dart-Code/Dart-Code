fc0f30d2d9c75f5589875dd0a9a6e9f8008b56ff
Danny Tuppeny <danny@tuppeny.com>
2018-08-27 13:04:19 +0100

    Remove tests that aren't valid now Dart is a static snippet set again

--- test/dart_only/providers/snippet_provider.test.ts
+++ test/dart_only/providers/snippet_provider.test.ts
@@ -1,15 +1,8 @@
-import { activate, ensureNoSnippet, ensureSnippet, getSnippetCompletionsAt, setTestContent } from "../../helpers";
+import { activate, ensureNoSnippet, getSnippetCompletionsAt, setTestContent } from "../../helpers";
 
 describe("snippet_provider", () => {
 
 	beforeEach("activate", () => activate());
-
-	it("returns dart items", async () => {
-		await setTestContent("mai");
-		const snippets = await getSnippetCompletionsAt("mai^");
-		ensureSnippet(snippets, "class", "class", `\`\`\`\nclass \${1:Name} {\n  $2\n}\n\`\`\``);
-		ensureSnippet(snippets, "main", "main", `\`\`\`\nmain(List<String> args) {\n  $1\n}\n\`\`\``);
-	});
 
 	it("does not return flutter items", async () => {
 		await setTestContent("stf");
--- test/flutter_only/providers/snippet_provider.test.ts
+++ test/flutter_only/providers/snippet_provider.test.ts
@@ -4,13 +4,6 @@ describe("snippet_provider", () => {
 
 	beforeEach("activate flutterEmptyFile", () => activate(flutterEmptyFile));
 
-	it("returns dart items", async () => {
-		await setTestContent("mai");
-		const snippets = await getSnippetCompletionsAt("mai^");
-		ensureSnippet(snippets, "class", "class", `\`\`\`\nclass \${1:Name} {\n  $2\n}\n\`\`\``);
-		ensureSnippet(snippets, "main", "main", `\`\`\`\nmain(List<String> args) {\n  $1\n}\n\`\`\``);
-	});
-
 	it("returns flutter items", async () => {
 		await setTestContent("stf");
 		const snippets = await getSnippetCompletionsAt("stf^");
