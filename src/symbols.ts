e9332a1f6a51b372bf901a368fcecd3bee02a8b3
Danny Tuppeny <danny@tuppeny.com>
2018-08-29 15:15:17 +0100

    Workaround for Symbol in API in VS Code Insiders
    See https://github.com/Microsoft/vscode/issues/57513

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,1 +1,4 @@
-export const internalApiSymbol = Symbol();
+// This is currently a string because a Symbol seems to have become broken in
+// VS Code. We can revert this if it gets fixed.
+// https://github.com/Microsoft/vscode/issues/57513
+export const internalApiSymbol = "private-API"; // Symbol();
