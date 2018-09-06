aaa4073c426265c404f95cfd056b7499c2fa0cdc
Danny Tuppeny <danny@tuppeny.com>
2018-09-06 14:11:21 +0100

    Revert "Skip potentially problematic test"
    This reverts commit 33d25ac06392e2729c5a4259d5c89021ace9765d.

--- test/flutter_only/debug/flutter_run.test.ts
+++ test/flutter_only/debug/flutter_run.test.ts
@@ -198,7 +198,7 @@ describe("flutter run debugger", () => {
 				? ""
 				: ` after ${numReloads} reload${numReloads === 1 ? "" : "s"}`;
 
-		it.skip("stops at a breakpoint" + reloadDescription, async function () { // tslint:disable-line:only-arrow-functions
+		it("stops at a breakpoint" + reloadDescription, async function () { // tslint:disable-line:only-arrow-functions
 			if (numReloads > 0) {
 				if (extApi.daemonCapabilities.debuggerIncorrectlyPausesOnHandledExceptions)
 					this.skip();
