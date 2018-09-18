2937054d39cc965cfb95c9d53d91e8000c5bdd42
Danny Tuppeny <danny@tuppeny.com>
2018-09-18 14:36:48 +0100

    Switch back to using a symbol
    I believe thie issue is because of https://github.com/Microsoft/vscode/issues/58388 and the previous workaround should fix this.

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,4 +1,1 @@
-// This is currently a string because a Symbol seems to have become broken in
-// VS Code. We can revert this if it gets fixed.
-// https://github.com/Microsoft/vscode/issues/57513
-export const internalApiSymbol = "private-API"; // Symbol();
+export const internalApiSymbol = Symbol();
