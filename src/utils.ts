1067a6acbceaaa5658bda9f260d136b1efbb04e4
Danny Tuppeny <danny@tuppeny.com>
2018-09-21 18:32:50 +0100

    Fix null ref error

--- src/utils.ts
+++ src/utils.ts
@@ -148,7 +148,7 @@ export function isTestFile(file: string): boolean {
 	// If we're either in a top-level test folder or the file ends with _test.dart then
 	// assume it's a test. We used to check for /test/ at any level, but sometimes people have
 	// non-test files named test (https://github.com/Dart-Code/Dart-Code/issues/1165).
-	return isInsideFolderNamed(file, "test") || file.toLowerCase().endsWith("_test.dart");
+	return isInsideFolderNamed(file, "test") || (file && file.toLowerCase().endsWith("_test.dart"));
 }
 
 export function supportsPubRunTest(folder: string, file: string): boolean {
