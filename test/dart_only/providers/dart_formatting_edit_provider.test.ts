12ca69571a4f25032969f6d5f8a456eeebc11eae
Danny Tuppeny <danny@tuppeny.com>
2018-10-04 09:48:39 +0100

    Merge formatting tests

--- test/dart_only/providers/dart_formatting_edit_provider.test.ts
+++ test/dart_only/providers/dart_formatting_edit_provider.test.ts
@@ -1,6 +1,6 @@
 import * as assert from "assert";
 import * as vs from "vscode";
-import { activate, currentDoc, currentEditor, documentEol, setTestContent } from "../../helpers";
+import { activate, currentDoc, currentEditor, documentEol, positionOf, setTestContent } from "../../helpers";
 
 describe("dart_formatting_edit_provider", () => {
 
@@ -13,9 +13,24 @@ describe("dart_formatting_edit_provider", () => {
 		await currentEditor().edit((b) => formatResult.forEach((f) => b.replace(f.range, f.newText)));
 	}
 
+	async function formatOnType(searchText: string, character: string): Promise<void> {
+		const position = positionOf(searchText);
+		const formatResult = await (vs.commands.executeCommand("vscode.executeFormatOnTypeProvider", currentDoc().uri, position, character) as Thenable<vs.TextEdit[]>);
+		assert.ok(formatResult);
+		assert.ok(formatResult.length);
+		await currentEditor().edit((b) => formatResult.forEach((f) => b.replace(f.range, f.newText)));
+	}
+
 	it("formats the document", async () => {
 		await setTestContent("   main ( ) {     }");
 		await formatDocument();
+		assert.equal(currentDoc().getText(), `main() {}${documentEol}`);
+	});
+
+	it("formats the code when typing a }", async () => {
+		// Currently we just format the whole doc on format as out formatter doesn't support ranges.
+		await setTestContent("   main ( ) { }");
+		await formatOnType("{ ^", "}");
 		assert.equal(currentDoc().getText(), `main() {}${documentEol}`);
 	});
 });
