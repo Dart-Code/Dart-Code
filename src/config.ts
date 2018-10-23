247c4c8d30f0ff64bdddb9f74ad4ab21e5ab5437
Danny Tuppeny <danny@tuppeny.com>
2018-10-23 14:59:10 +0100

    Basic task implementation for build_runner

--- src/config.ts
+++ src/config.ts
@@ -66,6 +66,7 @@ class Config {
 
 	// Preview features.
 	get previewHotReloadCoverageMarkers() { return this.getConfig<boolean>("previewHotReloadCoverageMarkers"); }
+	get previewBuildRunnerTasks() { return this.getConfig<boolean>("previewBuildRunnerTasks"); }
 	get previewToStringInDebugViews() { return this.getConfig<boolean>("previewToStringInDebugViews"); }
 
 	public for(uri?: Uri): ResourceConfig {
