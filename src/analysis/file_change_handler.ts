07d25cc697837b07104999608532091b13eb7552
Danny Tuppeny <danny@tuppeny.com>
2018-12-13 09:51:52 +0000

    Remove file path from warning message
    Fixes #1331.

--- src/analysis/file_change_handler.ts
+++ src/analysis/file_change_handler.ts
@@ -42,7 +42,7 @@ export class FileChangeHandler implements vs.Disposable {
 		const filePath = fsPath(e.document.uri);
 
 		if (config.warnWhenEditingFilesOutsideWorkspace && !this.filesWarnedAbout.has(filePath) && !util.isWithinWorkspace(filePath)) {
-			vs.window.showWarningMessage(`You are modifying a file outside of your open folders: ${filePath}`);
+			vs.window.showWarningMessage(`You are modifying a file outside of your open folders`);
 			this.filesWarnedAbout.add(filePath);
 		}
 
