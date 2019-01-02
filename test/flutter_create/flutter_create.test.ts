157363d6a26307e0b530039ddba82262bf0dc96e
Danny Tuppeny <danny@tuppeny.com>
2019-01-02 15:37:58 +0000

    Spit out content on failures to aid debuggingg Linux Travis failure

--- test/flutter_create/flutter_create.test.ts
+++ test/flutter_create/flutter_create.test.ts
@@ -12,7 +12,8 @@ describe("flutter", () => {
 		// Creating the sample may be a little slow, so allow up to 60 seconds for it.
 		await waitFor(() => fs.existsSync(mainFile), "lib/main.dart did not exist", 60000);
 		const contents = fs.readFileSync(mainFile);
-		assert.notEqual(contents.indexOf("title: 'Flutter Demo'"), -1, `Did not find "title: 'Flutter Demo'" in the sample file`);
+		if (contents.indexOf("title: 'Flutter Demo'") === -1)
+			assert.fail(`Did not find "title: 'Flutter Demo'" in the sample file:\n\n${contents}`);
 	});
 	it("created a sample project", async () => {
 		const sampleProjectFolder = fsPath(vs.workspace.workspaceFolders[1].uri);
@@ -20,6 +21,7 @@ describe("flutter", () => {
 		// Creating the sample may be a little slow, so allow up to 60 seconds for it.
 		await waitFor(() => fs.existsSync(mainFile), "lib/main.dart did not exist", 60000);
 		const contents = fs.readFileSync(mainFile);
-		assert.notEqual(contents.indexOf("title: 'Flutter Code Sample for scaffold.Scaffold'"), -1, `Did not find "title: 'Flutter Code Sample for scaffold.Scaffold'" in the sample file`);
+		if (contents.indexOf("title: 'Flutter Code Sample for scaffold.Scaffold'") === -1)
+			assert.fail(`Did not find "title: 'Flutter Code Sample for scaffold.Scaffold'" in the sample file:\n\n${contents}`);
 	});
 });
