29565ba81d85943b7d1e95afff86c849095a17b7
Danny Tuppeny <danny@tuppeny.com>
2019-01-03 11:32:16 +0000

    Revert "Never use package: uris for "self" (local package) as breakpoints don't work"
    This reverts commit d15aa34978929680979d7a7e8c0554c2e12dcb1b.

--- src/debug/package_map.ts
+++ src/debug/package_map.ts
@@ -62,11 +62,11 @@ export class PackageMap {
 			return undefined;
 	}
 
-	public convertFileToPackageUri(file: string): string | undefined {
+	public convertFileToPackageUri(file: string, allowSelf = true): string | undefined {
 		for (const name of Object.keys(this.map)) {
 			const dir = this.map[name];
 			if (isWithinPath(file, dir)) {
-				if (name === this.localPackageName)
+				if (!allowSelf && name === this.localPackageName)
 					return undefined;
 				let rest = file.substring(dir.length);
 				// package: uri should always use forward slashes.
