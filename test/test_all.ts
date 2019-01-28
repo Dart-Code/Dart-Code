5fcca475265defaf7e0c6e39d64c88c06ae9c10f
Danny Tuppeny <danny@tuppeny.com>
2019-01-28 10:19:26 +0000

    (TEMP) Run less tests

--- test/test_all.ts
+++ test/test_all.ts
@@ -149,19 +149,19 @@ async function runAllTests(): Promise<void> {
 	let runNumber = 1;
 	try {
 		await runTests("dart_only", "hello_world", dartSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
-		await runTests("multi_root", "projects.code-workspace", flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
-		await runTests("multi_project_folder", "", flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
-		await runTests("not_activated/flutter_create", "empty", flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
-		await runTests("flutter_create", "flutter_create.code-workspace", flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
-		if (flutterRoot) {
-			await runTests("flutter_repository", flutterRoot, flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
-		} else {
-			console.error("FLUTTER_ROOT/FLUTTER_PATH NOT SET, SKIPPING FLUTTER REPO TESTS");
-			exitCode = 1;
-		}
-		// This one is run last because it's the most fragile, and can bring down the following tests if it hangs in a
-		// way that doesn't get caught by the test timeout properly.
-		await runTests("flutter_only", "flutter_hello_world", flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
+		// await runTests("multi_root", "projects.code-workspace", flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
+		// await runTests("multi_project_folder", "", flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
+		// await runTests("not_activated/flutter_create", "empty", flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
+		// await runTests("flutter_create", "flutter_create.code-workspace", flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
+		// if (flutterRoot) {
+		// 	await runTests("flutter_repository", flutterRoot, flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
+		// } else {
+		// 	console.error("FLUTTER_ROOT/FLUTTER_PATH NOT SET, SKIPPING FLUTTER REPO TESTS");
+		// 	exitCode = 1;
+		// }
+		// // This one is run last because it's the most fragile, and can bring down the following tests if it hangs in a
+		// // way that doesn't get caught by the test timeout properly.
+		// await runTests("flutter_only", "flutter_hello_world", flutterSdkPath, codeVersion, `${runNumber++} of ${totalRuns}`);
 	} catch (e) {
 		exitCode = 1;
 		console.error(e);
