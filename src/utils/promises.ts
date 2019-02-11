ada68c8caa79b82c509213ac895d8168a20faad8
Danny Tuppeny <danny@tuppeny.com>
2019-02-11 16:51:48 +0000

    Ensure we always have values for waitFor

--- src/utils/promises.ts
+++ src/utils/promises.ts
@@ -1,4 +1,6 @@
 export async function waitFor<T>(action: () => T, checkEveryMilliseconds: number, tryForMilliseconds: number, token?: { isCancellationRequested: boolean }): Promise<T | undefined> {
+	checkEveryMilliseconds = checkEveryMilliseconds || 500;
+	tryForMilliseconds = tryForMilliseconds || 10000;
 	let timeRemaining = tryForMilliseconds;
 	while (timeRemaining > 0 && !(token && token.isCancellationRequested)) {
 		const res = action();
