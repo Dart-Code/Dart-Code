7822c56d8d623e03682b46a836e68e36d07053ee
Danny Tuppeny <danny@tuppeny.com>
2019-02-11 15:58:14 +0000

    Fix bug in waitFor not waiting long enough

--- src/utils/promises.ts
+++ src/utils/promises.ts
@@ -5,6 +5,6 @@ export async function waitFor<T>(action: () => T, checkEveryMilliseconds: number
 		if (res)
 			return res;
 		await new Promise((resolve) => setTimeout(resolve, checkEveryMilliseconds));
-		timeRemaining -= 20;
+		timeRemaining -= checkEveryMilliseconds;
 	}
 }
