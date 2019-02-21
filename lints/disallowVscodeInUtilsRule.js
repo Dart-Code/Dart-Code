0e07f4ecc4ad96a946a0ca95d0dba4c7d58bcc00
Danny Tuppeny <danny@tuppeny.com>
2019-02-21 15:44:13 +0000

    Fix lint class name

--- lints/disallowVscodeInUtilsRule.js
+++ lints/disallowVscodeInUtilsRule.js
@@ -7,13 +7,13 @@ const Lint = require("tslint");
 class Rule extends Lint.Rules.AbstractRule {
 	apply(sourceFile) {
 		if (sourceFile.fileName.indexOf("src/utils/") !== -1) {
-			return this.applyWithWalker(new NoVsCodeInDebuggers(sourceFile, this.getOptions()));
+			return this.applyWithWalker(new NoVsCodeInUtils(sourceFile, this.getOptions()));
 		}
 	}
 }
 Rule.FAILURE_STRING = "Do not import vscode into utils files that are not in utils/vscode as when imported into the Debug Adapters they may run in a separate process to VS Code.";
 
-class NoVsCodeInDebuggers extends Lint.RuleWalker {
+class NoVsCodeInUtils extends Lint.RuleWalker {
 	visitImportDeclaration(node) {
 		if (node.moduleSpecifier.text === "vscode") {
 			this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
