6c37e548979723d1f74524398084fb381b0d3a97
Danny Tuppeny <danny@tuppeny.com>
2019-02-21 15:46:47 +0000

    Remove incorrect code/comments

--- lints/disallowVscodeInDebuggersRule.js
+++ lints/disallowVscodeInDebuggersRule.js
@@ -4,13 +4,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 
 const Lint = require("tslint");
 
-// These files aren't part of the debug adapters and
-// TODO: should probably be separated into another folder at some point.
-const excludedPaths = ["src/debug/flutter_run.ts", "src/debug/flutter_test.ts"]
-
 class Rule extends Lint.Rules.AbstractRule {
 	apply(sourceFile) {
-		if (sourceFile.fileName.indexOf("src/debug/") !== -1 && excludedPaths.indexOf(sourceFile.fileName) === -1) {
+		if (sourceFile.fileName.indexOf("src/debug/") !== -1) {
 			return this.applyWithWalker(new NoVsCodeInDebuggers(sourceFile, this.getOptions()));
 		}
 	}
