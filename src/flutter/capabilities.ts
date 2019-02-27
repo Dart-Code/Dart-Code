798e7e39f64438397a810ce9aeaa67d08f0665c8
Danny Tuppeny <danny@tuppeny.com>
2019-02-27 13:02:05 +0000

    Tweak flutter version that has multiple samples

--- src/flutter/capabilities.ts
+++ src/flutter/capabilities.ts
@@ -12,6 +12,6 @@ export class FlutterCapabilities {
 	get supportsPidFileForMachine() { return versionIsAtLeast(this.version, "0.10.0"); }
 	get trackWidgetCreationDefault() { return versionIsAtLeast(this.version, "0.10.2-pre"); }
 	get supportsCreatingSamples() { return versionIsAtLeast(this.version, "1.0.0"); }
-	get supportsMultipleSamplesPerElement() { return versionIsAtLeast(this.version, "1.1.0"); }
+	get supportsMultipleSamplesPerElement() { return versionIsAtLeast(this.version, "1.2.2"); }
 	get supportsDevTools() { return versionIsAtLeast(this.version, "1.1.0"); }
 }
