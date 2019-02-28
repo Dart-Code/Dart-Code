c95d910903b31d5b1194f588f628d916c7b72072
Danny Tuppeny <danny@tuppeny.com>
2019-02-28 09:33:18 +0000

    Add tests for test-at-cursor in Flutter project
    See https://github.com/Dart-Code/Dart-Code/issues/1492.

--- test/flutter_only/commands/run_test_at_cursor.test.ts
+++ test/flutter_only/commands/run_test_at_cursor.test.ts
@@ -2,18 +2,18 @@ import * as assert from "assert";
 import * as vs from "vscode";
 import { OpenFileTracker } from "../../../src/analysis/open_file_tracker";
 import { cursorIsInTest } from "../../../src/commands/test";
-import { activate, getPackages, helloWorldTestMainFile, openFile, positionOf, waitForResult } from "../../helpers";
+import { activate, flutterTestOtherFile, getPackages, openFile, positionOf, waitForResult } from "../../helpers";
 
 describe("run test at cursor", () => {
 
 	before("get packages", () => getPackages());
 	beforeEach("activate and wait for outline", async () => {
-		await activate(helloWorldTestMainFile);
-		await waitForResult(() => !!OpenFileTracker.getOutlineFor(helloWorldTestMainFile));
+		await activate(flutterTestOtherFile);
+		await waitForResult(() => !!OpenFileTracker.getOutlineFor(flutterTestOtherFile));
 	});
 
 	it("command is available when cursor is inside a test", async () => {
-		const editor = await openFile(helloWorldTestMainFile);
+		const editor = await openFile(flutterTestOtherFile);
 		editor.selection = new vs.Selection(positionOf("expect^("), positionOf("^expect("));
 
 		// Allow some time to check, because the flag is flipped in a selection change handler
@@ -25,7 +25,7 @@ describe("run test at cursor", () => {
 	});
 
 	it("command is not available when cursor is not inside a test", async () => {
-		const editor = await openFile(helloWorldTestMainFile);
+		const editor = await openFile(flutterTestOtherFile);
 		editor.selection = new vs.Selection(positionOf("main^("), positionOf("^main("));
 
 		// Allow some time to check, because the flag is flipped in a selection change handler
