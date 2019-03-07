5cfd7514ac5957f4a4f05ec3ca3057241b472372
Danny Tuppeny <danny@tuppeny.com>
2019-03-07 16:52:46 +0000

    Add a notification about DevTools
    Fixes #1503.

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,8 @@ export const restartReasonSave = "save";
 
 export const pubGlobalDocsUrl = "https://www.dartlang.org/tools/pub/cmd/pub-global";
 export const stagehandInstallationInstructionsUrl = "https://github.com/dart-lang/stagehand#installation";
+
+export const wantToTryDevToolsPrompt = "Dart DevTools (preview) includes additional tools for debugging and profiling Flutter apps, including a Widget Inspector. Try it?";
+export const openDevToolsAction = "Open DevTools";
+export const noThanksAction = "No Thanks";
+export const doNotAskAgainAction = "Don't Ask Again";
--- src/context.ts
+++ src/context.ts
@@ -13,4 +13,10 @@ export class Context {
 
 	// get newFlutterProject() { return this.context.globalState.get("newFlutterProject") as string; }
 	// set newFlutterProject(value: string) { this.context.globalState.update("newFlutterProject", value); }
+	get devToolsNotificationsShown(): number { return (this.context.globalState.get("devToolsNotificationsShown") as number) || 0; }
+	set devToolsNotificationsShown(value: number) { this.context.globalState.update("devToolsNotificationsShown", value); }
+	get devToolsNotificationLastShown(): number | undefined { return this.context.globalState.get("devToolsNotificationLastShown") as number; }
+	set devToolsNotificationLastShown(value: number) { this.context.globalState.update("devToolsNotificationLastShown", value); }
+	get devToolsNotificationDoNotShow(): boolean { return !!this.context.globalState.get("devToolsNotificationDoNotShowAgain"); }
+	set devToolsNotificationDoNotShow(value: boolean) { this.context.globalState.update("devToolsNotificationDoNotShowAgain", value); }
 }
