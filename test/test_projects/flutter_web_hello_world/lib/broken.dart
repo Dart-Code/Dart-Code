7d330647556818b46bdcd013312fe08eed6ad7af
Danny Tuppeny <danny@tuppeny.com>
2019-03-28 11:14:35 +0000

    Add web test project

---
+++ test/test_projects/flutter_web_hello_world/lib/broken.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_web/material.dart';
+
+void main() => runApp(new MyBrokenApp());
+
+class MyBrokenApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(primarySwatch: Colors.blue),
+      home: new MyBrokenHomePage(),
+    );
+  }
+}
+
+class MyBrokenHomePage extends StatelessWidget {
+  MyBrokenHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    throw new Exception("Oops");
+  }
+}
---
+++ test/test_projects/flutter_web_hello_world/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_web/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(primarySwatch: Colors.blue),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    myTopLevelFunction();
+    return new Text(
+      // BREAKPOINT1^
+      'Hello, world!',
+      textDirection: TextDirection.ltr,
+    );
+  }
+}
+
+myTopLevelFunction() {
+  const _ = 1;
+  // BREAKPOINT2^
+}
