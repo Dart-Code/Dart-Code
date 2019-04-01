89a73db470721f3a058b6057996630a1175a82ec
Danny Tuppeny <danny@tuppeny.com>
2019-04-01 14:02:32 +0100

    Add tests for Ignore Lint code actions

---
+++ test/dart_only/providers/ignore_lint_code_action_provider.test.ts
@@ -0,0 +1,36 @@
+import * as assert from "assert";
+import * as vs from "vscode";
+import { activate, currentDoc, emptyFile, ensureTestContent, openFile, rangeOf, setTestContent } from "../../helpers";
+
+describe("ignore_lint_code_action_provider", () => {
+	beforeEach("activate", () => activate());
+
+	it("provides action to edit a lint", async () => {
+		await openFile(emptyFile);
+		await setTestContent(`main() {
+  var a = 1;
+}`);
+		const fixResults = await (vs.commands.executeCommand("vscode.executeCodeActionProvider", currentDoc().uri, rangeOf("|a| = 1")) as Thenable<vs.CodeAction[]>);
+		assert.ok(fixResults);
+		assert.ok(fixResults.length);
+
+		const ignoreLintAction = fixResults.find((r) => r.title.indexOf("Ignore hint 'unused_local_variable' for this line") !== -1);
+		assert.ok(ignoreLintAction);
+	});
+
+	it("edits in '// ignore: ' comment", async () => {
+		await openFile(emptyFile);
+		await setTestContent(`main() {
+  var a = 1;
+}`);
+		const fixResults = await (vs.commands.executeCommand("vscode.executeCodeActionProvider", currentDoc().uri, rangeOf("|a| = 1")) as Thenable<vs.CodeAction[]>);
+		const ignoreLintAction = fixResults.find((r) => r.title.indexOf("Ignore hint 'unused_local_variable' for this line") !== -1);
+
+		await vs.workspace.applyEdit(ignoreLintAction!.edit);
+
+		await ensureTestContent(`main() {
+  // ignore: unused_local_variable
+  var a = 1;
+}`);
+	});
+});
