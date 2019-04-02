923febb1b39de8344402849033d53bdc2132d7fd
Danny Tuppeny <danny@tuppeny.com>
2019-04-02 11:10:42 +0100

    Add files for easier comments comparison in future

---
+++ test/test_projects/manual_testing/lib/comments.dart
@@ -0,0 +1,82 @@
+/// Any line that starts with `var` should be coloured.
+/// Every other line should be a comment.
+
+/* test /* test */ */
+var a = 1;
+/** test /** test */ */
+var b = 1;
+/** test /* test */ */
+var c = 1;
+/* test /** test */ */
+var d = 1;
+// test
+var e = 1;
+
+/**
+ * test
+ *     test
+ * test
+ */
+
+/*
+/**
+ * test
+ *     test
+ * test
+ */
+*/
+
+// /**
+//  * test
+//  *     test
+//  * test
+//  */
+
+/// /*
+/// /**
+///  * test
+///  *     test
+///  * test
+///  */
+/// */
+
+/// test
+var f = 1;
+
+/// test
+/// test
+var g = 1; /// test
+var h = 1; /// test
+/// test
+
+
+// test
+var i = 1;
+
+// test
+// test
+var j = 1; // test
+var k = 1; // test
+// test
+
+/* // */
+var l = 1;
+
+/* /// */
+var m = 1;
+
+/*
+ * // Test
+ */
+var n = 1;
+
+/** // */
+var o = 1;
+
+/** /// */
+var p = 1;
+
+/**
+  * // Test
+  */
+var q = 1;
