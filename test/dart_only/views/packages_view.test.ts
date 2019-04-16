497324a96c9ebb2dc60dbd0d8a58e02ad86c57c3
Danny Tuppeny <danny@tuppeny.com>
2019-04-16 16:32:15 +0100

    Update tests for dependencies tree

--- test/dart_only/views/packages_view.test.ts
+++ test/dart_only/views/packages_view.test.ts
@@ -1,30 +1,30 @@
 import * as assert from "assert";
 import { fsPath } from "../../../src/utils";
-import { PackageDep } from "../../../src/views/packages_view";
+import { PackageDepFile, PackageDepPackage } from "../../../src/views/packages_view";
 import { ensurePackageTreeNode, extApi, getPackages, myPackageThingFile } from "../../helpers";
 
 describe("packages tree", () => {
 	// We have tests that require external packages.
 	before("get packages", () => getPackages());
 
-	it("includes known packages at the top level", async () => {
-		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
-		ensurePackageTreeNode(topLevel, PackageDep, "my_package");
-		ensurePackageTreeNode(topLevel, PackageDep, "meta");
-		ensurePackageTreeNode(topLevel, PackageDep, "test");
+	it("includes known packages at the top level", () => {
+		const topLevel = extApi.packagesTreeProvider.getChildren(undefined);
+		ensurePackageTreeNode(topLevel, PackageDepPackage, "my_package");
+		ensurePackageTreeNode(topLevel, PackageDepPackage, "meta");
+		ensurePackageTreeNode(topLevel, PackageDepPackage, "test");
 	});
 
-	it("does not include own package", async () => {
-		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
+	it("does not include own package", () => {
+		const topLevel = extApi.packagesTreeProvider.getChildren(undefined);
 		const self = topLevel.find((node) => node.label === "hello_world");
 		assert.equal(self, undefined);
 	});
 
-	it("includes know folders from inside lib/", async () => {
-		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
-		const myPackage = ensurePackageTreeNode(topLevel, PackageDep, "my_package");
-		const myPackageLibContents = await extApi.packagesTreeProvider.getChildren(myPackage);
-		const file = ensurePackageTreeNode(myPackageLibContents, PackageDep, "my_thing.dart");
+	it("includes know folders from inside lib/", () => {
+		const topLevel = extApi.packagesTreeProvider.getChildren(undefined);
+		const myPackage = ensurePackageTreeNode(topLevel, PackageDepPackage, "my_package");
+		const myPackageLibContents = extApi.packagesTreeProvider.getChildren(myPackage);
+		const file = ensurePackageTreeNode(myPackageLibContents, PackageDepFile, "my_thing.dart");
 		assert.equal(fsPath(file.resourceUri), fsPath(myPackageThingFile));
 	});
 });
