79b0dd0768390302c7392ba0b9f328b358fdf9a6
Danny Tuppeny <danny@tuppeny.com>
2019-04-18 15:35:57 +0100

    Increase delays waiting for resolve :(

--- test/helpers.ts
+++ test/helpers.ts
@@ -424,8 +424,9 @@ export async function acceptFirstSuggestion(): Promise<void> {
 	// before we accept, so that we don't insert the standard label without the extra
 	// edits which are added in in resolve).
 	await vs.commands.executeCommand("editor.action.triggerSuggest");
-	await delay(1000); // Increased from 500, this is definitely a source of flakes.
+	await delay(3000);
 	await waitForEditorChange(() => vs.commands.executeCommand("acceptSelectedSuggestion"));
+	await delay(1000);
 }
 
 export function ensureError(errors: vs.Diagnostic[], text: string) {
