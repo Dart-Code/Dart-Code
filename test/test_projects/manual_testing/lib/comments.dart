9d8484ef8a59143c1cd48c8cf7c124e04cddc1f2
Danny Tuppeny <danny@tuppeny.com>
2019-04-24 11:06:14 +0100

    Update comment sample for #1635

--- test/test_projects/manual_testing/lib/comments.dart
+++ test/test_projects/manual_testing/lib/comments.dart
@@ -80,3 +80,86 @@ var p = 1;
   * // Test
   */
 var q = 1;
+
+
+class ClassToAddIndenting {
+  /* test /* test */ */
+  var a = 1;
+  /** test /** test */ */
+  var b = 1;
+  /** test /* test */ */
+  var c = 1;
+  /* test /** test */ */
+  var d = 1;
+  // test
+  var e = 1;
+
+  /**
+   * test
+   *     test
+   * test
+   */
+
+  /*
+  /**
+   * test
+   *     test
+   * test
+   */
+  */
+
+  // /**
+  //  * test
+  //  *     test
+  //  * test
+  //  */
+
+  /// /*
+  /// /**
+  ///  * test
+  ///  *     test
+  ///  * test
+  ///  */
+  /// */
+
+  /// test
+  var f = 1;
+
+  /// test
+  /// test
+  var g = 1; /// test
+  var h = 1; /// test
+  /// test
+
+
+  // test
+  var i = 1;
+
+  // test
+  // test
+  var j = 1; // test
+  var k = 1; // test
+  // test
+
+  /* // */
+  var l = 1;
+
+  /* /// */
+  var m = 1;
+
+  /*
+  * // Test
+  */
+  var n = 1;
+
+  /** // */
+  var o = 1;
+
+  /** /// */
+  var p = 1;
+
+  /**
+    * // Test
+    */
+  var q = 1;
+}
