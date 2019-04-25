ee6aec2fcb17e9b0bd7231768a147ac5d5d89965
Danny Tuppeny <danny@tuppeny.com>
2019-04-25 11:08:19 +0100

    Allow DevTools port to be specified in config
    Fixes #1644.

--- src/config.ts
+++ src/config.ts
@@ -38,6 +38,7 @@ class Config {
 	get buildRunnerAdditionalArgs(): string[] { return this.getConfig<string[]>("buildRunnerAdditionalArgs", []); }
 	get checkForSdkUpdates(): boolean { return this.getConfig<boolean>("checkForSdkUpdates", true); }
 	get closingLabels(): boolean { return this.getConfig<boolean>("closingLabels", true); }
+	get devToolsPort(): undefined | number { return this.getConfig<null | number>("devToolsPort", null); }
 	get devToolsTheme(): "dark" | "light" { return this.getConfig<"dark" | "light">("devToolsTheme", "dark"); }
 	get enableSdkFormatter(): boolean { return this.getConfig<boolean>("enableSdkFormatter", true); }
 	get env(): object { return this.getConfig<object>("env", {}); }
