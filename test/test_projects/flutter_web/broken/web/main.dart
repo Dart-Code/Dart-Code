c82616aea93cf4aa1e9f017502784bb95659e0f3
Danny Tuppeny <danny@tuppeny.com>
2019-05-08 09:13:25 +0100

    Remove redundant TODO

--- test/test_projects/flutter_web/broken/web/main.dart
+++ test/test_projects/flutter_web/broken/web/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_web_ui/ui.dart' as web_ui;
 import 'package:broken/main.dart' as my_app;
-// TODO: Figure out how to have multiple entry points.
-// import 'package:hello_world/broken.dart' as my_app;
 
 main() async {
   await web_ui.webOnlyInitializePlatform();
