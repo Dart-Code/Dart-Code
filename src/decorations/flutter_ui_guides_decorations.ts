85ba39a502ce6d8c7ed865c8287a9551d3647243
Danny Tuppeny <danny@tuppeny.com>
2019-05-15 14:22:39 +0100

    Fix handling when start > end

--- src/decorations/flutter_ui_guides_decorations.ts
+++ src/decorations/flutter_ui_guides_decorations.ts
@@ -66,8 +66,8 @@ export class FlutterUiGuideDecorations implements vs.Disposable {
 	private getDecorations(guidesByLine: { [key: number]: WidgetGuide[] }): vs.DecorationOptions[] {
 		const decorations: vs.DecorationOptions[] = [];
 		for (const line of Object.keys(guidesByLine).map((k) => parseInt(k, 10))) {
-			const firstChar = Math.min(...guidesByLine[line].map((g) => g.start.character));
-			const lastChar = Math.max(...guidesByLine[line].map((g) => g.end.character));
+			const firstChar = Math.min(...guidesByLine[line].map((g) => Math.min(g.start.character, g.end.character)));
+			const lastChar = Math.max(...guidesByLine[line].map((g) => Math.max(g.start.character, g.end.character)));
 			const decorationString = new Array(lastChar).fill(nonBreakingSpace);
 			for (const guide of guidesByLine[line]) {
 				if (line !== guide.end.line) {
