86d0fc0ef483b85064f3753092955b0c1814d014
Danny Tuppeny <danny@tuppeny.com>
2019-05-22 15:10:10 +0100

    Fix up some tests to work with base VS classes
    Without dependencies on our specific implementations.

--- src/test/dart_only/views/packages_view.test.ts
+++ src/test/dart_only/views/packages_view.test.ts
@@ -1,37 +1,37 @@
 import * as assert from "assert";
-import { fsPath } from "../../../extension/utils";
 import { PackageDepFile, PackageDepPackage } from "../../../extension/views/packages_view";
+import { fsPath } from "../../../shared/vscode/utils";
 import { ensurePackageTreeNode, extApi, getPackages, myPackageThingFile, renderedItemLabel } from "../../helpers";
 
 describe("packages tree", () => {
 	// We have tests that require external packages.
 	before("get packages", () => getPackages());
 
-	it("includes known packages at the top level", () => {
-		const topLevel = extApi.packagesTreeProvider.getChildren(undefined);
+	it("includes known packages at the top level", async () => {
+		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
 		ensurePackageTreeNode(topLevel, PackageDepPackage, "my_package");
 		ensurePackageTreeNode(topLevel, PackageDepPackage, "meta");
 		ensurePackageTreeNode(topLevel, PackageDepPackage, "test");
 	});
 
-	it("does not include own package", () => {
-		const topLevel = extApi.packagesTreeProvider.getChildren(undefined);
+	it("does not include own package", async () => {
+		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
 		const self = topLevel.find((node) => node.label === "hello_world");
 		assert.equal(self, undefined);
 	});
 
-	it("includes known folders from inside lib/", () => {
-		const topLevel = extApi.packagesTreeProvider.getChildren(undefined);
+	it("includes known folders from inside lib/", async () => {
+		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
 		const myPackage = ensurePackageTreeNode(topLevel, PackageDepPackage, "my_package");
-		const myPackageLibContents = extApi.packagesTreeProvider.getChildren(myPackage);
+		const myPackageLibContents = await extApi.packagesTreeProvider.getChildren(myPackage);
 		const file = ensurePackageTreeNode(myPackageLibContents, PackageDepFile, "my_thing.dart");
 		assert.equal(fsPath(file.resourceUri), fsPath(myPackageThingFile));
 	});
 
-	it("sorts the same way as VS Code explorer", () => {
-		const topLevel = extApi.packagesTreeProvider.getChildren(undefined);
+	it("sorts the same way as VS Code explorer", async () => {
+		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
 		const myPackage = ensurePackageTreeNode(topLevel, PackageDepPackage, "my_package");
-		const myPackageLibContents = extApi.packagesTreeProvider.getChildren(myPackage);
+		const myPackageLibContents = await extApi.packagesTreeProvider.getChildren(myPackage);
 
 		const names = myPackageLibContents.map((f) => renderedItemLabel(f));
 		// This isn't quite the same as VS Code explorer, as it does complicated things
--- src/test/multi_root/views/packages_view.test.ts
+++ src/test/multi_root/views/packages_view.test.ts
@@ -1,6 +1,6 @@
 import * as assert from "assert";
-import { fsPath } from "../../../extension/utils";
 import { PackageDepFile, PackageDepPackage, PackageDepProject } from "../../../extension/views/packages_view";
+import { fsPath } from "../../../shared/vscode/utils";
 import { ensurePackageTreeNode, extApi, flutterHelloWorldMainFile, getPackages, helloWorldMainFile, myPackageThingFile } from "../../helpers";
 
 describe("packages tree", () => {
@@ -8,26 +8,26 @@ describe("packages tree", () => {
 	before("get packages (0)", () => getPackages(helloWorldMainFile));
 	before("get packages (1)", () => getPackages(flutterHelloWorldMainFile));
 
-	it("includes project folders at the top level", () => {
-		const topLevel = extApi.packagesTreeProvider.getChildren(undefined);
+	it("includes project folders at the top level", async () => {
+		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
 		ensurePackageTreeNode(topLevel, PackageDepProject, "hello_world");
 		ensurePackageTreeNode(topLevel, PackageDepProject, "flutter_hello_world");
 	});
 
-	it("does not include own package", () => {
-		const topLevel = extApi.packagesTreeProvider.getChildren(undefined);
+	it("does not include own package", async () => {
+		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
 		const helloWorld = ensurePackageTreeNode(topLevel, PackageDepProject, "hello_world");
-		const packages = extApi.packagesTreeProvider.getChildren(helloWorld);
+		const packages = await extApi.packagesTreeProvider.getChildren(helloWorld);
 		const self = packages.find((node) => node.label === "hello_world");
 		assert.equal(self, undefined);
 	});
 
-	it("includes known folders from inside lib/", () => {
-		const topLevel = extApi.packagesTreeProvider.getChildren(undefined);
+	it("includes known folders from inside lib/", async () => {
+		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
 		const helloWorld = ensurePackageTreeNode(topLevel, PackageDepProject, "hello_world");
-		const packages = extApi.packagesTreeProvider.getChildren(helloWorld);
+		const packages = await extApi.packagesTreeProvider.getChildren(helloWorld);
 		const myPackage = ensurePackageTreeNode(packages, PackageDepPackage, "my_package");
-		const myPackageLibContents = extApi.packagesTreeProvider.getChildren(myPackage);
+		const myPackageLibContents = await extApi.packagesTreeProvider.getChildren(myPackage);
 		const file = ensurePackageTreeNode(myPackageLibContents, PackageDepFile, "my_thing.dart");
 		assert.equal(fsPath(file.resourceUri), fsPath(myPackageThingFile));
 	});
