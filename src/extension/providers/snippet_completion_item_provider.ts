09df6ecb0cd988a244626954631267a1011a8a5b
Danny Tuppeny <danny@tuppeny.com>
2019-05-22 12:18:40 +0100

    Improve path handling to aid packing

--- src/extension/providers/snippet_completion_item_provider.ts
+++ src/extension/providers/snippet_completion_item_provider.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import { isArray } from "util";
 import { CancellationToken, CompletionContext, CompletionItem, CompletionItemKind, CompletionItemProvider, CompletionList, MarkdownString, Position, SnippetString, TextDocument, Uri } from "vscode";
+import { extensionPath, readJson } from "../utils";
 
 export class SnippetCompletionItemProvider implements CompletionItemProvider {
 	private completions = new CompletionList();
@@ -8,7 +9,7 @@ export class SnippetCompletionItemProvider implements CompletionItemProvider {
 
 	constructor(filename: string, shouldRender: (uri: Uri) => boolean) {
 		this.shouldRender = shouldRender;
-		const snippets = require(path.join("../../../..", filename));
+		const snippets = readJson(path.join(extensionPath, filename));
 		for (const snippetType of Object.keys(snippets)) {
 			for (const snippetName of Object.keys(snippets[snippetType])) {
 				const snippet = snippets[snippetType][snippetName];
