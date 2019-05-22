443b1c79ddb621a74522b19190d311d75d845031
Danny Tuppeny <danny@tuppeny.com>
2019-05-22 17:55:07 +0100

    Fix package tree tests
    Use context value instead of constructors so it works with packed extension.

--- src/test/flutter_only/views/packages_view.test.ts
+++ src/test/flutter_only/views/packages_view.test.ts
@@ -1,5 +1,5 @@
 import * as assert from "assert";
-import { PackageDepPackage, PackageDepProject } from "../../../extension/views/packages_view";
+import { DART_DEP_PACKAGE_NODE_CONTEXT, DART_DEP_PROJECT_NODE_CONTEXT } from "../../../shared/constants";
 import { ensurePackageTreeNode, extApi, getPackages } from "../../helpers";
 
 describe("packages tree", () => {
@@ -8,22 +8,22 @@ describe("packages tree", () => {
 
 	it("includes multiple projects from single workspace folder at the top level", async () => {
 		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
-		ensurePackageTreeNode(topLevel, PackageDepProject, "flutter_hello_world");
-		ensurePackageTreeNode(topLevel, PackageDepProject, "example", "flutter_hello_world");
+		ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "flutter_hello_world");
+		ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "example", "flutter_hello_world");
 	});
 
 	it("includes known packages inside the project", async () => {
 		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
-		const myPackage = ensurePackageTreeNode(topLevel, PackageDepProject, "flutter_hello_world");
+		const myPackage = ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "flutter_hello_world");
 		const packages = await extApi.packagesTreeProvider.getChildren(myPackage);
-		ensurePackageTreeNode(packages, PackageDepPackage, "meta");
-		ensurePackageTreeNode(packages, PackageDepPackage, "path");
-		ensurePackageTreeNode(packages, PackageDepPackage, "test_api");
+		ensurePackageTreeNode(packages, DART_DEP_PACKAGE_NODE_CONTEXT, "meta");
+		ensurePackageTreeNode(packages, DART_DEP_PACKAGE_NODE_CONTEXT, "path");
+		ensurePackageTreeNode(packages, DART_DEP_PACKAGE_NODE_CONTEXT, "test_api");
 	});
 
 	it("does not include own package inside the project", async () => {
 		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
-		const myPackage = ensurePackageTreeNode(topLevel, PackageDepProject, "flutter_hello_world");
+		const myPackage = ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "flutter_hello_world");
 		const packages = await extApi.packagesTreeProvider.getChildren(myPackage);
 		const self = packages.find((node) => node.label === "hello_world");
 		assert.equal(self, undefined);
