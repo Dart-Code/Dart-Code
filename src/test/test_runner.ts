aa6a287aee933a2a2e227d132b03b1667b2c722c
Danny Tuppeny <danny@tuppeny.com>
2019-06-05 08:57:29 +0100

    Switch over to new vscode-test runner

--- src/test/test_runner.ts
+++ src/test/test_runner.ts
@@ -1,8 +1,10 @@
 console.log("Starting test runner...");
 
 import * as fs from "fs";
+import * as glob from "glob";
+import * as Mocha from "mocha";
+import * as path from "path";
 import { MultiReporter } from "./mocha_multi_reporter";
-import testRunner = require("vscode/lib/testrunner");
 const onExit = require("signal-exit"); // tslint:disable-line:no-var-requires
 
 // Ensure we write coverage on exit.
@@ -19,18 +21,39 @@ onExit(() => {
 	}
 });
 
-testRunner.configure({
-	forbidOnly: !!process.env.MOCHA_FORBID_ONLY,
-	reporter: MultiReporter,
-	reporterOptions: {
-		output: process.env.TEST_XML_OUTPUT,
-		summaryFile: process.env.TEST_CSV_SUMMARY,
-		testRunName: process.env.TEST_RUN_NAME,
-	},
-	slow: 10000,       // increased threshold before marking a test as slow
-	timeout: 180000,   // increased timeout because starting up Code, Analyzer, Pub, etc. is slooow
-	ui: "bdd",        // the TDD UI is being used in extension.test.ts (suite, test, etc.)
-	useColors: true,  // colored output from test results
-} as MochaSetupOptions & { reporterOptions: any });
+module.exports = {
+	run(testsRoot: string, cb: (error: any, failures?: number) => void): void {
+		// Create the mocha test
+		const mocha = new Mocha({
+			forbidOnly: !!process.env.MOCHA_FORBID_ONLY,
+			reporter: MultiReporter,
+			reporterOptions: {
+				output: process.env.TEST_XML_OUTPUT,
+				summaryFile: process.env.TEST_CSV_SUMMARY,
+				testRunName: process.env.TEST_RUN_NAME,
+			},
+			slow: 10000,       // increased threshold before marking a test as slow
+			timeout: 180000,   // increased timeout because starting up Code, Analyzer, Pub, etc. is slooow
+			ui: "bdd",        // the TDD UI is being used in extension.test.ts (suite, test, etc.)
+			useColors: true,  // colored output from test results
+		});
+		// Use any mocha API
+		mocha.useColors(true);
 
-module.exports = testRunner;
+		glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
+			if (err) {
+				return cb(err);
+			}
+
+			// Add files to the test suite
+			files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+
+			try {
+				// Run the mocha test
+				mocha.run((failures) => cb(null, failures));
+			} catch (err) {
+				cb(err);
+			}
+		});
+	},
+};
