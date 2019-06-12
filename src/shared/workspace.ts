dee47dc35b27f94389a852126302fd90ad4af051
Danny Tuppeny <danny@tuppeny.com>
2019-06-12 09:19:44 +0100

    Improve recording of unknown project types

--- src/shared/workspace.ts
+++ src/shared/workspace.ts
@@ -27,8 +27,17 @@ export class WorkspaceContext {
 			types.push("Flutter Web");
 		if (this.hasProjectsInFuchsiaTree)
 			types.push("Fuchsia");
-		if (types.length === 0)
-			types.push("Unknown");
+
+		// If we didn't detect any projects, record as unknown, but include info
+		// on the type of SDK we had found.
+		if (types.length === 0) {
+			if (this.sdks && this.sdks.dartSdkIsFromFlutter)
+				types.push("Unknown (Flutter SDK)");
+			else if (this.sdks && this.sdks.dart)
+				types.push("Unknown (Dart SDK)");
+			else
+				types.push("Unknown (No SDK)");
+		}
 
 		return types.join(", ");
 	}
