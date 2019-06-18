58014ce1b4fa0793ca38a11f235c7adccb05fa25
Danny Tuppeny <danny@tuppeny.com>
2019-06-18 15:03:31 +0100

    Fix advertising of source actions based on context.only

--- src/extension/providers/source_code_action_provider.ts
+++ src/extension/providers/source_code_action_provider.ts
@@ -5,32 +5,45 @@ const SourceSortMembers = CodeActionKind.Source.append("sortMembers");
 
 export class SourceCodeActionProvider implements CodeActionProvider {
 	public readonly metadata: CodeActionProviderMetadata = {
-		providedCodeActionKinds: [CodeActionKind.SourceOrganizeImports, SourceSortMembers],
+		providedCodeActionKinds: [CodeActionKind.Source, CodeActionKind.SourceOrganizeImports, SourceSortMembers],
 	};
 
 	public provideCodeActions(document: TextDocument, range: Range, context: CodeActionContext, token: CancellationToken): CodeAction[] | undefined {
 		if (!isAnalyzableAndInWorkspace(document))
 			return undefined;
-		// If we were only asked for specific action types and that doesn't include
-		// source (which is all we supply), bail out.
-		if (context && context.only && !context.only.contains(CodeActionKind.Source))
-			return undefined;
-		return [{
-			command: {
-				arguments: [document],
-				command: "_dart.organizeImports",
+
+		const actions = [];
+
+		if (!context
+			|| !context.only
+			|| context.only.contains(CodeActionKind.Source)
+			|| context.only.contains(CodeActionKind.SourceOrganizeImports)) {
+			actions.push({
+				command: {
+					arguments: [document],
+					command: "_dart.organizeImports",
+					title: "Organize Imports",
+				},
+				kind: CodeActionKind.SourceOrganizeImports,
 				title: "Organize Imports",
-			},
-			kind: CodeActionKind.SourceOrganizeImports,
-			title: "Organize Imports",
-		}, {
-			command: {
-				arguments: [document],
-				command: "dart.sortMembers",
+			});
+		}
+
+		if (!context
+			|| !context.only
+			|| context.only.contains(CodeActionKind.Source)
+			|| context.only.contains(SourceSortMembers)) {
+			actions.push({
+				command: {
+					arguments: [document],
+					command: "dart.sortMembers",
+					title: "Sort Members",
+				},
+				kind: SourceSortMembers,
 				title: "Sort Members",
-			},
-			kind: SourceSortMembers,
-			title: "Sort Members",
-		}];
+			});
+		}
+
+		return actions;
 	}
 }
