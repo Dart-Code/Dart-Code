af03e0cc4844510d35766183ac52496027f741e5
Danny Tuppeny <danny@tuppeny.com>
2019-06-25 17:16:11 +0100

    Fix up coverage for webpacked extension

--- webpack.config.js
+++ webpack.config.js
@@ -19,35 +19,44 @@
 
 const path = require("path");
 
-/**
- * @type {import("webpack").Configuration}
- */
-const config = {
-	devtool: "source-map",
-	entry: "./src/extension/extension.ts",
-	// https://webpack.js.org/configuration/externals/
-	externals: {
-		vscode: "commonjs vscode",
-	},
-	module: {
-		rules: [{
-			exclude: /node_modules/,
-			test: /\.ts$/,
-			use: [{
+module.exports = env => {
+	/**
+	 * @type {import("webpack").Configuration}
+	 */
+	const config = {
+		devtool: "source-map",
+		entry: "./src/extension/extension.ts",
+		// https://webpack.js.org/configuration/externals/
+		externals: {
+			vscode: "commonjs vscode",
+		},
+		module: {
+			rules: [{
+				exclude: /node_modules/,
+				test: /\.ts$/,
 				loader: "ts-loader",
 			}],
-		}],
-	},
-	output: {
-		devtoolModuleFilenameTemplate: "../../[resource-path]",
-		filename: "extension.js",
-		libraryTarget: "commonjs2",
-		path: path.resolve(__dirname, "out/dist"),
-	},
-	resolve: {
-		extensions: [".ts", ".js"],
-	},
-	target: "node",
-};
+		},
+		output: {
+			devtoolModuleFilenameTemplate: "../../[resource-path]",
+			filename: "extension.js",
+			libraryTarget: "commonjs2",
+			path: path.resolve(__dirname, "out/dist"),
+		},
+		resolve: {
+			extensions: [".ts", ".js"],
+		},
+		target: "node",
+	};
 
-module.exports = config;
+	if (env && env.instrumentation) {
+		config.module.rules.push({
+			enforce: "post",
+			exclude: /node_modules/,
+			test: /\.ts$/,
+			loader: "istanbul-instrumenter-loader",
+		});
+	}
+
+	return config;
+};
