89cb4ad087afe6fc01c6f01552bb6a49b4d551fd
Danny Tuppeny <danny@tuppeny.com>
2019-06-25 12:36:00 +0100

    Make an interface for Flutter daemon to allow it to be replaced in tests

--- src/extension/services/stdio_service.ts
+++ src/extension/services/stdio_service.ts
@@ -1,6 +1,7 @@
 import * as child_process from "child_process";
 import * as fs from "fs";
 import { IAmDisposable, Logger } from "../../shared/interfaces";
+import { UnknownResponse } from "../../shared/services/interfaces";
 import { getLogHeader } from "../utils/log";
 import { safeSpawn } from "../utils/processes";
 
@@ -275,24 +276,3 @@ export abstract class StdIOService<T> implements IAmDisposable {
 		this.disposables.forEach((d) => d.dispose());
 	}
 }
-
-export interface Request<T> {
-	id: string;
-	method: string;
-	params: T;
-}
-
-export interface Response<T> {
-	id: string;
-	error: any;
-	result: T;
-}
-
-export interface UnknownResponse extends Response<any> { }
-
-export interface Notification<T> {
-	event: string;
-	params: T;
-}
-
-export interface UnknownNotification extends Notification<any> { }
