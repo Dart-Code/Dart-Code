ace638784f6809c9e4cd815266f26ba0a437878c
Danny Tuppeny <danny@tuppeny.com>
2019-06-26 15:45:07 +0100

    Fix logging thing

--- src/test/mocha_logging_reporter.ts
+++ src/test/mocha_logging_reporter.ts
@@ -1,7 +1,12 @@
 import { ITest, reporters } from "mocha";
 import { LogCategory } from "../shared/enums";
 import { InternalExtensionApi } from "../shared/vscode/interfaces";
-import { logger } from "./helpers";
+
+function getLogger() {
+	// TODO: Make a logger that buffers then flushes when it gets the real one.
+	const extApi: InternalExtensionApi = require("./helpers").extApi;
+	return extApi && extApi.logger;
+}
 
 export class LoggingReporter extends reporters.Base {
 	constructor(runner: any, options: any) {
@@ -11,26 +16,26 @@ export class LoggingReporter extends reporters.Base {
 			// TODO: Make this less hacky.
 
 			runner.on("test", (test: ITest) => {
-				const extApi: InternalExtensionApi = require("./helpers").extApi;
-				if (extApi)
+				const logger = getLogger();
+				if (logger)
 					logger.info(`Starting test ${test.fullTitle()}...`, LogCategory.CI);
 			});
 
 			runner.on("pending", (test: ITest) => {
-				const extApi: InternalExtensionApi = require("./helpers").extApi;
-				if (extApi)
+				const logger = getLogger();
+				if (logger)
 					logger.info(`Test ${test.fullTitle()} pending/skipped`, LogCategory.CI);
 			});
 
 			runner.on("pass", (test: ITest) => {
-				const extApi: InternalExtensionApi = require("./helpers").extApi;
-				if (extApi)
+				const logger = getLogger();
+				if (logger)
 					logger.info(`Test ${test.fullTitle()} passed after ${test.duration}ms`, LogCategory.CI);
 			});
 
 			runner.on("fail", (test: ITest) => {
-				const extApi: InternalExtensionApi = require("./helpers").extApi;
-				if (extApi) {
+				const logger = getLogger();
+				if (logger) {
 					logger.error(`Test ${test.fullTitle()} failed after ${test.duration}ms`, LogCategory.CI);
 					const err = (test as any).err;
 					if (err) {
