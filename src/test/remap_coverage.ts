214aa547b1742b267f0c8ef922c6cb5ff8fa8844
Danny Tuppeny <danny@tuppeny.com>
2019-06-26 11:52:41 +0100

    Get coverage working for non-packed files

---
+++ src/test/remap_coverage.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+import * as path from "path";
+const loadCoverage = require("remap-istanbul/lib/loadCoverage");
+const remap = require("remap-istanbul/lib/remap");
+const writeReport = require("remap-istanbul/lib/writeReport");
+
+const files = fs.readdirSync("../../.nyc_output")
+	.filter((item) => item.endsWith(".json"));
+
+for (const filename of files) {
+	const fullPath = `../../.nyc_output/${filename}`;
+	const coverage = loadCoverage(fullPath);
+	const collector = remap(coverage, {
+		mapFileName: path.resolve,
+	});
+	writeReport(collector, "json", {}, fullPath);
+}
