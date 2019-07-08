b5fb2f105aef9f4887641cc9f55030f549df35de
Danny Tuppeny <danny@tuppeny.com>
2019-07-08 10:55:08 +0100

    Add the ability to detect when running remotely
    See #1738.

--- src/shared/vscode/utils.ts
+++ src/shared/vscode/utils.ts
@@ -1,5 +1,11 @@
-import { commands, Uri } from "vscode";
+import { commands, ExtensionKind, extensions, Uri } from "vscode";
+import { dartCodeExtensionIdentifier } from "../constants";
 import { forceWindowsDriveLetterToUppercase } from "../utils";
+
+const dartExtension = extensions.getExtension(dartCodeExtensionIdentifier);
+// The extension kind is declared as Workspace, but VS Code will return UI in the
+// case that there is no remote extension host.
+export const isRunningLocally = dartExtension.extensionKind === ExtensionKind.UI;
 
 export function fsPath(uri: Uri | string) {
 	// tslint:disable-next-line:disallow-fspath
