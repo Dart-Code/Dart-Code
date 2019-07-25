b3e5c5cc0a9e48b3882b3caf6640940039417ddb
Danny Tuppeny <danny@tuppeny.com>
2019-07-25 09:38:51 +0100

    Fix package tree tests after adding example sub-folder

--- src/test/dart_only/views/packages_view.test.ts
+++ src/test/dart_only/views/packages_view.test.ts
@@ -1,5 +1,5 @@
 import * as assert from "assert";
-import { DART_DEP_FILE_NODE_CONTEXT, DART_DEP_PACKAGE_NODE_CONTEXT } from "../../../shared/constants";
+import { DART_DEP_FILE_NODE_CONTEXT, DART_DEP_PACKAGE_NODE_CONTEXT, DART_DEP_PROJECT_NODE_CONTEXT } from "../../../shared/constants";
 import { fsPath } from "../../../shared/vscode/utils";
 import { ensurePackageTreeNode, extApi, getPackages, myPackageThingFile, renderedItemLabel } from "../../helpers";
 
@@ -7,22 +7,37 @@ describe("packages tree", () => {
 	// We have tests that require external packages.
 	before("get packages", () => getPackages());
 
-	it("includes known packages at the top level", async () => {
+	it("includes multiple projects from single workspace folder at the top level", async () => {
 		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
-		ensurePackageTreeNode(topLevel, DART_DEP_PACKAGE_NODE_CONTEXT, "my_package");
-		ensurePackageTreeNode(topLevel, DART_DEP_PACKAGE_NODE_CONTEXT, "meta");
-		ensurePackageTreeNode(topLevel, DART_DEP_PACKAGE_NODE_CONTEXT, "test");
+		ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "hello_world");
+		ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "example", "hello_world");
+	});
+
+	it("includes known packages in the project", async () => {
+		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
+		const packageNode = ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "hello_world");
+		const packageLevel = await extApi.packagesTreeProvider.getChildren(packageNode);
+
+		ensurePackageTreeNode(packageLevel, DART_DEP_PACKAGE_NODE_CONTEXT, "my_package");
+		ensurePackageTreeNode(packageLevel, DART_DEP_PACKAGE_NODE_CONTEXT, "meta");
+		ensurePackageTreeNode(packageLevel, DART_DEP_PACKAGE_NODE_CONTEXT, "test");
 	});
 
 	it("does not include own package", async () => {
 		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
-		const self = topLevel.find((node) => node.label === "hello_world");
+		const packageNode = ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "hello_world");
+		const packageLevel = await extApi.packagesTreeProvider.getChildren(packageNode);
+
+		const self = packageLevel.find((node) => node.label === "hello_world");
 		assert.equal(self, undefined);
 	});
 
 	it("includes known folders from inside lib/", async () => {
 		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
-		const myPackage = ensurePackageTreeNode(topLevel, DART_DEP_PACKAGE_NODE_CONTEXT, "my_package");
+		const packageNode = ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "hello_world");
+		const packageLevel = await extApi.packagesTreeProvider.getChildren(packageNode);
+
+		const myPackage = ensurePackageTreeNode(packageLevel, DART_DEP_PACKAGE_NODE_CONTEXT, "my_package");
 		const myPackageLibContents = await extApi.packagesTreeProvider.getChildren(myPackage);
 		const file = ensurePackageTreeNode(myPackageLibContents, DART_DEP_FILE_NODE_CONTEXT, "my_thing.dart");
 		assert.equal(fsPath(file.resourceUri), fsPath(myPackageThingFile));
@@ -30,7 +45,10 @@ describe("packages tree", () => {
 
 	it("sorts the same way as VS Code explorer", async () => {
 		const topLevel = await extApi.packagesTreeProvider.getChildren(undefined);
-		const myPackage = ensurePackageTreeNode(topLevel, DART_DEP_PACKAGE_NODE_CONTEXT, "my_package");
+		const packageNode = ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "hello_world");
+		const packageLevel = await extApi.packagesTreeProvider.getChildren(packageNode);
+
+		const myPackage = ensurePackageTreeNode(packageLevel, DART_DEP_PACKAGE_NODE_CONTEXT, "my_package");
 		const myPackageLibContents = await extApi.packagesTreeProvider.getChildren(myPackage);
 
 		const names = myPackageLibContents.map((f) => renderedItemLabel(f));
