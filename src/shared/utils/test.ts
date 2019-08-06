0ac39fb041edb4ec61192deb8a3271320b909128
Danny Tuppeny <danny@tuppeny.com>
2019-08-06 17:21:29 +0100

    Allow launch configs to be used as templates for running tests
    Fixes #1901.

--- src/shared/utils/test.ts
+++ src/shared/utils/test.ts
@@ -1,14 +1,18 @@
 import { escapeRegExp } from "../../shared/utils";
 
-export function getLaunchConfig(noDebug: boolean, path: string, testName: string, isGroup: boolean) {
-	return {
-		args: testName ? ["--name", makeRegexForTest(testName, isGroup)] : undefined,
-		name: "Tests",
-		noDebug,
-		program: path,
-		request: "launch",
-		type: "dart",
-	};
+export function getLaunchConfig(noDebug: boolean, path: string, testName: string, isGroup: boolean, template: any | undefined) {
+	return Object.assign(
+		{},
+		template,
+		{
+			args: testName ? ["--name", makeRegexForTest(testName, isGroup)] : undefined,
+			name: "Tests",
+			noDebug,
+			program: path,
+			request: "launch",
+			type: "dart",
+		},
+	);
 }
 
 export function makeRegexForTest(name: string, isGroup: boolean) {
