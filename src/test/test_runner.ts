2f866783bc6a4ee1a6835c3b5887412377487fa1
Danny Tuppeny <danny@tuppeny.com>
2019-10-15 16:27:31 +0100

    Change callback not to quit, as the extension host seems to be quitting

--- src/test/test_runner.ts
+++ src/test/test_runner.ts
@@ -43,26 +43,18 @@ module.exports = {
 		// Set up source map support.
 		require("source-map-support").install();
 
-		const callbackAndQuit = (error: any, failures?: number) => {
-
-			// Sometimes test runs hang because there are timers left around that
-			// prevent Node from quitting. Mocha used to always call exit at the
-			// end of a run, but that changed in v4. To avoid hanging CI builds,
-			// write an error and then just quit if we're still around after 10 seconds.
-			// unref() will prevent the timeout from keeping the process alive, so
-			// if it quits normally the error will not be written.
+		const callCallback = (error: any, failures?: number) => {
 			setTimeout(() => {
-				console.error(`Test process did not quit within 10 seconds, calling exit!`);
-				process.exit(1);
+				console.error(`Test process did not quit within 10 seconds!`);
 			}, 10000).unref();
 
-			console.log(`Test run is complete! Will quit in 10 seconds if process is still alive...`);
+			console.log(`Test run is complete! Calling VS Code callback with (${error}, ${failures})`);
 			cb(error, failures);
 		};
 
 		glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
 			if (err) {
-				return callbackAndQuit(err);
+				return callCallback(err);
 			}
 
 			// Add files to the test suite
@@ -70,9 +62,9 @@ module.exports = {
 
 			try {
 				// Run the mocha test
-				mocha.run((failures) => callbackAndQuit(null, failures));
+				mocha.run((failures) => callCallback(null, failures));
 			} catch (err) {
-				callbackAndQuit(err);
+				callCallback(err);
 			}
 		});
 	},
