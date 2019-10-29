4e583dc061eae255aacb5cda73462bff4b648e86
Danny Tuppeny <danny@tuppeny.com>
2019-10-29 08:04:56 +0000

    Add a tickbox to enable completion model
    Fixes #2075.

--- src/extension/sdk/capabilities.ts
+++ src/extension/sdk/capabilities.ts
@@ -15,4 +15,5 @@ export class DartCapabilities {
 	get hasDocumentationInCompletions() { return !versionIsAtLeast(this.version, "2.5.1"); }
 	get handlesPathsEverywhereForBreakpoints() { return versionIsAtLeast(this.version, "2.2.1-edge"); }
 	get supportsDisableServiceTokens() { return versionIsAtLeast(this.version, "2.2.1-dev.4.2"); }
+	get supportsCompletionModel() { return versionIsAtLeast(this.version, "2.5.0"); }
 }
