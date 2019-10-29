5f587135f0068b3bc29a1ce6b8b490d782aca890
Danny Tuppeny <danny@tuppeny.com>
2019-10-29 15:40:33 +0000

    Make outline test less flaky

--- src/test/flutter_only/views/flutter_outline.test.ts
+++ src/test/flutter_only/views/flutter_outline.test.ts
@@ -1,5 +1,6 @@
 import * as assert from "assert";
-import { activate, delay, extApi, flutterHelloWorldOutlineFile, getExpectedResults, getPackages, makeTextTree, openFile, waitForResult } from "../../helpers";
+import { waitFor } from "../../../shared/utils/promises";
+import { activate, extApi, flutterHelloWorldOutlineFile, getExpectedResults, getPackages, makeTextTree, openFile, waitForResult } from "../../helpers";
 
 describe("flutter_outline", () => {
 	// We have tests that require external packages.
@@ -12,14 +13,17 @@ describe("flutter_outline", () => {
 		await openFile(flutterHelloWorldOutlineFile);
 		await waitForResult(() => !!extApi.fileTracker.getOutlineFor(flutterHelloWorldOutlineFile));
 
-		// HACK: We may need to wait for an extra round trip when forceNotificationsFor is called?
-		await delay(100);
+		// Wait until we get some child nodes so we know the outline has been processed.
+		await waitFor(async () => {
+			const res = await extApi.flutterOutlineTreeProvider!.getChildren(undefined);
+			return res && res.length;
+		});
 
 		const expectedResults = getExpectedResults();
 		const actualResults = (await makeTextTree(undefined, extApi.flutterOutlineTreeProvider!)).join("\n");
 
-		assert.ok(expectedResults);
-		assert.ok(actualResults);
+		assert.ok(expectedResults, "Expected results were empty");
+		assert.ok(actualResults, "Actual results were empty");
 		assert.equal(actualResults, expectedResults);
 	});
 });
