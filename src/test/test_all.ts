94e96b125da8545679a156e230f9476af3e6679f
Danny Tuppeny <danny@tuppeny.com>
2019-10-30 11:24:52 +0000

    Rename all _only folders

--- src/test/test_all.ts
+++ src/test/test_all.ts
@@ -85,13 +85,13 @@ async function runAllTests(): Promise<void> {
 
 	try {
 		if (!process.env.BOT || process.env.BOT === "dart") {
-			await runTests("dart_only", "hello_world");
+			await runTests("dart", "hello_world");
 		}
 		if (!process.env.BOT || process.env.BOT === "dart_web") {
-			await runTests("web_only", "web");
+			await runTests("web", "web");
 		}
 		if (!process.env.BOT || process.env.BOT === "flutter") {
-			await runTests("flutter_only", "flutter_hello_world");
+			await runTests("flutter", "flutter_hello_world");
 		}
 		if (!process.env.BOT || process.env.BOT === "misc") {
 			await runTests("dart_create_tests", "dart_create_tests.code-workspace");
