98f03a1b79216cedb4ce348453f8016fcbed2671
Danny Tuppeny <danny@tuppeny.com>
2019-12-02 12:34:22 +0000

    Add placeholder for tests

---
+++ src/test/dart_only/utils/vscode.test.ts
@@ -0,0 +1,5 @@
+describe("asExternalUri", () => {
+	// TODO: We need to mock the implementation of asExternalUri in order to
+	// provide mapped URIs when running locally (in tests), without mocking the
+	// logic that handles the WS correctly.
+});
