2a80e0887b22a2a568ef209fa2ab6d1a1ce7bfd3
Danny Tuppeny <danny@tuppeny.com>
2019-12-04 11:50:12 +0000

    Fix race conditions in tests by fixing support for promises in waitFor

--- src/shared/utils/promises.ts
+++ src/shared/utils/promises.ts
@@ -1,9 +1,9 @@
 export const resolvedPromise = Promise.resolve(true);
 
-export async function waitFor<T>(action: () => T, checkEveryMilliseconds: number = 500, tryForMilliseconds: number = 10000, token?: { isCancellationRequested: boolean }): Promise<T | undefined> {
+export async function waitFor<T>(action: () => T | Promise<T>, checkEveryMilliseconds: number = 500, tryForMilliseconds: number = 10000, token?: { isCancellationRequested: boolean }): Promise<T | undefined> {
 	let timeRemaining = tryForMilliseconds;
 	while (timeRemaining > 0 && !(token && token.isCancellationRequested)) {
-		const res = action();
+		const res = await action();
 		if (res)
 			return res;
 		await new Promise((resolve) => setTimeout(resolve, checkEveryMilliseconds));
