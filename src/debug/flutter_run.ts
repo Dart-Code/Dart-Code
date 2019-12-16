86f220968b38bf77ba402cd7f1d07919ca1a5d9a
Danny Tuppeny <danny@tuppeny.com>
2019-12-16 09:50:36 +0000

    Update result type for exposeUrl

--- src/debug/flutter_run.ts
+++ src/debug/flutter_run.ts
@@ -4,7 +4,7 @@ import { CategoryLogger } from "../shared/logging";
 import { RunDaemonBase, RunMode } from "./run_daemon_base";
 
 export class FlutterRun extends RunDaemonBase {
-	constructor(mode: RunMode, flutterBinPath: string, globalFlutterArgs: string[], projectFolder: string | undefined, args: string[], env: { envOverrides?: { [key: string]: string | undefined }, toolEnv: {} }, logFile: string | undefined, logger: Logger, urlExposer: (url: string) => Promise<string>, maxLogLineLength: number) {
+	constructor(mode: RunMode, flutterBinPath: string, globalFlutterArgs: string[], projectFolder: string | undefined, args: string[], env: { envOverrides?: { [key: string]: string | undefined }, toolEnv: {} }, logFile: string | undefined, logger: Logger, urlExposer: (url: string) => Promise<{ url: string }>, maxLogLineLength: number) {
 		super(mode, logFile, new CategoryLogger(logger, LogCategory.FlutterRun), urlExposer, maxLogLineLength, true, true);
 
 		const command = mode === RunMode.Attach ? "attach" : "run";
--- src/debug/web_run.ts
+++ src/debug/web_run.ts
@@ -2,7 +2,7 @@ import { Logger } from "../shared/interfaces";
 import { RunDaemonBase, RunMode } from "./run_daemon_base";
 
 export class WebRun extends RunDaemonBase {
-	constructor(mode: RunMode, pubBinPath: string, projectFolder: string | undefined, args: string[], env: { envOverrides?: { [key: string]: string | undefined }, toolEnv: {} }, logFile: string | undefined, logger: Logger, urlExposer: (url: string) => Promise<string>, maxLogLineLength: number) {
+	constructor(mode: RunMode, pubBinPath: string, projectFolder: string | undefined, args: string[], env: { envOverrides?: { [key: string]: string | undefined }, toolEnv: {} }, logFile: string | undefined, logger: Logger, urlExposer: (url: string) => Promise<{ url: string }>, maxLogLineLength: number) {
 		super(mode, logFile, logger, urlExposer, maxLogLineLength, true, true);
 
 		this.createProcess(projectFolder, pubBinPath, ["global", "run", "webdev", "daemon"].concat(args), env);
