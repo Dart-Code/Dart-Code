cc0a0ea5bb915300445feec7eddee2760b516e67
Danny Tuppeny <danny@tuppeny.com>
2020-01-24 12:45:53 +0000

    Disable CompleteStatement for LSP

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -4,7 +4,7 @@ import { isArray } from "util";
 import * as vs from "vscode";
 import { Analyzer } from "../shared/analyzer";
 import { DaemonCapabilities, FlutterCapabilities } from "../shared/capabilities/flutter";
-import { dartPlatformName, flutterExtensionIdentifier, flutterPath, HAS_LAST_DEBUG_CONFIG, isWin, IS_RUNNING_LOCALLY_CONTEXT, platformDisplayName } from "../shared/constants";
+import { dartPlatformName, flutterExtensionIdentifier, flutterPath, HAS_LAST_DEBUG_CONFIG, isWin, IS_LSP_CONTEXT, IS_RUNNING_LOCALLY_CONTEXT, platformDisplayName } from "../shared/constants";
 import { LogCategory } from "../shared/enums";
 import { setUserAgent } from "../shared/fetch";
 import { DartWorkspaceContext, FlutterSdks, IFlutterDaemon, Sdks } from "../shared/interfaces";
@@ -197,6 +197,7 @@ export async function activate(context: vs.ExtensionContext, isRestart: boolean 
 	if (config.previewLsp || process.env.DART_CODE_FORCE_LSP) {
 		isUsingLsp = true;
 	}
+	vs.commands.executeCommand("setContext", IS_LSP_CONTEXT, isUsingLsp);
 
 	// Fire up the analyzer process.
 	const analyzerStartTime = new Date();
