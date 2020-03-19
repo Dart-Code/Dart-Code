5cce062f2507a175bcbcb3c195e56cdd3a2f252e
Danny Tuppeny <danny@tuppeny.com>
2020-03-19 13:07:47 +0000

    Switch from pseudo terminals to real terminal processes
    Debug adapter sends a custom event to the editor allowing it to spawn the process locally, and watches for the --write-service-info file to connect to the VM.
    
    Fixes #2230.

--- src/extension/utils/vscode/debug.ts
+++ src/extension/utils/vscode/debug.ts
@@ -1,6 +1,5 @@
 import * as vs from "vscode";
 import { PromiseCompleter } from "../../../shared/utils";
-import { red, yellow } from "../../../shared/utils/colors";
 
 export class DartDebugSessionInformation {
 	public observatoryUri?: string;
@@ -12,71 +11,7 @@ export class DartDebugSessionInformation {
 	public progressPromise?: PromiseCompleter<void>;
 	public progressReporter?: vs.Progress<{ message?: string; increment?: number; }>;
 	public progressID?: string;
+	public terminal?: vs.Terminal;
 	public readonly sessionStart: Date = new Date();
-	constructor(public readonly session: vs.DebugSession, public readonly debuggerType: string, public readonly terminal: DartDebugSessionPseudoterminal | undefined) { }
-}
-
-const spawnedTerminalsByName: { [key: string]: vs.Terminal } = {};
-export class DartDebugSessionPseudoterminal {
-	private readonly closeCompleter = new PromiseCompleter<void>();
-	public readonly close = this.closeCompleter.promise;
-	private readonly userInputEmitter = new vs.EventEmitter<string>();
-	public readonly userInput = this.userInputEmitter.event;
-	private readonly emitter = new vs.EventEmitter<string>();
-	private readonly terminal: vs.Terminal;
-	private readonly pseudoterminal: vs.Pseudoterminal;
-	private isRunning = true;
-
-	constructor(public readonly terminalName: string) {
-		this.pseudoterminal = {
-			close: () => this.closeCompleter.resolve(),
-			handleInput: (data) => {
-				if (!this.isRunning)
-					return;
-
-				data = data === "\r" ? "\r\n" : data;
-				this.addOutput(data, "userInput");
-				this.userInputEmitter.fire(data);
-			},
-			onDidWrite: this.emitter.event,
-			// tslint:disable-next-line: no-empty
-			open: () => { },
-
-		};
-		// Close any existing terminal with the same name to ensure we're not creating
-		// new ones each time.
-		if (spawnedTerminalsByName[terminalName])
-			spawnedTerminalsByName[terminalName].dispose();
-		this.terminal = vs.window.createTerminal({ name: terminalName, pty: this.pseudoterminal });
-		spawnedTerminalsByName[terminalName] = this.terminal;
-		this.terminal.show();
-	}
-
-	public end() {
-		// Used to stop echoing user input back once the process has terminated.
-		this.isRunning = false;
-	}
-
-	public addOutput(output: string, category: string) {
-		if (!output)
-			return;
-
-		// If we don't send \r's then VS Code's terminal will not reset
-		// to column 0.
-		output = output.replace(/\n/g, "\r\n");
-
-		switch (category) {
-			case "stderr":
-				output = red(output);
-				break;
-			case "stdout":
-			case "userInput":
-				break;
-			default:
-				output = yellow(output);
-				break;
-		}
-
-		this.emitter.fire(output);
-	}
+	constructor(public readonly session: vs.DebugSession, public readonly debuggerType: string) { }
 }
