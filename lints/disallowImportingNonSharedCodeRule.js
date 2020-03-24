2b778082aea16e32317167e73f90c80a81d3ce34
Danny Tuppeny <danny@tuppeny.com>
2020-03-24 14:22:50 +0000

    Enable lint for detecting extension code in debugger

--- lints/disallowImportingNonSharedCodeRule.js
+++ lints/disallowImportingNonSharedCodeRule.js
@@ -6,7 +6,6 @@ const Lint = require("tslint");
 
 class Rule extends Lint.Rules.AbstractRule {
 	apply(sourceFile) {
-		// Remove this if statement when the below is uncommented too.
 		if (sourceFile.fileName.indexOf("src/debug/") === -1) {
 			return this.applyWithWalker(new NoNonSharedCode(sourceFile, this.getOptions()));
 		}
@@ -17,11 +16,12 @@ Rule.EXTENSION_FAILURE_STRING = "Do not import extension code because the extens
 
 class NoNonSharedCode extends Lint.RuleWalker {
 	visitImportDeclaration(node) {
-		// TODO: Uncomment this when DAs are not running in process.
+		// TODO: Remove first part of this condition when DAs are not running in process.
 		// https://github.com/Dart-Code/Dart-Code/issues/1876
-		// if (node.moduleSpecifier.text.indexOf("../debug/") !== -1) {
-		// 	this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.DEBUG_FAILURE_STRING));
-		// }
+		if (this.sourceFile.fileName.indexOf("debug_config_provider") === -1
+			&& node.moduleSpecifier.text.indexOf("../debug/") !== -1) {
+			this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.DEBUG_FAILURE_STRING));
+		}
 
 		if (node.moduleSpecifier.text.indexOf("../extension/") !== -1) {
 			this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.EXTENSION_FAILURE_STRING));
