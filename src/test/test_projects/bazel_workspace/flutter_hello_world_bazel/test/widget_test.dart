87cb02c2e549094141853ae1a1486a1030162339
Danny Tuppeny <danny@tuppeny.com>
2020-04-07 09:05:38 +0100

    Add a test project for Bazel workspaces

---
+++ src/test/test_projects/bazel_workspace/flutter_hello_world_bazel/test/widget_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../lib/main.dart' as hello_world;
+
+// This comment is extracted by the test and compared to a text representation
+// built from the tree provider in the test. It must be maintained to match
+// the results from teh tests below.
+// == EXPECTED RESULTS ==
+// test/widget_test.dart (pass.svg)
+//     Hello world test (pass.svg)
+// == /EXPECTED RESULTS ==
+
+void main() {
+  testWidgets('Hello world test', (WidgetTester tester) async {
+    hello_world.main(); // BREAKPOINT1
+    await tester.pump();
+    expect(find.text('Hello, world!'), findsOneWidget);
+  });
+}
