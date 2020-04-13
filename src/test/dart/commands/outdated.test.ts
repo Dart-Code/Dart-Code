4dc618b33b4bd89eddc00019cc27d0eaee305ef8
Danny Tuppeny <danny@tuppeny.com>
2020-04-13 18:38:06 +0100

    Add basic support for calling "pub outdated"

---
+++ src/test/dart/commands/outdated.test.ts
@@ -0,0 +1,19 @@
+import * as assert from "assert";
+import * as vs from "vscode";
+import { pubExecutableName } from "../../../shared/constants";
+import { activate, captureOutput, getPackages } from "../../helpers";
+
+describe("pub outdated", () => {
+	before("get packages", () => getPackages());
+	beforeEach("activate", () => activate());
+
+	it("runs and prints output", async () => {
+		const buffer = captureOutput("pub");
+		const exitCode = await vs.commands.executeCommand("pub.outdated");
+		assert.equal(exitCode, 0);
+
+		const output = buffer.buffer.join("").trim();
+		assert.equal(output.startsWith(`[hello_world] ${pubExecutableName} outdated`), true);
+		assert.equal(output.endsWith("exit code 0"), true);
+	});
+});
