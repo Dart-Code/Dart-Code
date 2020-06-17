f89dfefe068c3de9a7d8a8d79602fdfb2d587217
Danny Tuppeny <danny@tuppeny.com>
2020-06-17 09:18:46 +0100

    Convert dart.progress added since this PR was made to use DAP progress

--- src/debug/flutter_debug_impl.ts
+++ src/debug/flutter_debug_impl.ts
@@ -140,7 +140,7 @@ export class FlutterDebugSession extends DartDebugSession {
 			// This text comes through as stdout and not Progress, so map it over
 			// to progress indicator.
 			if (msg.indexOf("Waiting for connection from") !== -1) {
-				this.sendEvent(new Event("dart.progress", { message: msg, progressID: "DEBUG-EXT" }));
+				this.sendEvent(new ProgressUpdateEvent(debugLaunchProgressId, msg));
 			}
 
 			this.logToUser(msg, forceErrorCategory ? "stderr" : this.outputCategory);
