c349cefce2e1d7f4681a4ffdbbc7416a9201185e
Danny Tuppeny <danny@tuppeny.com>
2020-06-24 12:13:38 +0100

    Tweak tests

--- src/test/dart/services/stdio_service.test.ts
+++ src/test/dart/services/stdio_service.test.ts
@@ -39,12 +39,11 @@ describe("stdio_service", () => {
 
 			assert.deepStrictEqual(service.notifications, [event1]);
 		});
+
+		it("processes unhandled messages", async () => {
+			sendRaw(`this is a string${terminator}this is a string${terminator}`);
+
+			assert.deepStrictEqual(service.unhandledMessages, [`this is a string\n`, `this is a string\n`]);
+		});
 	});
-
-	it("processed unhandled messages", async () => {
-		service.sendStdOut("this is a string\n");
-
-		assert.deepStrictEqual(service.unhandledMessages, ["this is a string\n"]);
-	});
-
 });
