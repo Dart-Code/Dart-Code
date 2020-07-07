a7aba62c32adc3e8e970ea38415778f91920c317
BootBlock <BootBlock@users.noreply.github.com>
2020-07-07 16:24:53 +0100

    Only read first color per line
    Also handle whitespace and multi-line breaking.
    
    Fixes #2606.

--- src/test/flutter/decorations/flutter_colors.test.ts
+++ src/test/flutter/decorations/flutter_colors.test.ts
@@ -13,10 +13,11 @@ import 'package:flutter/cupertino.dart' show CupertinoColors;
 
 final ThemeData base2 = ThemeData(
   indicatorColor: Colors.white,
-  toggleableActiveColor: const Color(0xFF1E88E5),
+  toggleableActiveColor: const Color(
+      0xFF1E88E5),
   splashColor: Colors.black26,
-  canvasColor: Colors.white,
-  errorColor: const Color(0xFFB00020),
+  canvasColor: Colors.white, // Color(0xFFDEADBEEF)  // Colors.blue
+  errorColor: const Color(0xFFB00020), // Colors.orange
   buttonTheme: ButtonThemeData(
     buttonColor: Colors.pinkAccent,
     disabledColor: Colors.purpleAccent.shade400,
@@ -33,8 +34,14 @@ var cupertino = [
 ];
 
 var formats = [
-	Color.fromARGB(255, 30, 30, 30),
-	Color.fromARGB(255, 0x1e, 0x1e, 0x1e)
+    Color.fromARGB(255, 30,
+        30, 30),
+    Color.fromARGB(
+      255,
+      0x1e,
+      0x1e,
+      0x1e,
+    )
 ];
 		`));
 
@@ -49,7 +56,7 @@ var formats = [
 			assert.deepStrictEqual(results[hex].map(rangeString), ranges.map(rangeString), `Incorrect ranges for ${hex}`);
 		};
 		ensureColor("ffffffff", rangesOf("|Colors.white|"));
-		ensureColor("ff1e88e5", rangesOf("|Color(0xFF1E88E5)|"));
+		ensureColor("ff1e88e5", rangesOf("|Color(\n      0xFF1E88E5)|"));
 		ensureColor("42000000", rangesOf("|Colors.black26|"));
 		ensureColor("ffb00020", rangesOf("|Color(0xFFB00020)|"));
 		ensureColor("ffff4081", rangesOf("|Colors.pinkAccent|"));
@@ -63,8 +70,8 @@ var formats = [
 		ensureColor("ffffd426", rangesOf("|CupertinoColors.systemYellow.darkHighContrastElevatedColor|"));
 
 		ensureColor("ff1e1e1e", [
-			rangeOf("|Color.fromARGB(255, 30, 30, 30)|"),
-			rangeOf("|Color.fromARGB(255, 0x1e, 0x1e, 0x1e)|"),
+			rangeOf("|Color.fromARGB(255, 30,\n        30, 30)|"),
+			rangeOf("|Color.fromARGB(\n      255,\n      0x1e,\n      0x1e,\n      0x1e,\n    )|"),
 		]);
 	});
 });
