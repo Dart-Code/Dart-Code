becf5cd6594df53ee89aa2ba10475b8e1d5aa20c
Danny Tuppeny <danny@tuppeny.com>
2020-07-16 11:43:55 +0100

    Reduce default checkEveryMilliseconds to speed up tests a little

--- src/shared/utils/promises.ts
+++ src/shared/utils/promises.ts
@@ -1,6 +1,6 @@
 export const resolvedPromise = Promise.resolve(true);
 
-export async function waitFor<T>(action: () => T | Promise<T>, checkEveryMilliseconds: number = 500, tryForMilliseconds: number = 10000, token?: { isCancellationRequested: boolean }): Promise<T | undefined> {
+export async function waitFor<T>(action: () => T | Promise<T>, checkEveryMilliseconds: number = 100, tryForMilliseconds: number = 10000, token?: { isCancellationRequested: boolean }): Promise<T | undefined> {
 	let timeRemaining = tryForMilliseconds;
 	while (timeRemaining > 0 && !(token && token.isCancellationRequested)) {
 		const res = await action();
