1b5cb98d64aa42cb2b263c6d7e6ba0043c4ae654
Danny Tuppeny <danny@tuppeny.com>
2020-08-11 18:43:17 +0100

    Add suport for launching debug adapters as servers for debugging

--- src/extension/providers/debug_adapter_descriptor_factory.ts
+++ src/extension/providers/debug_adapter_descriptor_factory.ts
@@ -1,11 +1,17 @@
-import { DebugAdapterDescriptor, DebugAdapterDescriptorFactory, DebugAdapterExecutable, DebugSession, ExtensionContext } from "vscode";
-import { getDebugAdapterPath } from "../../shared/vscode/debugger";
+import * as path from "path";
+import { DebugAdapterDescriptor, DebugAdapterDescriptorFactory, DebugAdapterExecutable, DebugAdapterServer, DebugSession, ExtensionContext } from "vscode";
+import { getDebugAdapterPath, getDebugAdapterPort } from "../../shared/utils/debug";
 
 export class DartDebugAdapterDescriptorFactory implements DebugAdapterDescriptorFactory {
 	constructor(private readonly extensionContext: ExtensionContext) { }
 
 	public createDebugAdapterDescriptor(session: DebugSession, executable: DebugAdapterExecutable | undefined): DebugAdapterDescriptor {
 		const scriptPath = getDebugAdapterPath((p) => this.extensionContext.asAbsolutePath(p), session.configuration.debuggerType);
+
+		if (process.env.DART_CODE_USE_DEBUG_SERVERS) {
+			return new DebugAdapterServer(getDebugAdapterPort(path.basename(scriptPath).split(".")[0]));
+		}
+
 		const args = [scriptPath];
 		return new DebugAdapterExecutable("node", args);
 	}
