f5db2fba571706fe97d59e0417a1dc48aeb2dd40
Danny Tuppeny <danny@tuppeny.com>
2020-09-10 17:25:45 +0100

    Re-skip web tests
    Failing with different errors on CI, can be debugging on a branch.

--- src/test/web_debug/debug/web.test.ts
+++ src/test/web_debug/debug/web.test.ts
@@ -7,9 +7,7 @@ import { DartDebugClient } from "../../dart_debug_client";
 import { createDebugClient, ensureVariable, waitAllThrowIfTerminates } from "../../debug_helpers";
 import { activate, closeAllOpenFiles, defer, delay, extApi, getLaunchConfiguration, getPackages, logger, openFile, positionOf, sb, setConfigForTest, waitForResult, watchPromise, webBrokenIndexFile, webBrokenMainFile, webHelloWorldExampleSubFolder, webHelloWorldExampleSubFolderIndexFile, webHelloWorldIndexFile, webHelloWorldMainFile, webProjectContainerFolder } from "../../helpers";
 
-// Skipped due to not quitting
-// https://github.com/dart-lang/webdev/issues/950
-describe("web debugger", () => {
+describe.skip("web debugger", () => {
 	before("get packages (0)", () => getPackages(webHelloWorldIndexFile));
 	before("get packages (1)", () => getPackages(webBrokenIndexFile));
 
