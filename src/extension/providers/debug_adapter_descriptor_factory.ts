395041edd25722225c1b1bd466f44531696e8368
Danny Tuppeny <danny@tuppeny.com>
2020-09-15 15:04:19 +0100

    Add additional logging for debug adapter descriptor factory

--- src/extension/providers/debug_adapter_descriptor_factory.ts
+++ src/extension/providers/debug_adapter_descriptor_factory.ts
@@ -1,17 +1,24 @@
 import { DebugAdapterDescriptor, DebugAdapterDescriptorFactory, DebugAdapterExecutable, DebugAdapterServer, DebugSession, ExtensionContext } from "vscode";
 import { debugAdapterPath } from "../../shared/constants";
+import { DebuggerType } from "../../shared/enums";
+import { Logger } from "../../shared/interfaces";
 import { getDebugAdapterName, getDebugAdapterPort } from "../../shared/utils/debug";
 
 export class DartDebugAdapterDescriptorFactory implements DebugAdapterDescriptorFactory {
-	constructor(private readonly extensionContext: ExtensionContext) { }
+	constructor(private readonly logger: Logger, private readonly extensionContext: ExtensionContext) { }
 
 	public createDebugAdapterDescriptor(session: DebugSession, executable: DebugAdapterExecutable | undefined): DebugAdapterDescriptor {
 		const debuggerName = getDebugAdapterName(session.configuration.debuggerType);
+		this.logger.info(`Using ${debuggerName} debugger for ${DebuggerType[session.configuration.debuggerType]}`);
 
 		if (process.env.DART_CODE_USE_DEBUG_SERVERS) {
-			return new DebugAdapterServer(getDebugAdapterPort(debuggerName));
+			const port = getDebugAdapterPort(debuggerName);
+			this.logger.info(`Running debugger in server mode on port ${port} because DART_CODE_USE_DEBUG_SERVERS is set`);
+			return new DebugAdapterServer(port);
 		}
 
-		return new DebugAdapterExecutable("node", [this.extensionContext.asAbsolutePath(debugAdapterPath), debuggerName]);
+		const args = [this.extensionContext.asAbsolutePath(debugAdapterPath), debuggerName];
+		this.logger.info(`Running debugger via node with ${args.join('    ')}`);
+		return new DebugAdapterExecutable("node", args);
 	}
 }
