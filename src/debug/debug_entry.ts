df0510299e365e28978e5044ce1ade32443573d6
Danny Tuppeny <danny@tuppeny.com>
2020-10-06 16:46:25 +0100

    Record when debug adapter starts / errors

--- src/debug/debug_entry.ts
+++ src/debug/debug_entry.ts
@@ -1,3 +1,6 @@
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
 import { DebugSession } from "vscode-debugadapter";
 import { DartDebugSession } from "./dart_debug_impl";
 import { DartTestDebugSession } from "./dart_test_debug_impl";
@@ -5,6 +8,12 @@ import { FlutterDebugSession } from "./flutter_debug_impl";
 import { FlutterTestDebugSession } from "./flutter_test_debug_impl";
 import { WebDebugSession } from "./web_debug_impl";
 import { WebTestDebugSession } from "./web_test_debug_impl";
+
+const timestamp = Date.now();
+const logFilename = path.join(os.tmpdir(), `dart-code-debug-adapter-log-${timestamp}.txt`);
+const errorLogFilename = path.join(os.tmpdir(), `dart-code-debug-adapter-log-${timestamp}-error.txt`);
+fs.writeFileSync(logFilename, `Dart debug adaper spawned ${process.argv} /  ${process.execArgv} / ${process.execPath}`);
+process.on("uncaughtException", (e) => fs.writeFileSync(errorLogFilename, e));
 
 const args = process.argv.slice(2);
 const debugType = args.length ? args[0] : undefined;
