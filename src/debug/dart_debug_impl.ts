6eb43787c3db859335e77aed7cd0daa020d2a148
Danny Tuppeny <danny@tuppeny.com>
2020-11-10 16:16:40 +0000

    Prevent pub exit exeption from pausing the debugger
    Fixes #2949.

--- src/debug/dart_debug_impl.ts
+++ src/debug/dart_debug_impl.ts
@@ -751,7 +751,7 @@ export class DartDebugSession extends DebugSession {
 	// When shutting down, we may need to remove all breakpoints and resume all threads
 	// to avoid things like waiting for tests to exit that will never exit. We don't wait
 	// for any responses here as if the VM has shut down we won't get them.
-	private async tryRemoveAllBreakpointsAndResumeAllThreads(request: string) {
+	protected async tryRemoveAllBreakpointsAndResumeAllThreads(request: string) {
 		this.log(`${request}: Disabling break-on-exception and removing all breakpoints`);
 		await this.raceIgnoringErrors(() => Promise.all([
 			this.threadManager.setExceptionPauseMode("None"),
