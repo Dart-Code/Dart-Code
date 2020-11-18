51d2c4acd3a64aee3238a96e3136de0be8d7cc98
Danny Tuppeny <danny@tuppeny.com>
2020-11-18 15:10:19 +0000

    Consider things in a /third_party/ folder as external code

--- src/debug/dart_debug_impl.ts
+++ src/debug/dart_debug_impl.ts
@@ -2104,7 +2104,10 @@ export class DartDebugSession extends DebugSession {
 			return false;
 
 		// HACK: Take a guess at whether it's inside the pubcache (in which case we're considering it external).
-		return path.indexOf("/hosted/pub.dartlang.org/") !== -1 || path.indexOf("\\hosted\\pub.dartlang.org\\") !== -1;
+		return path.indexOf("/hosted/pub.dartlang.org/") !== -1
+			|| path.indexOf("\\hosted\\pub.dartlang.org\\") !== -1
+			|| path.indexOf("/third_party/") !== -1
+			|| path.indexOf("\\third_party\\") !== -1;
 	}
 
 	private resolveFileLocation(script: VMScript, tokenPos: number): FileLocation | undefined {
