6f7a92638f9ed9dcfeade81bd8a48a9fdf70c783
BootBlock <BootBlock@users.noreply.github.com>
2021-01-06 18:21:18 +0000

    No longer be runnable during debugging
    The user-exposed Clear Test Results command is not available during an active debug session as there is the potential for a race condition; initial testing shows this isn't the case, but it hasn't been thoroughly investigated as of yet.

--- src/extension/views/test_view.ts
+++ src/extension/views/test_view.ts
@@ -31,7 +31,14 @@ export class TestResultsProvider implements vs.Disposable, vs.TreeDataProvider<T
 		this.disposables.push(vs.commands.registerCommand("dart.startWithoutDebuggingFailedTests", (treeNode: SuiteNode | GroupNode | TestNode) => this.runTests(treeNode, this.getTestNames(treeNode, true), false, false)));
 		this.disposables.push(vs.commands.registerCommand("dart.runAllFailedTestsWithoutDebugging", () => this.runAllFailedTests()));
 
-		this.disposables.push(vs.commands.registerCommand("dart.clearTestResults", () => this.data.clearAllResults()));
+		this.disposables.push(vs.commands.registerCommand("dart.clearTestResults", () => {
+			// Avoids any potential race condition until scenario thoroughly investigated.
+			if (vs.debug.activeDebugSession)
+				return;
+
+			return this.data.clearAllResults();
+		}));
+
 		this.disposables.push(vs.commands.registerCommand("_dart.displaySuite", (treeNode: SuiteNode) => vs.commands.executeCommand("_dart.jumpToLineColInUri", vs.Uri.file(treeNode.suiteData.path))));
 		this.disposables.push(vs.commands.registerCommand("_dart.displayGroup", (treeNode: GroupNode) => {
 			if (!treeNode.path)
