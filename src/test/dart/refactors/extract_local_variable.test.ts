b8262bd13aeb4000bc3d2188a99fbc55950c94d9
Danny Tuppeny <danny@tuppeny.com>
2021-02-17 17:55:32 +0000

    Fix up Extract-Local-Variable test for LSP

--- src/test/dart/refactors/extract_local_variable.test.ts
+++ src/test/dart/refactors/extract_local_variable.test.ts
@@ -1,13 +1,10 @@
+import * as assert from "assert";
 import * as vs from "vscode";
 import { activate, currentDoc, ensureTestContent, extApi, rangeOf, setTestContent } from "../../helpers";
 
 describe("refactor", () => {
 
 	beforeEach("activate", () => activate());
-	beforeEach("skip for LSP", async function () {
-		if (extApi.isLsp)
-			this.skip();
-	});
 
 	it("can extract simple code into a local variable", async () => {
 		await setTestContent(`
@@ -15,13 +12,24 @@ String name() {
   return "Danny";
 }
 		`);
-		await (vs.commands.executeCommand("_dart.performRefactor", currentDoc(), rangeOf(`|"Danny"|`), "EXTRACT_LOCAL_VARIABLE"));
-		// Incorrect indenting is due to https://github.com/Microsoft/vscode/issues/63129
-		// When that's fixed, this test will fail and we can fix it up.
+
+		const actions = await (vs.commands.executeCommand("vscode.executeCodeActionProvider", currentDoc().uri, rangeOf(`|"Danny"|`)) as Thenable<vs.CodeAction[]>);
+		assert.ok(actions);
+		assert.ok(actions.length);
+
+		const extractLocalAction = actions.find((r) => r.title.indexOf("Extract Local Variable") !== -1);
+		assert.ok(extractLocalAction, "Action was not found");
+
+		await (vs.commands.executeCommand(extractLocalAction.command!.command, ...extractLocalAction.command!.arguments || []));
+
+		// Incorrect indenting for non-LSP is due to https://github.com/Microsoft/vscode/issues/63129
+		// When that's fixed, this variable can be removed.
+		const extraIndent = extApi.isLsp ? "" : "  ";
+
 		await ensureTestContent(`
 String name() {
   var s = "Danny";
-    return s;
+  ${extraIndent}return s;
 }
 		`);
 	});
