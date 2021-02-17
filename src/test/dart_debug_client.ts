b486dbe499899397f5559b36fefbfd2c6fe46ff5
Danny Tuppeny <danny@tuppeny.com>
2021-02-17 16:49:20 +0000

    Fix a possible source of unhandled rejected promises in tests
    If this waitForEvent() timed out, the error wouldn't be handled and could lead to a vague "UnhandledRejectedPromise" error.

--- src/test/dart_debug_client.ts
+++ src/test/dart_debug_client.ts
@@ -129,8 +129,9 @@ export class DartDebugClient extends DebugClient {
 			workspaceFolder: undefined,
 		};
 		this.debugCommands.handleDebugSessionStart(this.currentSession);
-		// tslint:disable-next-line: no-floating-promises
-		this.waitForEvent("terminated").then(() => this.debugCommands.handleDebugSessionEnd(this.currentSession!));
+		this.waitForEvent("terminated")
+			.then(() => this.debugCommands.handleDebugSessionEnd(this.currentSession!))
+			.catch((e) => console.error(`Error while waiting for termiantion: ${e}`));
 
 		// We override the base method to swap for attachRequest when required, so that
 		// all the existing methods that provide useful functionality but assume launching
