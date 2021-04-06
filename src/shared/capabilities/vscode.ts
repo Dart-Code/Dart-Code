724506b0504eba97f833e7d143ee9e4652c88c93
Danny Tuppeny <danny@tuppeny.com>
2021-04-06 18:40:51 +0100

    Remove a Theia workaround that's no longer required

--- src/shared/capabilities/vscode.ts
+++ src/shared/capabilities/vscode.ts
@@ -1,13 +1,13 @@
 import { version as codeVersion } from "vscode";
 import { versionIsAtLeast } from "../utils";
-import { isTheia } from "../vscode/utils_cloud";
 
 export class CodeCapabilities {
 	public version: string;
+
 	constructor(version: string) {
 		this.version = version;
 	}
-	get supportsResolveDebugConfigurationWithSubstitutedVariables() { return !isTheia; }
+
 	// This version should match the minimum the LSP client we're using supports.
 	// https://github.com/microsoft/vscode-languageserver-node/blob/main/client/src/node/main.ts#L25
 	get supportsLatestLspClient() { return versionIsAtLeast(this.version, "1.52.0"); }
