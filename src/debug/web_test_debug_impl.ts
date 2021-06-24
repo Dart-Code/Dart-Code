417a0a3fce0ec4f0817c2bdece8462ba091cab23
Danny Tuppeny <danny@tuppeny.com>
2021-06-24 15:26:24 +0100

    Build fixes

--- src/debug/web_test_debug_impl.ts
+++ src/debug/web_test_debug_impl.ts
@@ -1,10 +1,10 @@
-import { DartLaunchRequestArguments } from "../shared/debug/interfaces";
+import { DartLaunchArgs } from "../shared/debug/interfaces";
 import { SpawnedProcess } from "../shared/interfaces";
 import { DartTestDebugSession } from "./dart_test_debug_impl";
 
 export class WebTestDebugSession extends DartTestDebugSession {
 
-	protected async spawnProcess(args: DartLaunchRequestArguments): Promise<SpawnedProcess> {
+	protected async spawnProcess(args: DartLaunchArgs): Promise<SpawnedProcess> {
 		// TODO: This!
 		throw new Error("NYI");
 	}
