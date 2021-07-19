a400bad23c58463821e81e458aa9706da2aaa305
Danny Tuppeny <danny@tuppeny.com>
2021-07-19 10:18:43 +0100

    Remove .only()

--- src/test/dart/pub/add.test.ts
+++ src/test/dart/pub/add.test.ts
@@ -1,9 +1,25 @@
 import { activate } from "../../helpers";
 
-describe.only("pub add", () => {
+describe("pub add", () => {
 	beforeEach("activate", () => activate());
 
-	describe("cache", () => {
+	describe.skip("can add a dependency using command", () => {
+
+	});
+
+	describe.skip("can add a dev-dependency using command", () => {
+
+	});
+
+	describe.skip("can add a Flutter SDK dependency using command", () => {
+
+	});
+
+	describe.skip("can add from a quick fix if not listed in pubspec.yaml", () => {
+
+	});
+
+	describe.skip("cannot add from a quick fix if already listed in pubspec.yaml", () => {
 
 	});
 });
