b4882e4f0c2af0bfdb0c49b9f38bab87d82fa74f
Danny Tuppeny <danny@tuppeny.com>
2021-09-22 10:48:17 +0100

    Add debouncing to test discovery update

--- src/extension/lsp/test_discoverer.ts
+++ src/extension/lsp/test_discoverer.ts
@@ -11,7 +11,10 @@ import { LspFileTracker } from "../analysis/file_tracker_lsp";
 import { isTestFile } from "../utils";
 
 export class TestDiscoverer implements IAmDisposable {
-	private disposables: IAmDisposable[] = [];
+	private readonly disposables: IAmDisposable[] = [];
+
+	private readonly debounceTimers: { [key: string]: NodeJS.Timeout } = {};
+	private readonly debounceDuration = 1500;
 
 	constructor(private readonly logger: Logger, fileTracker: LspFileTracker, private readonly model: TestModel) {
 		this.disposables.push(fileTracker.onOutline.listen((o) => this.handleOutline(o)));
@@ -19,13 +22,17 @@ export class TestDiscoverer implements IAmDisposable {
 
 	private handleOutline(outline: OutlineParams) {
 		const suitePath = forceWindowsDriveLetterToUppercase(uriToFilePath(outline.uri));
-		if (isTestFile(suitePath)) {
-			// Force creation of a node.
-			const [suite, didCreate] = this.model.getOrCreateSuite(suitePath);
-			const stack: Array<{ id: number, name: string }> = [];
-			const model = this.model;
 
-			let id = 1;
+		const existingTimeout = this.debounceTimers[suitePath];
+		if (existingTimeout)
+			clearTimeout(existingTimeout);
+		this.debounceTimers[suitePath] = setTimeout(() => this.rebuildFromOutline(suitePath, outline), this.debounceDuration);
+	}
+
+	private rebuildFromOutline(suitePath: string, outline: OutlineParams) {
+		if (isTestFile(suitePath)) {
+			// Force creation of a node if it's not already there.
+			const [suite, _] = this.model.getOrCreateSuite(suitePath);
 
 			// Generate a unique ID for these IDs to be owned by so that they can be looked
 			// up independent of any other ongoing runs.
@@ -33,53 +40,61 @@ export class TestDiscoverer implements IAmDisposable {
 
 			// Mark everything in the suite as potentially-deleted so that we can detect anything
 			// that was not present in the new list to remove it afterwards.
-			model.markAllAsPotentiallyDeleted(suite, TestSource.Outline);
+			this.model.markAllAsPotentiallyDeleted(suite, TestSource.Outline);
 
-			const visitor = new class extends LspOutlineVisitor {
-				protected visitUnitTestTest(outline: Outline) {
-					this.handleItem(outline, false, super.visitUnitTestTest);
-				}
-				protected visitUnitTestGroup(outline: Outline) {
-					this.handleItem(outline, true, super.visitUnitTestGroup);
-				}
-
-				private handleItem(outline: Outline, isGroup: boolean, base: (outline: Outline) => void) {
-					const name = extractTestNameFromOutline(outline.element.name);
-					if (!name || !outline.element.range)
-						return;
-
-					const range = outline.codeRange || outline.range || (outline.element ? outline.element.range : undefined);
-					const parent = stack.length > 0 ? stack[stack.length - 1] : undefined;
-
-					const fullName = parent?.name
-						? `${parent.name} ${name}`
-						: name;
-
-					const thisID = id++;
-					if (isGroup)
-						model.groupDiscovered(dartCodeDebugSessionID, suitePath, TestSource.Outline, thisID, fullName, parent?.id, undefined, range);
-					else
-						model.testDiscovered(dartCodeDebugSessionID, suitePath, TestSource.Outline, thisID, fullName, parent?.id, undefined, range, undefined);
-
-					if (isGroup)
-						stack.push({ id: thisID, name: fullName });
-					try {
-						base.bind(this)(outline);
-					} finally {
-						if (isGroup)
-							stack.pop();
-					}
-				}
-			}(this.logger);
+			const visitor = new TestDiscoveryVisitor(this.logger, this.model, dartCodeDebugSessionID, suitePath);
 			visitor.visit(outline.outline);
 
-			model.removeAllPotentiallyDeletedNodes(suite);
-
+			this.model.removeAllPotentiallyDeletedNodes(suite);
 			this.model.updateNode();
 		}
 	}
 
 	public dispose(): any {
 		disposeAll(this.disposables);
+	}
+}
+
+class TestDiscoveryVisitor extends LspOutlineVisitor {
+	private readonly stack: Array<{ id: number, name: string }> = [];
+	private id = 1;
+
+	constructor(logger: Logger, private readonly model: TestModel, private readonly dartCodeDebugSessionID: string, private readonly suitePath: string) {
+		super(logger);
+	}
+
+	protected visitUnitTestTest(outline: Outline) {
+		this.handleItem(outline, false, super.visitUnitTestTest);
+	}
+	protected visitUnitTestGroup(outline: Outline) {
+		this.handleItem(outline, true, super.visitUnitTestGroup);
+	}
+
+	private handleItem(outline: Outline, isGroup: boolean, base: (outline: Outline) => void) {
+		const name = extractTestNameFromOutline(outline.element.name);
+		if (!name || !outline.element.range)
+			return;
+
+		const range = outline.codeRange || outline.range || (outline.element ? outline.element.range : undefined);
+		const parent = this.stack.length > 0 ? this.stack[this.stack.length - 1] : undefined;
+
+		const fullName = parent?.name
+			? `${parent.name} ${name}`
+			: name;
+
+		const thisID = this.id++;
+		if (isGroup)
+			this.model.groupDiscovered(this.dartCodeDebugSessionID, this.suitePath, TestSource.Outline, thisID, fullName, parent?.id, undefined, range);
+		else
+			this.model.testDiscovered(this.dartCodeDebugSessionID, this.suitePath, TestSource.Outline, thisID, fullName, parent?.id, undefined, range, undefined);
+
+		if (isGroup)
+			this.stack.push({ id: thisID, name: fullName });
+		try {
+			base.bind(this)(outline);
+		} finally {
+			if (isGroup)
+				this.stack.pop();
+		}
 	}
 }
