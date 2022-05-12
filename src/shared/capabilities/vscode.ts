f9815fe860c80d021c7a67b1d73b3bbb4b35ee0e
Danny Tuppeny <danny@tuppeny.com>
2022-05-12 10:10:38 +0100

    Update dependencies

--- src/shared/capabilities/vscode.ts
+++ src/shared/capabilities/vscode.ts
@@ -11,7 +11,7 @@ export class CodeCapabilities {
 
 	// This version should match the minimum the LSP client we're using supports.
 	// https://github.com/microsoft/vscode-languageserver-node/blob/main/client/src/node/main.ts#L25
-	get supportsLatestLspClient() { return versionIsAtLeast(this.version, "1.52.0"); }
+	get supportsLatestLspClient() { return versionIsAtLeast(this.version, "1.67.0"); }
 	// Theia doesn't currently support launching without a launch.json. This may need updating to also
 	// check the version in future.
 	get supportsDebugWithoutLaunchJson() { return !isTheia; }
