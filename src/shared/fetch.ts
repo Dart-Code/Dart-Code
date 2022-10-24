6cbf014aec94871c04bc5c8b886c9750ab943ab4
Danny Tuppeny <danny@tuppeny.com>
2022-10-24 11:24:19 +0100

    Add support for redirects in survey/etc.

--- src/shared/fetch.ts
+++ src/shared/fetch.ts
@@ -34,30 +34,42 @@ export class WebClient {
 				port,
 			};
 
-			const req = https.request(options, (resp) => {
-				if (!resp || !resp.statusCode || resp.statusCode < 200 || resp.statusCode > 300) {
-					reject({ message: `Failed to get ${path}: ${resp && resp.statusCode}: ${resp && resp.statusMessage}` });
-				} else {
-					const chunks: any[] = [];
-					resp.on("data", (b) => chunks.push(b));
-					resp.on("end", () => {
-						const buffer = Buffer.concat(chunks);
-						const encoding = resp.headers["content-encoding"];
-						if (encoding === "gzip") {
-							zlib.gunzip(buffer, (err, decoded) => {
-								if (err)
-									reject(err);
-								else
-									resolve(decoded?.toString());
-							});
-						} else {
-							resolve(buffer.toString());
-						}
-					});
-				}
-			});
+			const req = https.request(options, (res) => this.handleResponse(headers, res, resolve, reject, path));
 			req.end();
 		});
+	}
+
+	private handleResponse(headers: http.OutgoingHttpHeaders, resp: http.IncomingMessage, resolve: (value: string | PromiseLike<string>) => void, reject: (reason?: any) => void, path: string | undefined): void {
+		if (!resp || !resp.statusCode) {
+			reject({ message: `Failed to get ${path}: ${resp && resp.statusMessage}` });
+		} else if (resp.statusCode >= 301 && resp.statusCode <= 302) {
+			const newLocation = resp.headers.location;
+			if (!newLocation) {
+				reject({ message: `Redirect with no 'location' header for ${path}: ${resp && resp.statusCode}: ${resp && resp.statusMessage}` });
+			} else {
+				resolve(this.fetch(newLocation, headers));
+			}
+		} else if (resp.statusCode < 200 || resp.statusCode > 300) {
+			reject({ message: `Bad status code for ${path}: ${resp && resp.statusCode}: ${resp && resp.statusMessage}` });
+		} else {
+			const chunks: any[] = [];
+			resp.on("data", (b) => chunks.push(b));
+			resp.on("end", () => {
+				const buffer = Buffer.concat(chunks);
+				const encoding = resp.headers["content-encoding"];
+				if (encoding === "gzip") {
+					zlib.gunzip(buffer, (err, decoded) => {
+						if (err)
+							reject(err);
+
+						else
+							resolve(decoded?.toString());
+					});
+				} else {
+					resolve(buffer.toString());
+				}
+			});
+		}
 	}
 
 	private fetchHttp(hostname: string | undefined, port: string | undefined, path: string | undefined, headers: http.OutgoingHttpHeaders = {}): Promise<string> {
@@ -73,19 +85,9 @@ export class WebClient {
 				port,
 			};
 
-			const req = http.request(options, (resp) => {
-				if (!resp || !resp.statusCode || resp.statusCode < 200 || resp.statusCode > 300) {
-					reject({ message: `Failed to get ${path}: ${resp && resp.statusCode}: ${resp && resp.statusMessage}` });
-				} else {
-					const chunks: string[] = [];
-					resp.on("data", (b: Buffer | string) => chunks.push(b.toString()));
-					resp.on("end", () => {
-						const data = chunks.join("");
-						resolve(data);
-					});
-				}
-			});
+			const req = http.request(options, (res) => this.handleResponse(headers, res, resolve, reject, path));
 			req.end();
 		});
 	}
 }
+
