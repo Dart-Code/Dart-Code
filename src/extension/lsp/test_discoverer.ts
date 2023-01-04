52041b9f8e854128165ff53ac119df5cae8ac12b
Danny Tuppeny <danny@tuppeny.com>
2023-01-04 11:32:33 +0000

    Improve toggling of skipped tests

--- src/extension/lsp/test_discoverer.ts
+++ src/extension/lsp/test_discoverer.ts
@@ -155,7 +155,7 @@ export class TestDiscoverer implements IAmDisposable {
 			visitor.visit(outline);
 
 			this.model.removeAllPotentiallyDeletedNodes(suite);
-			this.model.updateSuiteTestCountLabels(suite);
+			this.model.updateSuiteTestCountLabels(suite, false);
 		}
 	}
 
