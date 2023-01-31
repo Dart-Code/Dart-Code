ea87691774d06ab1efd95d27afb3f8f72d352e6d
Danny Tuppeny <danny@tuppeny.com>
2023-01-31 12:59:02 +0000

    Add an option to toggle Decimal/Hex display from Variables context menu

--- src/extension/providers/debug_adapter_hex_view_factory.ts
+++ src/extension/providers/debug_adapter_hex_view_factory.ts
@@ -1,4 +1,5 @@
 import * as vs from "vscode";
+import { SUPPORTS_DEBUG_VALUE_FORMAT } from "../../shared/constants";
 import { IAmDisposable, Logger } from "../../shared/interfaces";
 import { disposeAll } from "../../shared/utils";
 import { config } from "../config";
@@ -17,17 +18,28 @@ class DartDebugAdapterHexView implements vs.DebugAdapterTracker {
 
 	constructor(private readonly logger: Logger, private readonly session: vs.DebugSession) {
 		this.disposables.push(vs.workspace.onDidChangeConfiguration(this.handleConfigChange, this));
+		this.disposables.push(vs.commands.registerCommand("_dart.showDebuggerNumbersAsHex", () => this.setFormatHex(true)));
+		this.disposables.push(vs.commands.registerCommand("_dart.showDebuggerNumbersAsDecimal", () => this.setFormatHex(false)));
+	}
+
+	private async setFormatHex(enabled: boolean) {
+		await config.setShowDebuggerNumbersAsHex(enabled ? true : undefined);
+		await this.invalidate();
 	}
 
 	private async handleConfigChange(conf: vs.ConfigurationChangeEvent) {
 		if (conf.affectsConfiguration("dart.showDebuggerNumbersAsHex")) {
 			if (this.supportsFormatting) {
-				try {
-					await this.session.customRequest("_invalidateAreas", { areas: ["variables"] });
-				} catch (e) {
-					this.logger.error(e);
-				}
+				await this.invalidate();
 			}
+		}
+	}
+
+	private async invalidate() {
+		try {
+			await this.session.customRequest("_invalidateAreas", { areas: ["variables"] });
+		} catch (e) {
+			this.logger.error(e);
 		}
 	}
 
@@ -38,6 +50,7 @@ class DartDebugAdapterHexView implements vs.DebugAdapterTracker {
 	onDidSendMessage(message: any): void {
 		if (!this.supportsFormatting && message?.command === "initialize" && message?.body?.supportsValueFormattingOptions) {
 			this.supportsFormatting = true;
+			vs.commands.executeCommand("setContext", SUPPORTS_DEBUG_VALUE_FORMAT, true);
 		}
 	}
 
