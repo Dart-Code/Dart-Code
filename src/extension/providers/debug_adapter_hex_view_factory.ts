aebfce93fdda3f7e44880e2a5cb681fed7281263
Danny Tuppeny <danny@tuppeny.com>
2023-01-31 15:16:26 +0000

    Handle double-registration of commands

--- src/extension/providers/debug_adapter_hex_view_factory.ts
+++ src/extension/providers/debug_adapter_hex_view_factory.ts
@@ -4,38 +4,51 @@ import { IAmDisposable, Logger } from "../../shared/interfaces";
 import { disposeAll } from "../../shared/utils";
 import { config } from "../config";
 
-export class DartDebugAdapterHexViewFactory implements vs.DebugAdapterTrackerFactory {
-	constructor(private readonly logger: Logger) { }
-	createDebugAdapterTracker(session: vs.DebugSession): vs.DebugAdapterTracker {
-		return new DartDebugAdapterHexView(this.logger, session);
-	}
-}
+export class DartDebugAdapterHexViewFactory implements vs.DebugAdapterTrackerFactory, IAmDisposable {
+	protected readonly disposables: IAmDisposable[] = [];
+	public readonly hexFormatters = new Set<DartDebugAdapterHexView>();
 
-class DartDebugAdapterHexView implements vs.DebugAdapterTracker {
-	private supportsFormatting = false;
+	public supportsFormatting = false;
 
-	protected disposables: IAmDisposable[] = [];
-
-	constructor(private readonly logger: Logger, private readonly session: vs.DebugSession) {
+	constructor(private readonly logger: Logger) {
 		this.disposables.push(vs.workspace.onDidChangeConfiguration(this.handleConfigChange, this));
 		this.disposables.push(vs.commands.registerCommand("_dart.showDebuggerNumbersAsHex", () => this.setFormatHex(true)));
 		this.disposables.push(vs.commands.registerCommand("_dart.showDebuggerNumbersAsDecimal", () => this.setFormatHex(false)));
 	}
 
-	private async setFormatHex(enabled: boolean) {
-		await config.setShowDebuggerNumbersAsHex(enabled ? true : undefined);
-		await this.invalidate();
+	createDebugAdapterTracker(session: vs.DebugSession): vs.DebugAdapterTracker {
+		const hexFormatter = new DartDebugAdapterHexView(this, this.logger, session);
+		this.hexFormatters.add(hexFormatter);
+		return hexFormatter;
 	}
 
 	private async handleConfigChange(conf: vs.ConfigurationChangeEvent) {
 		if (conf.affectsConfiguration("dart.showDebuggerNumbersAsHex")) {
 			if (this.supportsFormatting) {
-				await this.invalidate();
+				await this.invalidateAll();
 			}
 		}
 	}
 
-	private async invalidate() {
+	private async setFormatHex(enabled: boolean) {
+		await config.setShowDebuggerNumbersAsHex(enabled ? true : undefined);
+		await this.invalidateAll();
+	}
+
+	private async invalidateAll() {
+		await Promise.all([...this.hexFormatters].map((formatter) => formatter.invalidate()));
+	}
+
+	public dispose(): any {
+		this.hexFormatters.clear();
+		disposeAll(this.disposables);
+	}
+}
+
+class DartDebugAdapterHexView implements vs.DebugAdapterTracker {
+	constructor(private readonly factory: DartDebugAdapterHexViewFactory, private readonly logger: Logger, private readonly session: vs.DebugSession) { }
+
+	public async invalidate() {
 		try {
 			await this.session.customRequest("_invalidateAreas", { areas: ["variables"] });
 		} catch (e) {
@@ -44,18 +57,18 @@ class DartDebugAdapterHexView implements vs.DebugAdapterTracker {
 	}
 
 	onExit(code: number | undefined, signal: string | undefined): void {
-		disposeAll(this.disposables);
+		this.factory.hexFormatters.delete(this);
 	}
 
 	onDidSendMessage(message: any): void {
-		if (!this.supportsFormatting && message?.command === "initialize" && message?.body?.supportsValueFormattingOptions) {
-			this.supportsFormatting = true;
+		if (!this.factory.supportsFormatting && message?.command === "initialize" && message?.body?.supportsValueFormattingOptions) {
+			this.factory.supportsFormatting = true;
 			vs.commands.executeCommand("setContext", SUPPORTS_DEBUG_VALUE_FORMAT, true);
 		}
 	}
 
 	onWillReceiveMessage(message: any): void {
-		if (this.supportsFormatting && (message.command === "evaluate" || message.command === "variables")) {
+		if (this.factory.supportsFormatting && (message.command === "evaluate" || message.command === "variables")) {
 			message.arguments ??= {};
 			message.arguments.format ??= {};
 			// Don't override if we happen to be a future version of VS Code that supports this.
