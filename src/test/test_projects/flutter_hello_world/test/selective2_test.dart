844a6f886eaf9df24949eaad82ace8b6e7ab9cba
Danny Tuppeny <danny@tuppeny.com>
2023-04-19 14:43:45 +0100

    Add tests that run multiple tests from multiple suites via the VS Code runner

---
+++ src/test/test_projects/flutter_hello_world/test/selective2_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('pass one', (WidgetTester tester) async {
+    expect(1, 1);
+  });
+
+  testWidgets('pass two', (WidgetTester tester) async {
+    expect(1, 1);
+  });
+
+  testWidgets('fail one', (WidgetTester tester) async {
+    expect(1, 1);
+  });
+
+  testWidgets('fail two', (WidgetTester tester) async {
+    expect(1, 1);
+  });
+}
---
+++ src/test/test_projects/hello_world/test/selective1_test.dart
@@ -0,0 +1,19 @@
+import "package:test/test.dart";
+
+void main() {
+  test('pass one', () {
+    expect(1, 1);
+  });
+
+  test('pass two', () {
+    expect(1, 1);
+  });
+
+  test('fail one', () {
+    expect(1, 1);
+  });
+
+  test('fail two', () {
+    expect(1, 1);
+  });
+}
