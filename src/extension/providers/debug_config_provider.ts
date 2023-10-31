Dart-Code/Dart-Code

commit 74fd1ff0b6968cd59d3a03d2472897c25de482de

Use top level folder name instead of finding bin/tool/etc. anywhere in the path

src/extension/providers/debug_config_provider.ts
@@ -312,19 +312,19 @@ export class DebugConfigProvider implements DebugConfigurationProvider {
 		const isIntegrationTest = debugConfig.program && isInsideFolderNamed(debugConfig.program, "integration_test");
 
 		let debugType = DebuggerType.Dart;
-		if (debugConfig.cwd
-			// TODO: This isInsideFolderNamed often fails when we found a better project root above.
-			&& !isInsideFolderNamed(debugConfig.program, "bin")
-			&& !isInsideFolderNamed(debugConfig.program, "tool")
-			&& !isInsideFolderNamed(debugConfig.program, ".dart_tool")) {
-			// Check if we're a Flutter or Web project.
-			if (isFlutterProjectFolder(debugConfig.cwd) || this.wsContext.config.forceFlutterDebug) {
-				debugType = DebuggerType.Flutter;
-			} else if (isInsideFolderNamed(debugConfig.program, "web") && !isInsideFolderNamed(debugConfig.program, "test"))
-				debugType = DebuggerType.Web;
-
-			else
-				logger.info(`Project (${debugConfig.program}) not recognised as Flutter or Web, will use Dart debugger`);
+		if (debugConfig.cwd && debugConfig.program && isWithinPath(debugConfig.program, debugConfig.cwd)) {
+			const relativePath = path.relative(debugConfig.cwd, debugConfig.program);
+			const firstPathSegment = relativePath.split(path.sep)[0];
+			if (firstPathSegment !== "bin" && firstPathSegment !== "tool" && firstPathSegment !== ".dart_tool") {
+				// Check if we're a Flutter or Web project.
+				if (isFlutterProjectFolder(debugConfig.cwd) || this.wsContext.config.forceFlutterDebug) {
+					debugType = DebuggerType.Flutter;
+				} else if (firstPathSegment === "web")
+					debugType = DebuggerType.Web;
+				else
+					logger.info(`Program (${debugConfig.program}) not recognised as Flutter or Web, will use Dart debugger`);
+			}
+			logger.info(`Program is 'bin', 'tool' or '.dart_tool' so will use Dart debugger`);
 		}
 		logger.info(`Detected launch project as ${DebuggerType[debugType]}`);
 

src/test/dart_debug/debugger_type.test.ts
@@ -21,6 +21,7 @@ describe("dart debugger", () => {
 			"lib/temp1_test.dart": DebuggerType.Dart,
 			"lib/temp2_test.dart*": DebuggerType.DartTest, // Special case for allowTestsOutsideTestFolder
 			"test/temp_test.dart": DebuggerType.DartTest,
+			"test/tool/temp_tool_test.dart": DebuggerType.DartTest,
 			"tool/temp_tool.dart": DebuggerType.Dart,
 		};
 		for (let testPath of Object.keys(tests)) {

src/test/flutter_debug/debugger_type.test.ts
@@ -27,8 +27,7 @@ describe(`flutter debugger`, () => {
 			"lib/temp1_test.dart": DebuggerType.Flutter,
 			"lib/temp2_test.dart*": DebuggerType.FlutterTest, // Special case for allowTestsOutsideTestFolder
 			"test/temp_test.dart": DebuggerType.FlutterTest,
-			// TODO(dantup): This is broken. https://github.com/Dart-Code/Dart-Code/issues/4809
-			// "test/tool/temp_tool.dart": DebuggerType.FlutterTest,
+			"test/tool/temp_tool_test.dart": DebuggerType.FlutterTest,
 			"tool/temp_tool.dart": DebuggerType.Dart,
 		};
 		for (let testPath of Object.keys(tests)) {

commit 67434785e440612cf4415bb739b1082bfd45e4a4

Fix attaching to Flutter app with no program
This was broken by 74fd1ff0b6968cd59d3a03d2472897c25de482de and causing all attach tests to fail

src/extension/providers/debug_config_provider.ts
@@ -312,9 +312,9 @@ export class DebugConfigProvider implements DebugConfigurationProvider {
 		const isIntegrationTest = debugConfig.program && isInsideFolderNamed(debugConfig.program, "integration_test");
 
 		let debugType = DebuggerType.Dart;
-		if (debugConfig.cwd && debugConfig.program && isWithinPath(debugConfig.program, debugConfig.cwd)) {
-			const relativePath = path.relative(debugConfig.cwd, debugConfig.program);
-			const firstPathSegment = relativePath.split(path.sep)[0];
+		if (debugConfig.cwd && (!debugConfig.program || isWithinPath(debugConfig.program, debugConfig.cwd))) {
+			const relativePath = debugConfig.program ? path.relative(debugConfig.cwd, debugConfig.program) : undefined;
+			const firstPathSegment = relativePath?.split(path.sep)[0];
 			if (firstPathSegment !== "bin" && firstPathSegment !== "tool" && firstPathSegment !== ".dart_tool") {
 				// Check if we're a Flutter or Web project.
 				if (isFlutterProjectFolder(debugConfig.cwd) || this.wsContext.config.forceFlutterDebug) {
@@ -323,8 +323,8 @@ export class DebugConfigProvider implements DebugConfigurationProvider {
 					debugType = DebuggerType.Web;
 				else
 					logger.info(`Program (${debugConfig.program}) not recognised as Flutter or Web, will use Dart debugger`);
-			}
-			logger.info(`Program is 'bin', 'tool' or '.dart_tool' so will use Dart debugger`);
+			} else
+				logger.info(`Program is 'bin', 'tool' or '.dart_tool' so will use Dart debugger`);
 		}
 		logger.info(`Detected launch project as ${DebuggerType[debugType]}`);
 