f40a3de412d90558a2843b8d250d8f39e69e19de
Danny Tuppeny <danny@tuppeny.com>
2023-11-16 12:43:24 +0000

    Fix Flutter completion tests for new completion labels

--- src/test/flutter/providers/completion_item_provider.test.ts
+++ src/test/flutter/providers/completion_item_provider.test.ts
@@ -1,6 +1,6 @@
 import { strict as assert } from "assert";
 import * as vs from "vscode";
-import { activate, ensureCompletion, extApi, flutterHelloWorldMainFile, getCompletionsAt, getPackages, openFile, setTestContent } from "../../helpers";
+import { activate, completionLabel, ensureCompletion, extApi, flutterHelloWorldMainFile, getCompletionsAt, getPackages, openFile, setTestContent } from "../../helpers";
 
 describe("completion_item_provider", () => {
 
@@ -37,9 +37,16 @@ main() {
 }
 		`);
 			const completions = await getCompletionsAt("EdgeInsetsDirecti^", { requireComplete: true });
-			const edgeInsetsCompletions = completions.filter((c) => c.label === "EdgeInsetsDirectional");
+			const edgeInsetsCompletions = completions.filter((c) => completionLabel(c) === "EdgeInsetsDirectional");
 			// We should get at least 5 because it's in rendering, painting, widgets, material, cupertino.
-			assert.equal(edgeInsetsCompletions.length >= 5, true);
+			assert.equal(
+				edgeInsetsCompletions.length >= 5,
+				true,
+				[
+					"Expected at least 5 EdgeInsetsDirectional completions (rendering, painting, widgets, material, cupertino) but only found:",
+					...edgeInsetsCompletions.map(completionLabel),
+				].join("\n    "),
+			);
 		});
 
 		it("does not include overlapping unimported symbols from multiple files if one is already imported", async () => {
@@ -51,7 +58,7 @@ main() {
 }
 		`);
 			const completions = await getCompletionsAt("EdgeInsetsDirecti^");
-			const edgeInsetsCompletions = completions.filter((c) => c.label === "EdgeInsetsDirectional");
+			const edgeInsetsCompletions = completions.filter((c) => completionLabel(c) === "EdgeInsetsDirectional");
 			// We should only get one from the already imported file.
 			assert.equal(edgeInsetsCompletions.length, 1);
 		});
@@ -67,7 +74,7 @@ main() {
 }
 		`);
 			const completions = await getCompletionsAt("TextOverflo^");
-			const clipCompletion = completions.filter((c) => c.label === "TextOverflow.clip");
+			const clipCompletion = completions.filter((c) => completionLabel(c) === "TextOverflow.clip");
 			// We should only get one from the already imported file.
 			assert.equal(clipCompletion.length, 1);
 		});
