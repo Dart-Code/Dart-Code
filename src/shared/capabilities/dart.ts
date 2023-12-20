643186afe1693d68443c0a90a2d8b120606c2f3b
Danny Tuppeny <danny@tuppeny.com>
2023-12-20 13:22:34 +0000

    Update Dart version for extension recommendations

--- src/shared/capabilities/dart.ts
+++ src/shared/capabilities/dart.ts
@@ -59,8 +59,7 @@ export class DartCapabilities {
 	get supportsShowTodoArray() { return versionIsAtLeast(this.version, "2.16.0-0"); }
 	get sdkDapProvidesExceptionText() { return versionIsAtLeast(this.version, "2.18.0-265"); }
 	get supportsSetIsolatePauseModeForWeb() { return versionIsAtLeast(this.version, "2.19.0"); }
-	// TODO: Get version number of the Dart SDK that includes >= devtools_shared 6.0.3 for the path/URI fixes.
-	get supportsDevToolsVsCodeExtensions() { return versionIsAtLeast(this.version, "9.9.9"); }
+	get supportsDevToolsVsCodeExtensions() { return versionIsAtLeast(this.version, "3.3.0-247"); }
 	// TODO: Update these (along with Flutter) when supported.
 	get webSupportsEvaluation() { return false; }
 	get webSupportsDebugging() { return true; }
