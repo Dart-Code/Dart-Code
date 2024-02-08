2752ac320e42f744502ee26c4db850e5241f16b8
Danny Tuppeny <danny@tuppeny.com>
2024-02-08 15:13:22 +0000

    Only tell debugger macros are supported when enabled

--- src/extension/providers/debug_adapter_support_uris_factory.ts
+++ src/extension/providers/debug_adapter_support_uris_factory.ts
@@ -1,4 +1,5 @@
 import * as vs from "vscode";
+import { config } from "../config";
 
 export class DartDebugAdapterSupportsUrisFactory implements vs.DebugAdapterTrackerFactory {
 	createDebugAdapterTracker(session: vs.DebugSession): vs.DebugAdapterTracker {
@@ -8,7 +9,7 @@ export class DartDebugAdapterSupportsUrisFactory implements vs.DebugAdapterTrack
 
 class DartDebugAdapterSupportsUris implements vs.DebugAdapterTracker {
 	onWillReceiveMessage(message: any): void {
-		if (message?.command === "initialize" && message.arguments) {
+		if (message?.command === "initialize" && message.arguments && config.experimentalMacroSupport) {
 			message.arguments.supportsDartUris = true;
 		}
 	}
