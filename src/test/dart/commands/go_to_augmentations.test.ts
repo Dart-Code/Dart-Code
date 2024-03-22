f97b74c4c27fe6bdcc47c184aa20223b1c12e04b
Danny Tuppeny <danny@tuppeny.com>
2024-03-22 10:49:08 +0000

    Fix Go to Augmented/Augmentation tests based on SDK versions

---
+++ src/test/dart/commands/go_to_augmentations.test.ts
@@ -0,0 +1,83 @@
+import { strict as assert } from "assert";
+import * as fs from "fs";
+import * as path from "path";
+import * as vs from "vscode";
+import { fsPath, mkDirRecursive } from "../../../shared/utils/fs";
+import { activate, currentEditor, ensureIsRange, extApi, helloWorldFolder, helloWorldPubspec, openFile, rangeOf, waitForResult } from "../../helpers";
+
+const libFile = vs.Uri.file(path.join(fsPath(helloWorldFolder), "lib/go_to_augmentations/lib.dart"));
+const augmentationFile = vs.Uri.file(path.join(fsPath(helloWorldFolder), "lib/go_to_augmentations/lib_augmentation.dart"));
+
+// These files are written at test time because they're only valid for some SDK versions.
+const libContent = `
+import augment 'lib_augmentation.dart';
+
+class A {}
+`;
+const augmentationContent = `
+library augment  'lib.dart';
+
+augment class A {}
+`;
+
+describe("go to", () => {
+	beforeEach("activate", async () => {
+		await activate();
+	});
+
+	beforeEach("skip if not supported", function () {
+		if (!extApi.isLsp || !extApi.dartCapabilities.supportsAugmentations)
+			this.skip();
+	});
+
+	beforeEach("write files", () => {
+		fs.writeFileSync(fsPath(helloWorldPubspec), fs.readFileSync(fsPath(helloWorldPubspec)).toString().replace("sdk: '>=2.12.0 <4.0.0'", "sdk: '>=3.3.0 <4.0.0'"));
+		mkDirRecursive(path.dirname(fsPath(libFile)));
+		fs.writeFileSync(fsPath(libFile), libContent);
+		fs.writeFileSync(fsPath(augmentationFile), augmentationContent);
+	});
+
+	describe("augmentation", () => {
+		beforeEach("wait for outline", async () => {
+			await openFile(libFile);
+			await waitForResult(() => !!extApi.fileTracker.getOutlineFor(libFile));
+		});
+
+		it("navigates to augmented class", async () => {
+			let editor = currentEditor();
+			const e = rangeOf("class |A|");
+			editor.selection = new vs.Selection(e.start, e.end);
+
+			await vs.commands.executeCommand("dart.goToAugmentation");
+
+			editor = currentEditor();
+			assert.equal(fsPath(editor.document.uri), fsPath(augmentationFile));
+			ensureIsRange(
+				editor.selection,
+				rangeOf("augment class |A| {}"),
+			);
+		});
+	});
+
+	describe("augmented", () => {
+		beforeEach("wait for outline", async () => {
+			await openFile(augmentationFile);
+			await waitForResult(() => !!extApi.fileTracker.getOutlineFor(libFile));
+		});
+
+		it("navigates to augmented class", async () => {
+			let editor = currentEditor();
+			const e = rangeOf("augment class |A|");
+			editor.selection = new vs.Selection(e.start, e.end);
+
+			await vs.commands.executeCommand("dart.goToAugmented");
+
+			editor = currentEditor();
+			assert.equal(fsPath(editor.document.uri), fsPath(libFile));
+			ensureIsRange(
+				editor.selection,
+				rangeOf("class |A| {}"),
+			);
+		});
+	});
+});
