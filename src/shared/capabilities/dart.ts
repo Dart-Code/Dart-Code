0c3722c15e88805cefed7fb60ac372bbf73d5227
Danny Tuppeny <danny@tuppeny.com>
2024-05-13 10:15:14 +0100

    Remove experimentalMacroSupport and gate on SDK version instead
    This will allow macros to be controlled just by the SDK experiment flags and not require an additional VS Code setting. It will only enable for >= 3.5.0-0 (current bleeding edge) since that's the only place macros work well enough to try out.
    
    Fixes https://github.com/Dart-Code/Dart-Code/issues/5105

--- src/shared/capabilities/dart.ts
+++ src/shared/capabilities/dart.ts
@@ -43,6 +43,7 @@ export class DartCapabilities {
 	get supportsDartDevToolsPathUrls() { return versionIsAtLeast(this.version, "2.18.0-0"); }
 	get supportsDartRunTest() { return versionIsAtLeast(this.version, "2.12.0-0"); }
 	get supportsLegacyAnalyzerProtocol() { return !versionIsAtLeast(this.version, "3.3.0"); }
+	get supportsMacroGeneratedFiles() { return versionIsAtLeast(this.version, "3.5.0-0"); }
 	get supportsMoveTopLevelToFile() { return versionIsAtLeast(this.version, "2.19.0-283"); }
 	get supportsNonFileSchemeWorkspaces() { return versionIsAtLeast(this.version, "2.13.0-28"); }
 	get supportsCommandParameterSupportedKinds() { return versionIsAtLeast(this.version, "2.19.0-283"); }
