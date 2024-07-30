58873ac1021dbedfab82fac064d954755e463558
Danny Tuppeny <danny@tuppeny.com>
2024-07-30 15:57:39 +0100

    Add more missing interpreters

--- src/test/test_projects/flutter_hello_world/scripts/custom_flutter_test.sh
+++ src/test/test_projects/flutter_hello_world/scripts/custom_flutter_test.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 mkdir -p `dirname "$0"`/has_run
 echo "$@" > `dirname "$0"`/has_run/flutter_test
 flutter "$@"
--- src/test/test_projects/hello_world/scripts/custom_test.sh
+++ src/test/test_projects/hello_world/scripts/custom_test.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 mkdir -p `dirname "$0"`/has_run
 echo "$@" > `dirname "$0"`/has_run/dart_test
 dart run test:test "$@"
