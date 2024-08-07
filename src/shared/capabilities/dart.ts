defdf25671c2d36eb839173139dd62fe829269f2
Danny Tuppeny <danny@tuppeny.com>
2024-08-07 12:24:32 +0100

    Update version for macro/augmentation support
    This is used by tests that need to be skipped when macros (augmentations) aren't supported.

--- src/shared/capabilities/dart.ts
+++ src/shared/capabilities/dart.ts
@@ -31,7 +31,7 @@ export class DartCapabilities {
 	get supportsPubUpgradeMajorVersions() { return versionIsAtLeast(this.version, "2.12.0"); }
 	get needsNoExampleForPubGet() { return versionIsAtLeast(this.version, "3.1.0"); }
 	get omitsVoidForSetters() { return versionIsAtLeast(this.version, "3.3.0-0"); }
-	get supportsAugmentations() { return versionIsAtLeast(this.version, "3.5.0-0"); }
+	get supportsAugmentations() { return versionIsAtLeast(this.version, "3.6.0-0"); }
 	get supportsPubOutdated() { return versionIsAtLeast(this.version, "2.8.0-a"); }
 	get supportsFlutterSidebar() { return versionIsAtLeast(this.version, "3.2.0-201"); }
 	get supportsPubDepsJson() { return versionIsAtLeast(this.version, "2.14.0-0"); }
