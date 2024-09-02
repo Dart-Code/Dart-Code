b1da25e9737f0aefb4719a86d96615806a24ec84
Danny Tuppeny <danny@tuppeny.com>
2024-09-02 11:32:38 +0100

    Normalize drive letter casing for global eval contexts
    Fixes https://github.com/Dart-Code/Dart-Code/issues/5237

--- src/extension/providers/debug_adapter_global_evaluation_context_factory.ts
+++ src/extension/providers/debug_adapter_global_evaluation_context_factory.ts
@@ -1,4 +1,5 @@
 import * as vs from "vscode";
+import { forceWindowsDriveLetterToUppercaseInUriString } from "../../shared/utils/fs";
 import { getActiveRealFileEditor, isDartDocument } from "../editors";
 
 export class DartDebugAdapterGlobalEvaluationContextFactory implements vs.DebugAdapterTrackerFactory {
@@ -13,8 +14,10 @@ class DartDebugAdapterGlobalEvaluationContext implements vs.DebugAdapterTracker 
 			const doc = getActiveRealFileEditor()?.document;
 			if (doc && isDartDocument(doc)) {
 				// Don't escape colons in drive letters, as the shipped DAP code here
-				// will call `lookupPackageUris` on this URI and it won't match.
-				message.arguments.context = doc?.uri.toString(true);
+				// will call `lookupPackageUris` on this URI and it won't match. Also normalize
+				// the drive letter because otherwise it might not work for Windows -> Android
+				// (see https://github.com/Dart-Code/Dart-Code/issues/5237).
+				message.arguments.context = forceWindowsDriveLetterToUppercaseInUriString(doc.uri.toString(true));
 			}
 		}
 	}
