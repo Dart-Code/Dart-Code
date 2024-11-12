1fb75ab8eaa39dd8d09c1992e359f72fa3df8194
Danny Tuppeny <danny@tuppeny.com>
2024-11-12 13:02:26 +0000

    Force allThreadsStopped too

---
+++ src/extension/providers/debug_adapter_forced_single_thread.ts
@@ -0,0 +1,30 @@
+import * as vs from "vscode";
+
+/**
+ * Forces `allThreadsContinued=false` on `continued` events and `allThreadsStopped=false` on `stopped` events
+ * as a workaround until shipped in the DA (and then for older SDK versions!).
+ *
+ * See https://github.com/Dart-Code/Dart-Code/issues/5252 and https://github.com/microsoft/vscode/issues/224832.
+ */
+export class DartDebugForcedSingleThreadFactory implements vs.DebugAdapterTrackerFactory {
+	createDebugAdapterTracker(session: vs.DebugSession): vs.DebugAdapterTracker | undefined {
+		return new DartDebugForcedSingleThread();
+	}
+}
+
+/**
+ * Forces `allThreadsContinued=false` on `continued` events and `allThreadsStopped=false` on `stopped` events
+ * as a workaround until shipped in the DA (and then for older SDK versions!).
+ *
+ * See https://github.com/Dart-Code/Dart-Code/issues/5252 and https://github.com/microsoft/vscode/issues/224832.
+ */
+class DartDebugForcedSingleThread implements vs.DebugAdapterTracker {
+	onDidSendMessage(message: any): void {
+		if (message && message.type === "event" && message.body) {
+			if (message.event === "continued" && message.body.allThreadsContinued === undefined)
+				message.body.allThreadsContinued = false;
+			if (message.event === "stopped" && message.body.allThreadsStopped === undefined)
+				message.body.allThreadsStopped = false;
+		}
+	}
+}
