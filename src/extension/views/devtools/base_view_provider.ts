e90d9b66fb3d2b79550787c742b612461d3e89fd
Danny Tuppeny <danny@tuppeny.com>
2024-12-03 15:06:09 +0000

    Extract a shared base class for DevTools-based sidebar panels

---
+++ src/extension/views/devtools/base_view_provider.ts
@@ -0,0 +1,126 @@
+import * as vs from "vscode";
+import { DartCapabilities } from "../../../shared/capabilities/dart";
+import { envUtils } from "../../../shared/vscode/utils";
+import { DevToolsManager } from "../../sdk/dev_tools/manager";
+
+
+export abstract class MyBaseWebViewProvider implements vs.WebviewViewProvider {
+	public webviewView: vs.WebviewView | undefined;
+	constructor(
+		private readonly devTools: DevToolsManager,
+		private readonly dartCapabilities: DartCapabilities,
+	) { }
+
+	abstract get pageName(): string;
+	abstract get pageRoute(): string;
+
+	public async resolveWebviewView(webviewView: vs.WebviewView, context: vs.WebviewViewResolveContext<unknown>, token: vs.CancellationToken): Promise<void> {
+		this.webviewView = webviewView;
+
+		await this.devTools.start();
+		let pageUrl = await this.devTools.urlFor(this.pageRoute);
+		if (!pageUrl) {
+			webviewView.webview.html = `
+			<html>
+			<body><h1>${this.pageName} Unavailable</h1><p>The ${this.pageName} requires DevTools but DevTools failed to start.</p></body>
+			</html>
+			`;
+			return;
+		}
+
+		pageUrl = await envUtils.exposeUrl(pageUrl);
+		const pageScript = this.getScript();
+
+		webviewView.webview.options = {
+			enableScripts: true,
+			localResourceRoots: [],
+		};
+
+		webviewView.webview.html = `
+			<html>
+			<head>
+			<meta http-equiv="Content-Security-Policy" content="default-src *; script-src 'unsafe-inline'; style-src 'unsafe-inline';">
+			<script>${pageScript}</script>
+			</head>
+			<body><iframe id="devToolsFrame" src="about:blank" frameborder="0" allow="clipboard-read; clipboard-write; cross-origin-isolated" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"></iframe></body>
+			</html>
+			`;
+
+		void webviewView.webview.postMessage({ command: "_dart-code.setUrl", url: pageUrl });
+	}
+
+	protected getScript() {
+		const embedFlags = this.dartCapabilities.requiresDevToolsEmbedFlag ? "embed=true&embedMode=one" : "embedMode=one";
+
+		return `
+	// Track the background color as an indicator of whether the theme changed.
+	let currentBackgroundColor;
+
+	function getTheme() {
+		const isDarkMode = !document.body.classList.contains('vscode-light');
+		const backgroundColor = currentBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--vscode-sideBar-background');
+		const foregroundColor = getComputedStyle(document.documentElement).getPropertyValue('--vscode-sideBar-foreground');
+		const fontSizeWithUnits = getComputedStyle(document.documentElement).getPropertyValue('--vscode-editor-font-size');
+		const fontSize = fontSizeWithUnits && fontSizeWithUnits.endsWith('px') ? parseFloat(fontSizeWithUnits) : undefined;
+
+		return {
+			isDarkMode: isDarkMode,
+			backgroundColor: backgroundColor,
+			foregroundColor: foregroundColor,
+			fontSize: fontSize,
+		};
+	}
+
+	const vscode = acquireVsCodeApi();
+	window.addEventListener('message', (event) => {
+		const devToolsFrame = document.getElementById('devToolsFrame');
+		const message = event.data;
+
+		// Handle any special commands first.
+		switch (message.command) {
+			case "_dart-code.setUrl":
+				const theme = document.body.classList.contains('vscode-light') ? 'light': 'dark';
+				const background = currentBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--vscode-sideBar-background');
+				const foreground = getComputedStyle(document.documentElement).getPropertyValue('--vscode-sideBarTitle-foreground');
+				const qsSep = message.url.includes("?") ? "&" : "?";
+				// Don't include # in colors
+				// https://github.com/flutter/flutter/issues/155992
+				let url = \`\${message.url}\${qsSep}${embedFlags}&theme=\${theme}&backgroundColor=\${encodeURIComponent(background?.replace('#', ''))}&foregroundColor=\${encodeURIComponent(foreground?.replace('#', ''))}\`;
+				const fontSizeWithUnits = getComputedStyle(document.documentElement).getPropertyValue('--vscode-editor-font-size');
+				if (fontSizeWithUnits && fontSizeWithUnits.endsWith('px')) {
+					url += \`&fontSize=\${encodeURIComponent(parseFloat(fontSizeWithUnits))}\`;
+				}
+				if (devToolsFrame.src !== url)
+					devToolsFrame.src = url;
+				return;
+		}
+	});
+
+	function sendTheme() {
+		const devToolsFrame = document.getElementById('devToolsFrame');
+		const theme = getTheme();
+		devToolsFrame.contentWindow.postMessage({
+			method: 'editor.themeChanged',
+			params: {
+				kind: 'themeChanged',
+				theme: theme,
+			}
+		}, "*");
+	}
+
+	document.addEventListener('DOMContentLoaded', function () {
+		new MutationObserver((mutationList) => {
+			for (const mutation of mutationList) {
+				if (mutation.type === "attributes" && mutation.attributeName == "class") {
+					let newBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--vscode-sideBar-background');
+					if (newBackgroundColor !== currentBackgroundColor) {
+						sendTheme();
+						break;
+					}
+				}
+			}
+		}).observe(document.body, { attributeFilter : ['class'], attributeOldValue: true });
+	});
+			`;
+	}
+}
--- src/extension/views/devtools/sidebar.ts
+++ src/extension/views/devtools/sidebar.ts
@@ -1,10 +1,9 @@
 import * as vs from "vscode";
-import { URI } from "vscode-uri";
 import { DartCapabilities } from "../../../shared/capabilities/dart";
 import { IAmDisposable } from "../../../shared/interfaces";
 import { disposeAll } from "../../../shared/utils";
-import { envUtils } from "../../../shared/vscode/utils";
 import { DevToolsManager } from "../../sdk/dev_tools/manager";
+import { MyBaseWebViewProvider } from "./base_view_provider";
 
 export class FlutterDtdSidebar implements IAmDisposable {
 	protected readonly disposables: vs.Disposable[] = [];
@@ -22,119 +21,11 @@ export class FlutterDtdSidebar implements IAmDisposable {
 	}
 }
 
-class MyWebViewProvider implements vs.WebviewViewProvider {
-	public webviewView: vs.WebviewView | undefined;
-	constructor(
-		private readonly devTools: DevToolsManager,
-		private readonly dartCapabilities: DartCapabilities,
-	) { }
-
-	public async resolveWebviewView(webviewView: vs.WebviewView, context: vs.WebviewViewResolveContext<unknown>, token: vs.CancellationToken): Promise<void> {
-		this.webviewView = webviewView;
-
-		await this.devTools.start();
-		let sidebarUrl = await this.devTools.urlFor("editorSidebar");
-		if (!sidebarUrl) {
-			webviewView.webview.html = `
-			<html>
-			<body><h1>Sidebar Unavailable</h1><p>The Flutter sidebar requires DevTools but DevTools failed to start.</p></body>
-			</html>
-			`;
-			return;
-		}
-
-		sidebarUrl = await envUtils.exposeUrl(sidebarUrl);
-		const sidebarUri = URI.parse(sidebarUrl);
-		const frameOrigin = `${sidebarUri.scheme}://${sidebarUri.authority}`;
-		const embedFlags = this.dartCapabilities.requiresDevToolsEmbedFlag ? "embed=true&embedMode=one" : "embedMode=one";
-		const pageScript = `
-// Track the background color as an indicator of whether the theme changed.
-let currentBackgroundColor;
-
-function getTheme() {
-	const isDarkMode = !document.body.classList.contains('vscode-light');
-	const backgroundColor = currentBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--vscode-sideBar-background');
-	const foregroundColor = getComputedStyle(document.documentElement).getPropertyValue('--vscode-sideBar-foreground');
-	const fontSizeWithUnits = getComputedStyle(document.documentElement).getPropertyValue('--vscode-editor-font-size');
-	const fontSize = fontSizeWithUnits && fontSizeWithUnits.endsWith('px') ? parseFloat(fontSizeWithUnits) : undefined;
-
-	return {
-		isDarkMode: isDarkMode,
-		backgroundColor: backgroundColor,
-		foregroundColor: foregroundColor,
-		fontSize: fontSize,
-	};
-}
-
-const vscode = acquireVsCodeApi();
-window.addEventListener('message', (event) => {
-	const devToolsFrame = document.getElementById('devToolsFrame');
-	const message = event.data;
-
-	// Handle any special commands first.
-	switch (message.command) {
-		case "_dart-code.setUrl":
-			const theme = document.body.classList.contains('vscode-light') ? 'light': 'dark';
-			const background = currentBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--vscode-sideBar-background');
-			const foreground = getComputedStyle(document.documentElement).getPropertyValue('--vscode-sideBarTitle-foreground');
-			const qsSep = message.url.includes("?") ? "&" : "?";
-			// Don't include # in colors
-			// https://github.com/flutter/flutter/issues/155992
-			let url = \`\${message.url}\${qsSep}${embedFlags}&theme=\${theme}&backgroundColor=\${encodeURIComponent(background?.replace('#', ''))}&foregroundColor=\${encodeURIComponent(foreground?.replace('#', ''))}\`;
-			const fontSizeWithUnits = getComputedStyle(document.documentElement).getPropertyValue('--vscode-editor-font-size');
-			if (fontSizeWithUnits && fontSizeWithUnits.endsWith('px')) {
-				url += \`&fontSize=\${encodeURIComponent(parseFloat(fontSizeWithUnits))}\`;
-			}
-			if (devToolsFrame.src !== url)
-				devToolsFrame.src = url;
-			return;
+class MyWebViewProvider extends MyBaseWebViewProvider {
+	get pageName(): string {
+		return "Flutter Sidebar";
 	}
-});
-
-function sendTheme() {
-	const devToolsFrame = document.getElementById('devToolsFrame');
-	const theme = getTheme();
-	devToolsFrame.contentWindow.postMessage({
-		method: 'editor.themeChanged',
-		params: {
-			kind: 'themeChanged',
-			theme: theme,
-		}
-	}, "*");
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-	new MutationObserver((mutationList) => {
-		for (const mutation of mutationList) {
-			if (mutation.type === "attributes" && mutation.attributeName == "class") {
-				let newBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--vscode-sideBar-background');
-				if (newBackgroundColor !== currentBackgroundColor) {
-					sendTheme();
-					break;
-				}
-			}
-		}
-	}).observe(document.body, { attributeFilter : ['class'], attributeOldValue: true });
-});
-		`;
-
-		webviewView.webview.options = {
-			enableScripts: true,
-			localResourceRoots: [],
-		};
-
-		webviewView.webview.html = `
-			<html>
-			<head>
-			<meta http-equiv="Content-Security-Policy" content="default-src *; script-src 'unsafe-inline'; style-src 'unsafe-inline';">
-			<script>${pageScript}</script>
-			</head>
-			<body><iframe id="devToolsFrame" src="about:blank" frameborder="0" allow="clipboard-read; clipboard-write; cross-origin-isolated" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"></iframe></body>
-			</html>
-			`;
-
-		void webviewView.webview.postMessage({ command: "_dart-code.setUrl", url: sidebarUrl });
+	get pageRoute(): string {
+		return "editorSidebar";
 	}
-
 }
-
