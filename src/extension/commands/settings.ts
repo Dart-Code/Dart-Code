4c638455467872dee147a4bc2840673c3547c059
Danny Tuppeny <danny@tuppeny.com>
2025-05-19 15:49:31 +0100

    Add a command to DevTools sidebar views to jump to modifying the dart.devtToolsLocation setting

--- src/extension/commands/settings.ts
+++ src/extension/commands/settings.ts
@@ -7,16 +7,21 @@ export class SettingsCommands implements vs.Disposable {
 	private disposables: vs.Disposable[] = [];
 
 	constructor(protected readonly logger: Logger, protected readonly wsContext: WorkspaceContext) {
+		this.disposables.push(vs.commands.registerCommand("_dart.settings.openDevToolsLocationSetting", () => this.openSettings("dart.devToolsLocation", true)));
 		this.disposables.push(vs.commands.registerCommand("_dart.settings.openDartTestAdditionalArgs", () => this.openSettings("dart.testAdditionalArgs")));
 		this.disposables.push(vs.commands.registerCommand("_dart.settings.openFlutterTestAdditionalArgs", () => this.openSettings("dart.flutterTestAdditionalArgs")));
 	}
 
-	private async openSettings(settingName: string): Promise<void> {
-		await vs.commands.executeCommand("workbench.action.openSettings2", {
-			query: settingName,
-			// https://github.com/microsoft/vscode/issues/226071
-			// target: vs.ConfigurationTarget.Workspace,
-		});
+	private async openSettings(settingName: string, forceJson?: boolean): Promise<void> {
+		if (forceJson) {
+			await vs.commands.executeCommand("workbench.action.openSettingsJson", { revealSetting: { key: settingName, edit: true } });
+		} else {
+			await vs.commands.executeCommand("workbench.action.openSettings2", {
+				query: settingName,
+				// https://github.com/microsoft/vscode/issues/226071
+				// target: vs.ConfigurationTarget.Workspace,
+			});
+		}
 	}
 
 	public dispose(): any {
