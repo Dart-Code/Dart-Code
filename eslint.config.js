8cbce06223358a331663535b0bd5545e2d16fcf6
Danny Tuppeny <danny@tuppeny.com>
2025-07-02 16:14:31 +0100

    Switch custom lints to standard rules

--- eslint.config.js
+++ eslint.config.js
@@ -27,5 +27,49 @@ export default defineConfig([
 				tsconfigRootDir: import.meta.dirname,
 			},
 		},
+		rules: {
+			"no-restricted-properties": [
+				"error",
+				{
+					"property": "fsPath"
+				}, {
+					"property": "fileName"
+				}
+			],
+			"no-restricted-imports": ["error", {
+				"patterns": [{
+					"group": ["**/../extension/**"],
+					"message": "Do not import extension code because the extension packing will mean duplicate definitions and state."
+				},
+				{
+					"group": ["**/../debug/**"],
+					"message": "Do not import debugger code because it is expected to run in another process."
+				}]
+			}],
+		}
+	},
+	{
+		// Block importing VS Code except for certain places.
+		files: ["**/*.ts"],
+		ignores: [
+			"src/extension/**",
+			"src/test/**",
+			"**/vscode/**",
+		],
+		rules: {
+			"no-restricted-imports": ["error", {
+				// We need to re-include the patterns from above otherwise they will not be
+				// applied to the bits that match this block.
+				"patterns": [{
+					"group": ["**/../extension/**"],
+					"message": "Do not import extension code because the extension packing will mean duplicate definitions and state."
+				},
+				{
+					"group": ["**/../debug/**"],
+					"message": "Do not import debugger code because it is expected to run in another process."
+				}],
+				"paths": ["vscode"]
+			}],
+		},
 	},
 ]);
