0b6414d6a2dbf5a955e11a344d8635e4180a07d9
Danny Tuppeny <danny@tuppeny.com>
2025-08-05 13:50:46 +0100

    Fix tests

--- src/test/flutter/views/packages_view.test.ts
+++ src/test/flutter/views/packages_view.test.ts
@@ -1,5 +1,5 @@
 import { strict as assert } from "assert";
-import { DART_DEP_DEPENDENCY_PACKAGE_NODE_CONTEXT, DART_DEP_DEV_DEPENDENCY_PACKAGE_NODE_CONTEXT, DART_DEP_TRANSITIVE_DEPENDENCY_PACKAGE_NODE_CONTEXT } from "../../../shared/constants.contexts";
+import { DART_DEP_DEPENDENCY_PACKAGE_NODE_CONTEXT, DART_DEP_DEV_DEPENDENCY_PACKAGE_NODE_CONTEXT, DART_DEP_PROJECT_NODE_CONTEXT, DART_DEP_TRANSITIVE_DEPENDENCY_PACKAGE_NODE_CONTEXT } from "../../../shared/constants.contexts";
 import { ensurePackageTreeNode, getPackages, privateApi } from "../../helpers";
 
 describe("packages tree", () => {
@@ -7,7 +7,10 @@ describe("packages tree", () => {
 	before("get packages", () => getPackages());
 
 	it("includes known packages inside the project in the correct categories", async () => {
-		const dependencyGroups = await privateApi.packagesTreeProvider.getChildren(undefined);
+		const topLevel = await privateApi.packagesTreeProvider.getChildren(undefined);
+		const packageNode = ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "flutter_hello_world");
+
+		const dependencyGroups = await privateApi.packagesTreeProvider.getChildren(packageNode);
 		const directDependencies = await privateApi.packagesTreeProvider.getChildren(dependencyGroups?.find((node) => node.label === "direct dependencies"));
 		const devDependencies = await privateApi.packagesTreeProvider.getChildren(dependencyGroups?.find((node) => node.label === "dev dependencies"));
 		const transitiveDependencies = await privateApi.packagesTreeProvider.getChildren(dependencyGroups?.find((node) => node.label === "transitive dependencies"));
@@ -18,7 +21,10 @@ describe("packages tree", () => {
 	});
 
 	it("does not include own package inside the project", async () => {
-		const dependencyGroups = await privateApi.packagesTreeProvider.getChildren(undefined);
+		const topLevel = await privateApi.packagesTreeProvider.getChildren(undefined);
+		const packageNode = ensurePackageTreeNode(topLevel, DART_DEP_PROJECT_NODE_CONTEXT, "flutter_hello_world");
+
+		const dependencyGroups = await privateApi.packagesTreeProvider.getChildren(packageNode);
 		const directDependencies = await privateApi.packagesTreeProvider.getChildren(dependencyGroups?.find((node) => node.label === "direct dependencies"));
 
 		const self = directDependencies!.find((node) => node.label === "flutter_hello_world");
