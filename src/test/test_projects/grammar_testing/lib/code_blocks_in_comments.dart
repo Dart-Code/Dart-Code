792d851a6ced2c22f44b3aaa12c4acb8000a22eb
Danny Tuppeny <danny@tuppeny.com>
2026-09-24 11:12:28 +0100

    Update Textmate Grammar to support more than 3 backticks for code blocks
    Fixes https://github.com/Dart-Code/Dart-Code/issues/6189

---
+++ src/test/test_projects/grammar_testing/lib/code_blocks_in_comments.dart
@@ -0,0 +1,23 @@
+/// Multiline dartdoc comment with more than three backticks.
+///
+/// ``````
+/// code in six backticks
+/// ```
+/// three backticks is nested code
+/// ```
+/// more code
+/// ``````
+///
+/// Text
+var moreBackticks;
+
+/// Multiline dartdoc comment with more closing backticks.
+///
+/// ``````
+/// code in six backticks
+/// ```
+/// still code
+/// ``````````````````
+///
+/// Text
+var moreClosingBackticks;
